9103460361fa4f2409af57476cea65a69a372a12
Dan Mall <dan@danielmall.com>
2011-04-03 10:35:11 -0400

    Morsel and Design swap project pages; added reset [deploy: staging]

--- bin/j/danielmall.v3.js
+++ bin/j/danielmall.v3.js
@@ -14,93 +14,100 @@ http://danielmall.com/
 
 $(document).ready(function() { 
     
-    var dmall = {};
+    var dmall = {
+    
+    
+        /*-------------------------------------------    
+            Home
+        -------------------------------------------*/
+    
+        homeTooltip: function(){
+        
+            $(".home-round-item a").tooltip({
+                bodyHandler: function() { 
+                    _tooltipText = $(this).attr('data-tooltip-content');
+                    return _tooltipText; 
+                }
+                ,showURL: false
+                ,track: true
+                ,fade: 250
+            });
+        
+        },
     
     
 
-    /*-------------------------------------------    
-        Custom Functions
-    -------------------------------------------*/
+        /*-------------------------------------------    
+            Work
+        -------------------------------------------*/
 
-    /** 
-    *   Creates trick clients heading rollover
-    *
-    *   @author Dan Mall <dan@danielmall.com>
-    *   @version 0.1
-    *   @copyright Dan Mall
-    *
-    */
+        initClientsRollOver: function(){
+        
+            var _animationSpeed = 300;
+            var _animationDelay = 200;
+        
+            $('#work-landing #clients h1').wrap('<div id="clients-headline1" class="mask-wrapper"></div>');
+            var _clientsHeadline = $('#clients-headline1');
+            _clientsHeadline.append('<div class="mask"></div>');
 
-    dmall.initClientsRollOver = function(){
+            _clientsHeadline.after('<div id="clients-headline2" class="mask-wrapper"><h1 id="doppelganger">Clients that couldn&rsquo;t live without me</h1><div class="mask"></div></div>');
+            var _doppelganger = $('#clients-headline2');
         
-        var _animationSpeed = 300;
-        var _animationDelay = 200;
-        
-        $('#work-landing #clients h1').wrap('<div id="clients-headline1" class="mask-wrapper"></div>');
-        var _clientsHeadline = $('#clients-headline1');
-        _clientsHeadline.append('<div class="mask"></div>');
-
-        _clientsHeadline.after('<div id="clients-headline2" class="mask-wrapper"><h1 id="doppelganger">Clients that couldn&rsquo;t live without me</h1><div class="mask"></div></div>');
-        var _doppelganger = $('#clients-headline2');
-        
-        $('#work-landing #clients').mouseenter(function(){
-            $('#clients-headline1 .mask').animate({
-                 width: '130px'
-            }, _animationSpeed, function(){
-                _doppelganger.css('z-index', 2);                
-            });
-            $('#clients-headline2 .mask').delay(_animationDelay).animate({
-                 width: '0'
-            }, _animationTiming);            
-        }).mouseleave(function(){            
-            $('#clients-headline2 .mask').animate({
-                 width: '130px'
-            }, _animationSpeed, function(){
-                _doppelganger.css('z-index', 1);                
-            });
-            $('#clients-headline1 .mask').delay(_animationDelay).animate({
-                 width: '0'
-            }, _animationSpeed);
+            $('#work-landing #clients').mouseenter(function(){
             
-        });
+                $('#clients-headline1 .mask').animate({
+                     width: '130px'
+                }, _animationSpeed, function(){
+                    _doppelganger.css('z-index', 2);                
+                });
 
-    }
+                $('#clients-headline2 .mask').delay(_animationDelay).animate({
+                     width: '0'
+                }, _animationSpeed);            
+            
+            }).mouseleave(function(){    
+                    
+                $('#clients-headline2 .mask').animate({
+                     width: '130px'
+                }, _animationSpeed, function(){
+                    _doppelganger.css('z-index', 1);                
+                });
+            
+                $('#clients-headline1 .mask').delay(_animationDelay).animate({
+                     width: '0'
+                }, _animationSpeed);
+            
+            });
+
+        },
     
     
-    dmall.fadeInTitle = function(){
+        /* Staggered animation for project headers */
+    
+        fadeInTitle: function(){
         
-        var _animationSpeed = 500;
-        var _animationDelay = 150;
+            var _animationSpeed = 500;
+            var _animationDelay = 150;
         
-        $('#project-header .page-title').fadeTo(_animationSpeed, 1);
-        $('#project-header .project-subtitle').delay(_animationDelay).fadeTo(_animationSpeed, 1);
-    }
+            $('#project-header .page-title').fadeTo(_animationSpeed, 1);
+            $('#project-header .project-subtitle').delay(_animationDelay).fadeTo(_animationSpeed, 1);
+            $('.browser-chrome-wrap, .project-image').delay(_animationDelay*3).fadeTo(_animationSpeed, 1);
+        
+        }
     
     
-    /*-------------------------------------------    
-        Global Functions
-    -------------------------------------------*/
-
-    dmall.openSection = function($el){
-        $($el).slideDown('normal');
-    }
-
-    dmall.closeSection = function($el){
-        $($el).slideUp('normal');
-    }
-
-    dmall.fadeInSection = function($el){
-        $($el).fadeIn('normal');
-    }
-
-    dmall.fadeOutSection = function($el){
-        $($el).fadeOut('normal');
+        /*-------------------------------------------    
+            Global Functions
+        -------------------------------------------*/
+        
     }
     
     
     /*-------------------------------------------    
         Initial Actions
     -------------------------------------------*/
+    // HOME
+    dmall.homeTooltip();
     
     // WORK
     dmall.initClientsRollOver();
