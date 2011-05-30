885e0c1c400e82ba8c23930580da7852625a8ffa
Dan Mall <dan@danielmall.com>
2011-05-30 15:57:29 -0400

    Apple icon [deploy:staging]

--- bin-static/_inc/meta.php
+++ bin-static/_inc/meta.php
@@ -1,8 +1,3 @@
 <meta charset="UTF-8" /> 
-    <meta name="author" content="Dan Mall" />
-    
+    <meta name="author" content="Dan Mall" />    
     <meta name="viewport" content="width=800" />
-    
-    <!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-    <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
