a34ba28ccf67d948e8c0031cb919e968944f069a
Dan Mall <dan@danielmall.com>
2011-05-30 18:51:32 -0400

    Adding Google Analytics [deploy: staging]

--- bin-static/_inc/close.php
+++ bin-static/_inc/close.php
@@ -1,7 +1,6 @@
-    <!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet
-         change the UA-XXXXX-X to be your site's ID -->
+    <!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet -->
     <script>
-        var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+        var _gaq = [['_setAccount', 'UA-6227622-1'], ['_trackPageview']];
         (function(d, t) {
             var g = d.createElement(t),
                 s = d.getElementsByTagName(t)[0];
