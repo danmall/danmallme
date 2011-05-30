7c09ed6aabf131a8a33c74fe14384ca904cdfc58
Dan Mall <dan@danielmall.com>
2011-05-30 19:05:56 -0400

    Changing homepage iPhone viewport width [deploy:staging]

--- bin-static/_inc/meta.php
+++ bin-static/_inc/meta.php
@@ -1,3 +1,7 @@
 <meta charset="UTF-8" /> 
-    <meta name="author" content="Dan Mall" />    
+    <meta name="author" content="Dan Mall" />
+    <?php if($_SERVER['REQUEST_URI'] == '/') { ?>      
+    <meta name="viewport" content="width=1100" />
+    <?php } else { ?>
     <meta name="viewport" content="width=900" />
+    <?php } ?>
