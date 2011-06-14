c33d803cfe85532c9916c7d6bffbe18c35ce8433
Dan Mall <dan@danielmall.com>
2011-06-14 06:09:14 -0400

    Clicky [deploy: live]

--- bin-static/_inc/close.php
+++ bin-static/_inc/close.php
@@ -9,7 +9,13 @@
             s.parentNode.insertBefore(g, s);
         })(document, 'script');
     </script>
-
+    
+    <div id="clicky" class="real-hide">
+        <a title="Google Analytics Alternative" href="http://getclicky.com/66439102"><img alt="Google Analytics Alternative" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+        <script src="//static.getclicky.com/js"></script>
+        <script type="text/javascript">try{ clicky.init(66439102); }catch(e){}</script>
+        <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66439102ns.gif" /></p></noscript>
+    </div><!-- #clicky -->
 
 </body>
 </html>
