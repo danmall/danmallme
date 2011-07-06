1e345a1046f33e5b9f51f451745b0ed267964d00
Dan Mall <dan@danielmall.com>
2011-07-06 07:16:02 -0400

    Coupon Craze start [deploy:live]

---
+++ bin-static/work/coupon-craze/index.php
@@ -0,0 +1,139 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/doctype.php"); echo "\n"; ?>
+<head>
+    <title>&ldquo;Coupon Craze,&rdquo; work by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/meta.php"); echo "\n"; ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/cssReference.php"); echo "\n"; ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/jsReference.php"); echo "\n"; ?>
+    <style>
+        .page-title { background-image: url(activate.png); width: 474px; height: 88px; }
+        #project-header hgroup { top: 12px; }  
+
+        .project-image { height: 730px; background: transparent url(activate-hero.jpg) no-repeat center 0; margin-bottom: 0; }
+        .project-url { left: -36px; top: 34px; }
+        
+        #awards { background: transparent url(always-beautiful-awards.png) no-repeat 0 0; height: 319px; }
+            #fwa a { width: 162px; height: 70px; top: 135px; left: 170px; }
+            #webby a { width: 226px; height: 143px; top: 113px; left: 448px; }
+                
+    </style>
+</head>
+
+<body id="work">	
+	
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/header.php"); echo "\n"; ?>
+	
+	<div id="work-area-wrap">
+	    
+	    <section id="work-area" class="wrap800">
+	        
+	        <nav class="breadcrumbs">	            
+	            <ul>
+	                <li id="breadcrumbs-home"><a href="/">Home</a></li>
+	                <li id="breadcrumbs-work"><a href="/work/">Work</a></li>
+	            </ul>
+	        </nav><!-- .breadcrumbs -->
+	        
+	        <div id="project-header">
+    	        <hgroup>
+        	        <h1 class="page-title phark">Coupon Craze</h1>
+        	        <h2 class="project-subtitle">Exclusive Coupons for the Savvy Shopper</h2>        	    
+        	    </hgroup>
+
+        	    <ul id="project-nav" class="no-marker">
+        	        <li class="prev">        	            
+            	        <a href="/work/activate/">Activate</a>
+        	        </li>
+        	        <li class="next">            	        
+            	        <a href="/work/healthymagination/">healthymagination</a>            	        
+        	        </li>
+        	    </ul><!-- #project-nav -->
+        	    
+        	    <span id="facebook-like">
+        	        <iframe src="http://www.facebook.com/plugins/like.php?href=http://danielmall.com/work/coupon-craze/" scrolling="no" frameborder="0" style="border:none; height: 30px;"></iframe>
+        	    </span>
+        	    
+    	    </div><!-- #project-header -->
+	        
+	    </section><!-- #work-area -->
+	    
+	    <div class="project-image">
+            <div class="wrap"><a class="project-url" href="http://activatedrinks.com/">http://activatedrinks.com/</a></div>
+        </div>
+
+        <section id="work-content">
+
+            <div class="wrap">
+
+                <div class="main">
+                    
+                    <p>Putting a new twist on vitamin-enhanced water, ACTIVATE has built their brand through innovative product design and the promise of &ldquo;a turn for the better&rdquo;&mdash;fresh vitamins released into the bottle with a simple twist of the cap. We began our ongoing relationship with the brand by crafting a digital foundation that mirrors their innovative personality, while visually showcasing the power of fresh vitamins.</p>
+
+                    <p>ACTIVATE represents more than just a drink&mdash;it&rsquo;s an action, a transition, and ultimately, a moment. Tasked with creating a digital presence to update the brand&rsquo;s image, we created a free-flowing site that encouraged consumers to explore the product for themselves. </p>
+
+                    <p>Upon landing at <a href="http://activatedrinks.com/">ACTIVATEdrinks.com</a>, users are prompted to &ldquo;twist the cap,&rdquo; activating a rich vertical animation experience that guides you through each section with a flow of vitamins and water. As you scroll, a custom parallax effect gives depth to layers of content and flowing water that changes color as you explore.</p>
+
+                    <p>Using design cues from the new .com, we created a rich tab for ACTIVATE&rsquo;s new Facebook presence. Vibrant colors showcase the brand&rsquo;s updated identity, while custom units allow fans to search for retailers, get free samples, and explore the brand&rsquo;s longstanding partnership with Nourish America. </p>
+
+                    <p>Ultimately, ACTIVATE&rsquo;s new digital presence showcases the brand&rsquo;s unique personality and affinity for innovation&mdash;and the possibilities that come with a turn for the better.</p>
+
+                    <section id="bibliography">
+
+                        <h1>See more</h1>
+
+                        <p><a href="http://www.bigspaceship.com/portfolio/activate-drinks/">Activate Drinks,</a> a case study by Big Spaceship</p>
+
+                    </section><!-- #bibliography -->
+
+                </div><!-- .main -->
+
+                <div class="sub">
+
+                    <h2>Client</h2>
+                    <p>ACTIVATE Drinks</p>
+
+                    <h2>Role</h2>
+                    <ul>
+                        <li>Art Direction</li>
+                        <li>Design</li>
+                    </ul>
+                    
+                    <?php /* ?>
+                    <h2>Team</h2>
+                    <ul>
+                        <li><a href="http://tysondamman.com/">Tyson Damman</a>&mdash;Art Direction, Design</li>
+                        <li><a href="http://www.bbojko.de/">Benjamin Bojko</a>&mdash;Development</li>
+                        <li><a href="http://blog.damian-atkinson.com/">Damian Atkinson</a>&mdash;Design</li>
+                        <li><a href="http://sabahme.com/">Sabah Ahmed</a>&mdash;Production</li>
+                        <li>Mike Kenny&mdash;Production</li>
+                        <li><a href="http://sarabel.tumblr.com/">Sara Lerner</a>&mdash;Strategy</li>
+                    </ul>
+                    <?php */ ?>
+
+                    <h2>Year</h2>
+                    <p>2011</p>
+
+                    <h2>Agency</h2>
+                    <p><a href="http://bigspaceship.com/">Big Spaceship</a></p>
+
+                    <h2>Live Site</h2>
+                    <p><a href="http://activatedrinks.com/">Activate</a></p>
+
+                </div><!-- .sub -->
+
+            </div><!-- .wrap -->
+
+        </section><!-- #work-content -->
+	    
+	    
+	</div><!-- #work-area-wrap -->
+	
+	<div class="wrap">
+    	
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/footer.php"); echo "\n"; ?>
+    	
+    	
+    </div><!-- #wrap -->
+    
+	
+<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/close.php"); echo "\n"; ?>
