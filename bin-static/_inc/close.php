8fd3de4a9dcc27be16805c48e2e63089e5b8da4c
Dan Mall <danielmall@dan.local>
2011-10-05 07:25:35 -0400

    No more Clicky [deploy:live]

--- bin-static/_inc/close.php
+++ bin-static/_inc/close.php
@@ -10,12 +10,14 @@
         })(document, 'script');
     </script>
     
+	<?php /* ?>
     <div id="clicky" class="real-hide">
         <a title="Google Analytics Alternative" href="http://getclicky.com/66439102"><img alt="Google Analytics Alternative" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
         <script src="//static.getclicky.com/js"></script>
         <script type="text/javascript">try{ clicky.init(66439102); }catch(e){}</script>
         <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66439102ns.gif" /></p></noscript>
     </div><!-- #clicky -->
+	<?php */ ?>
 
 </body>
 </html>
