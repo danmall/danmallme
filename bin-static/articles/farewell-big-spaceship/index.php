5b21404ca639506939aed4c2436d0210c6e37aa1
Dan Mall <danielmall@dan.local>
2012-01-25 00:14:54 -0500

    Farewell BSS article

---
+++ bin-static/articles/farewell-big-spaceship/index.php
@@ -0,0 +1,111 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/doctype.php"); echo "\n"; ?>
+<head profile="http://gmpg.org/xfn/11">
+    <title>&ldquo;Farewell, Big Spaceship,&rdquo; an article by Dan Mall</title>
+    <meta name="description" content="Sketchbooks" />
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/meta.php"); echo "\n"; ?>
+    
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/cssReference.php"); echo "\n"; ?>	
+	<script src="http://use.typekit.com/uxw1wbh.js"></script>			
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/jsReference.php"); echo "\n"; ?>
+	<script src="/j/libs/jquery.lettering.js"></script>
+	<script>
+	  $(document).ready(function() {
+		  $(".lettering-hook").lettering();		
+	  });
+	</script>
+    <link rel="stylesheet" href="farewell-bss.css" />		
+    <script>
+		document.write('<link rel="stylesheet" href="typekit.css" />')		
+	</script>	
+    
+</head>
+
+<body id="article" class="articles">	
+	
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/header.php"); echo "\n"; ?>
+	
+	<div id="article-area-wrap">
+	    
+	    <section id="farewell-bss" class="clearfix">
+			
+			<h1><span class="lettering-hook">Today</span> is my last day at <a href="http://bigspaceship.com/">Big Spaceship</a>.</h1>
+			
+			<p>I&rsquo;ve learned a lot here&hellip; more than I probably should have. It&rsquo;s been great to have the opportunity to work with some of the best in the world at what they do. I&rsquo;m incredibly grateful to Michael Lebowitz and the whole crew at Big Spaceship for the incredible opportunity to be a part of the team.</p>
+
+			<p>I’ve got some very exciting things on the horizon. Stay tuned.</p>
+	    
+	    </section><!-- #farewell-bss -->	    
+	    
+	</div><!-- /#article-area-wrap -->
+	
+	
+	<div id="comments">
+	
+	    <?php /* ?><section id="article-meta">
+
+	        <header class="easy-clearing">
+	            <h1>{if comment_total == 0}No comments <a href="#comment-form">yet</a>&hellip;{if:else}{comment_total} Comments{/if}</h1>
+	            <p>Posted in {categories backspace="2"}<a href="{path='channel/index'}">{category_name}</a>, {/categories}</p>
+	        </header>
+        
+	    </section><!-- /#article-meta --><?php */ ?>	    	    
+	    
+	    <div id="comments-wrap">                
+            
+            <div class="main">
+                
+                <div id="disqus_thread"></div>
+                <script>
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'danielmall'; // required: replace example with your forum shortname
+
+                    // The following are highly recommended additional parameters. Remove the slashes in front to use.
+                    var disqus_identifier = 'farewell-bss';
+                    var disqus_url = 'http://danielmall.com/articles/farewell-big-spaceship/';
+                    var disqus_developer = 1;
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+                
+            </div><!-- /.main -->
+	    
+	    
+    	    <div class="sub">
+	        
+    	        <section id="article-info">
+        	        <header>
+        	            <h2>Article Info</h2>
+        	        </header>
+        	        <time datetime="2012-01-02T12:06:30">Published January 2, 2011 at 12:06 <abbr title="Post Meridian">PM</abbr></time>
+    	        </section><!-- /#article-info -->
+    	        
+    	        
+    	        <?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/carbon.php"); echo "\n"; ?>
+	        
+	        
+    	    </div><!-- /.sub -->
+    	    
+    	    
+    	</div><!-- #comments-wrap -->
+	    
+	        
+	</div><!-- #comments -->
+	
+	
+	<div class="wrap">
+    	
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/footer.php"); echo "\n"; ?>
+    	
+    	
+    </div><!-- #wrap -->
+	    
+	
+<?php require($_SERVER["DOCUMENT_ROOT"]."/_inc/close.php"); echo "\n"; ?>
