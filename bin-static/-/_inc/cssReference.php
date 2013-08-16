a21e25d1b989602bbc6167f63f84eb1f9d10be45
Dan Mall <dan@danielmall.com>
2013-08-16 07:40:25 -0400

    Enhanced and Typekit CSS to Sass

--- bin-static/-/_inc/cssReference.php
+++ bin-static/-/_inc/cssReference.php
@@ -1,5 +1,4 @@
-<link rel="stylesheet" href="/-/c/html5reset-1.6.1.css" /> 
-    <link href="/-/c/danielmall.v3.1.css?v=20130725053701" rel="stylesheet" media="screen, projection" />
+<link href="/-/c/danielmall.v3.1.css?v=20130725053701" rel="stylesheet" media="screen, projection" />
     <script>
     document.write('<link href="/-/c/enhanced.css?v=20130725051942" rel="stylesheet" media="screen, projection" />');
     </script>
