5439ea6d1f14947378fbb12fc85ce0d53fb709f5
Dan Mall <dan@danielmall.com>
2013-10-05 14:57:56 -0400

    Testing new stats platforms

--- -/_inc/jsReferenceFooter.php
+++ -/_inc/jsReferenceFooter.php
@@ -1,4 +1,50 @@
-<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+<!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-44625378-1', 'danielmall.com');
+      ga('send', 'pageview');
+
+    </script>
+
+    <!-- GoSquared -->
+    <script type="text/javascript">
+      var GoSquared = {};
+      GoSquared.acct = "GSN-751993-F";
+      (function(w){
+        function gs(){
+          w._gstc_lt = +new Date;
+          var d = document, g = d.createElement("script");
+          g.type = "text/javascript";
+          g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+          var s = d.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(g, s);
+        }
+        w.addEventListener ?
+          w.addEventListener("load", gs, false) :
+          w.attachEvent("onload", gs);
+      })(window);
+    </script>
+
+    <!-- Gauges -->
+    <script type="text/javascript">
+      var _gauges = _gauges || [];
+      (function() {
+        var t   = document.createElement('script');
+        t.type  = 'text/javascript';
+        t.async = true;
+        t.id    = 'gauges-tracker';
+        t.setAttribute('data-site-id', '525060cd108d7b3a0100003a');
+        t.src = '//secure.gaug.es/track.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(t, s);
+      })();
+    </script>
+
+    <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" ></script>
     <script>!window.jQuery && document.write(unescape('%3Cscript src="/-/j/libs/jquery-1.5.1.min.js"%3E%3C/script%3E'))</script>
  
@@ -19,4 +65,4 @@
     <script src="/-/j/libs/waypoints-sticky.min.js"></script>
     
     <!-- Custom Functions -->
-    <script src="/-/j/danielmall.v3.1.min.js?v=20130725051942"></script>
+    <script src="/-/j/danielmall.v3.1.min.js?v=20131005145704"></script>
--- -/_inc/jsReferenceHead.php
+++ -/_inc/jsReferenceHead.php
@@ -1,5 +1,5 @@
 <!-- Mint -->
-    <script src="/mint/?js"></script>
+    <script src="http://stats.danielmall.com/?js"></script>
 
     <!-- Typekit -->
     <script src="http://use.typekit.com/byv8xvy.js"></script>
