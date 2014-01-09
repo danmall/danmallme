ca5801551c66587d26ffc996fc467c7e8972adc7
Dan Mall <danielmall@dantop.local>
2014-01-09 06:31:56 -0500

    New footer content and styles

--- -/_inc/cssReference.php
+++ -/_inc/cssReference.php
@@ -1,4 +1,4 @@
-<link href="/-/c/danielmall.v3.1.css?v=20130725053701" rel="stylesheet" media="screen, projection" />
+<link href="/-/c/danielmall.v3.1.css?v=20140109063000" rel="stylesheet" media="screen, projection" />
     <script>
-    document.write('<link href="/-/c/enhanced.css?v=20130725051942" rel="stylesheet" media="screen, projection" />');
+    document.write('<link href="/-/c/enhanced.css?v=20140109063000" rel="stylesheet" media="screen, projection" />');
     </script>
