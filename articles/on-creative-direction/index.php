e0c4d705b52311d4fd1864800c52171b6b0e28e1
Dan Mall <danielmall@dantop.home>
2014-01-27 08:30:08 -0500

    On Creative Direction: HTML from Editorially added

---
+++ articles/on-creative-direction/index.php
@@ -0,0 +1,167 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head profile="http://gmpg.org/xfn/11">
+    <title>&ldquo;On Creative Direction,&rdquo; an article by Dan Mall</title>
+    <meta name="description" content="Value Pricing" />
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>
+    
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/jsReferenceHead.php"); echo "\n"; ?> 
+	<link rel="stylesheet" href="on-creative-direction.css" />	
+
+	<!-- Facebook -->
+    <meta property="og:title" content="On Creative Direction" />
+    <meta property="og:description"content="A primer on creative direction as compared to art direction and design&hellip; and what they all mean in a digital context."/>	
+    <meta property="og:url" content="http://danielmall.com/articles/on-creative-direction/" />
+    <meta property="og:image" content="http://danielmall.com/articles/on-creative-direction/home-thumb.png" />
+    
+</head>
+
+<body id="article" class="articles">	
+	
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+	
+	<div id="article-area-wrap">
+			
+	    <section id="on-creative-direction" class="clearfix wrap">			
+		
+			<h1 class="article-title">On Creative Direction</h1>
+			<h2 class="article-subtitle">A primer on creative direction as compared to art direction and design&hellip; and what they all mean in a digital context.</h1>
+
+			<p>Three years ago, I wrote an article on <em>A List Apart</em> about one of my favorite topics: <a href="http://alistapart.com/article/art-direction-and-design">the difference between art direction and design</a>. One of the most common themes in the comments was the request to write a follow-up that included creative direction. This is that follow-up.</p>
+			<p>For what it&rsquo;s worth, I&rsquo;ll start by admitting that we&rsquo;re a pretty pedantic bunch, and this discussion certainly fits the mold. We love to define and redefine the terms we use every day. And I&rsquo;m glad we do. In a talk I heard from master storyteller <a href="http://www.prophet.com/about/leadership/stefanovich">Andy Stefanovich</a>, he said, “Words make the world.” Indeed. Beware: pedantry below.</p>
+			<hr />
+			<h1>Defining Creative Direction</h1>
+			<p>Creative direction is a tricky thing to isolate and define. There&rsquo;s a lot of overlap between creative direction, art direction, and design, so it&rsquo;s no surprise that the words are often used interchangeably. I&rsquo;ve had friends that own agencies list jobs for an Art Director, when actually what they needed was a Creative Director. I know people that hold the title of Creative Director and have no idea what&rsquo;s part of their job description—and neither do their bosses.</p>
+			<p>In the same style as my previous article, I&rsquo;ll first try to define the discipline before addressing the responsibilities of the role; that is, I&rsquo;ll try to explain creative direction before talking about Creative Director. Hopefully, you&rsquo;ll see what that&rsquo;s important in just a minute.</p>
+			<blockquote>
+			<p>Creative Direction is the intersection where Art Direction &amp; Design meet Strategy.</p>
+			</blockquote>
+			<p>Creative Direction is championing the intersection where Art Direction &amp; Design meet Strategy. Let&rsquo;s quickly define each of those terms:</p>
+			<ul>
+			<li><strong>Art Direction:</strong> the visceral resonance of how a piece of work feels. In other words, what you feel in your gut when you look at a website, app, or any piece of design work. Usually described in touchy-feely words like <em>elegant</em>, <em>grungy,</em> <em>retro,</em> and more.</li>
+			<li><strong>Design(1):</strong> the physical or literal aspects of a piece of work. Whoever said “design is subjective” wasn&rsquo;t trying hard enough. Good design is measured in precision. Is a headline kerned? Do the baselines align? Do the colors vibrate? Is the image resolution too low for the medium? A piece of work that doesn&rsquo;t adhere to the basic principles graphic designers learn in school, you can say it&rsquo;s poorly designed (even if the art direction calls for something poorly designed… see where we&rsquo;re going with that?).</li>
+			<li><strong>Strategy(2):</strong> the particular means used to achieve a particular goal. The goal of every football team is to win, specifically by scoring more points than the other team. If you have a fast running back and your opponent has a weak defensive line, your strategy—i.e. your particular way to win—is to run the ball. If you have a great quarterback and your opponent has a weak secondary, your strategy is to pass the ball. Your strategy changes as your circumstances do: who your opponent is, weather conditions, injuries on your roster, etc. In digital, if your goal is to sell more product and your target market is senior citizens, one strategy might be to buy Facebook ads (burn).</li>
+			</ul>
+			<p>(I go into more detail about the specifics of art direction &amp; design in <a href="http://alistapart.com/article/art-direction-and-design">my original article</a>.)</p>
+			<p>Back to our definition of creative direction: championing the intersection where art direction &amp; design meet strategy. The primary concern of good creative direction is making sure the art direction &amp; design approaches always support the client&rsquo;s bottom line. If any of those pieces fall short—even if the others are brilliant—that&rsquo;s poor creative direction. You can have a brilliant strategy and art direction, but if it&rsquo;s not appropriately designed, that&rsquo;s poor creative direction. You can have appropriate art direction and gorgeous design, but if the strategy&rsquo;s not sound, that&rsquo;s poor creative direction. You get the idea.</p>
+			<p>One of my favorite examples of great creative direction is <a href="http://www.wk.com/">Wieden+Kennedy</a>&rsquo;s “The Man Your Man Could Smell Like” campaign for Old Spice:</p>
+			<p><a href="http://www.adweek.com/video/wk-old-spice-case-study-120605">http://www.adweek.com/video/wk-old-spice-case-study-120605</a></p>
+			<p>I have no inside knowledge of how this project or the team worked on it, but I&rsquo;ll speculate about it based on what I&rsquo;ve seen. The components:</p>
+			<ul>
+			<li><strong>Goal:</strong> Sell more body wash.</li>
+			<li><strong>Strategy:</strong> Target both men and women, because no one in the category does that (or does that well).</li>
+			<li><strong>Art Direction:</strong> Create a set that looks like “the everyman&rsquo;s shower” and then transform it into a fantastical fantasyland.</li>
+			<li><strong>Design:</strong> Pick neutral shower curtain, average shower tile, etc.</li>
+			<li><strong>Creative Direction:</strong> Make sure all of that stuff holds together.</li>
+			</ul>
+			<p>Have you ever been so focused on writing a JavaScript method that you forget the point of the site for a minute? Or have you ever been so involved in tweaking a bezier curve that you lose sight of the how important it is to increase donations? Creative direction is about the forest <em>and</em> the trees. More specifically, it&rsquo;s about helping those in the trees see the forest, and helping those who only see the forest remember the trees too.</p>
+			<h2>The role of Creative Director</h2>
+			<p>Now that we&rsquo;ve established what <em>creative direction</em> is, let&rsquo;s talk about the role of Creative Director. While the fact that a Creative Director should do creative direction seems obvious, that&rsquo;s not always the case.</p>
+			<p>Many companies treat Creative Director as the next step up in the hierarchy. A designer moves up to being an Art Director and then to being a Creative Director, regardless of whether she has the skills or experience to do creative direction.</p>
+			<p>To make matters more complex, Art Directors can sometimes do creative direction and so can designers. As a Designer, I&rsquo;ve inadvertently done poor creative direction. I&rsquo;ve also worked on projects where my Art Director has given me better creative direction than the Creative Director. Thoroughly confused yet?</p>
+			<p>The bottom line for me is this: I&rsquo;d like to see more Creative Directors do more creative direction. At <a href="http://superfriend.ly/">SuperFriendly</a>, my main role is to be the best Creative Director I can be. For me, that has led to more partnering with Art Directors and Designers so that I can focus on the forest, on creative direction.</p>
+			<p>A Creative Director also has lots of other responsibilities, like mentoring, growing teams, seeing a vision through for each project, establishing a positive culture, and more. While those are important, those are traits of good <em>leaders</em>. A Creative Director is a leader and should certainly be concerned with those things. However, so should anyone that is a manager or has “Director”, “VP,” “Head,” or “Chief” in her title; none of those are specific to creative direction. (You see now why it&rsquo;s hard to distinguish Art Director from Creative Director; they both have the Director part in common, but most miss the main responsibility.)</p>
+			<h2>Creative Directors, creative direction, and me… oh my!</h2>
+			<p>What does this mean for you, dear reader? Here are a few scenarios you might find yourself faced with that need clarification:</p>
+			<ul>
+			<li>I&rsquo;m a client, looking for someone to set the visual tone for my new app/product. <strong>You need an Art Director.</strong></li>
+			<li>I&rsquo;m an agency owner, looking to hire someone who can see higher-level things than my Designers and can impact my business and that of my clients. <strong>You need a Creative Director.</strong></li>
+			<li>I&rsquo;m a Designer, looking for a mentor that can help me hone my skills so that my designs evoke just the right feel. <strong>You need an Art Director.</strong></li>
+			<li>I&rsquo;m a client, looking for someone to tell me why that particular color scheme for my new product is better for my users and will make me rich. <strong>You need a Creative Director.</strong></li>
+			<li>I&rsquo;m an agency owner, looking to hire someone who can ensure that my Designers&rsquo; work is on par. <strong>You need a Creative Director.</strong></li>
+			<li>I&rsquo;m a Designer, looking for guidance to help develop my critical thinking skills so that I can better understand how my designs can affect my clients&rsquo; business. <strong>You need a Creative Director.</strong></li>
+			</ul>
+			<p>Any questions?</p>
+			<h1>Bibliography</h1>
+			<ul>
+			<li><a href="http://www.creativebloq.com/career/how-become-creative-director-11121363">How to Be a Creative Director</a>, by Creative Bloq</li>
+			<li><a href="http://blogs.hbr.org/2013/12/managing-designers-on-two-different-tracks/">Managing Designers on Two Different Tracks</a>, by Harvard Business Review</li>
+			<li><a href="http://www.creativebloq.com/career/what-design-job-titles-mean-11410424">What Design Job Titles Really Mean</a>, by Creative Bloq</li>
+			<li><a href="http://thebitchwhocodes.com/2013/10/18/from-traditional-to-digital-the-modern-creative-director/">From Traditional to Digital—The Modern Creative Director</a></li>
+			</ul>
+			<h1>Footnotes</h1>
+			<ol>
+			<li>Please note that I&rsquo;m talking about design as a synonym for graphic design or visual design, not Design that indicates a larger, holistic system or process.</li>
+			<li>This definition comes from my friend <a href="https://twitter.com/cdmtthws">Chris Matthews</a>.</li>
+			</ol>
+			
+	    </section><!-- #on-creative-direction -->	  	
+	    
+	</div><!-- /#article-area-wrap -->	
+
+	<?php
+
+        echo readNext('How to Get the Work You Want', '/articles/how-to-get-the-work-you-want/');
+
+    ?>
+	
+	<div id="comments">
+	
+	    <?php /* ?><section id="article-meta">
+
+	        <header class="easy-clearing">
+	            <h1>{if comment_total == 0}No comments <a href="#comment-form">yet</a>&hellip;{if:else}{comment_total} Comments{/if}</h1>
+	            <p>Posted in {categories backspace="2"}<a href="{path='channel/index'}">{category_name}</a>, {/categories}</p>
+	        </header>
+        
+	    </section><!-- /#article-meta --><?php */ ?>	    	    
+	    
+	    <div id="comments-wrap">                
+            
+            <div class="main">
+                
+                <div id="disqus_thread"></div>
+                <script>
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'danielmall'; // required: replace example with your forum shortname
+
+                    // The following are highly recommended additional parameters. Remove the slashes in front to use.
+                    var disqus_identifier = 'on-creative-direction';
+                    var disqus_url = 'http://danielmall.com/articles/on-creative-direction/';
+                    var disqus_developer = 1;
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+                
+            </div><!-- /.main -->
+	    
+	    
+    	    <div class="sub">
+	        
+    	        <section class="article-info">
+        	        <header>
+        	            <h2>Article Info</h2>
+        	        </header>
+        	        <time datetime="2013-06-19T20:18:51">Published January 1, 2014 at 11:00 <abbr title="Post Meridian">PM</abbr></time>
+    	        </section><!-- /.article-info -->
+    	        
+    	        
+    	        <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/carbon.php"); echo "\n"; ?>
+	        
+	        
+    	    </div><!-- /.sub -->
+    	    
+    	    
+    	</div><!-- #comments-wrap -->
+	    
+	        
+	</div><!-- #comments -->
+	
+	
+	<div class="wrap">
+    	
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>
+    	
+    	
+    </div><!-- #wrap -->
+    
+	
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>
