d265d199f9483b39327d3c125ff81e19a4d3858a
Dan Mall <danielmall@dantop.home>
2014-01-30 06:23:22 -0500

    Net awards: project awards done

--- articles/net-awards-2014/index.php
+++ articles/net-awards-2014/index.php
@@ -53,7 +53,7 @@
                         for <strong>New Agency of the Year</strong>
                     </h1>
 
-                    <p>I&rsquo;ve seen Arbitrary&rsquo;s work this past year and even collaborated with them. I&rsquo;ll leave it to them to decide what to divulge publicly yet, but you&rsquo;ll be blown away by their quality of work.</p>
+                    <p>I&rsquo;ve seen <a href="http://arbitrary.io/">Arbitrary</a>&rsquo;s work this past year and even collaborated with them. I&rsquo;ll leave it to them to decide what to divulge publicly yet, but you&rsquo;ll be blown away by their quality of work.</p>
 
                 </article><!-- .award-type-entry -->
 
@@ -65,7 +65,7 @@
                         for <strong>Team of the Year</strong>
                     </h1>
 
-                    <p>Because I&rsquo;m part of a nomination in this category, I&rsquo;ll abstain from voting. If you enjoyed our work on <a href="/articles/techcrunch-responsive-redesign/">TechCrunch</a> or <a href="/responsive-mobile-entertainment-weekly/">Entertainment Weekly</a>, I&rsquo;d appreciate if you <a href="https://thenetawards.com/vote/team/the-globalmoxie-team/">gave us a vote</a>, but I won&rsquo;t vote in this category myself.</p>
+                    <p>Because I&rsquo;m part of a nomination in this category, I&rsquo;ll abstain from voting. If you enjoyed our work on <a href="/articles/techcrunch-responsive-redesign/">TechCrunch</a> or <a href="/responsive-mobile-entertainment-weekly/">Entertainment Weekly</a>, I&rsquo;d appreciate if you <a href="https://thenetawards.com/vote/team/the-globalmoxie-team/">give us a vote</a>, but I won&rsquo;t vote in this category myself.</p>
 
                 </article><!-- .award-type-entry -->
 
@@ -78,11 +78,11 @@
                         for <strong>Agency of the Year</strong>
                     </h1>
 
-                    <p>They launched <a href="http://www.teehanlax.com/">a great new site</a>, <a href="http://www.teehanlax.com/story/medium/">helped make Medium a wonderful platform</a>, released <a href="http://labs.teehanlax.com/project/hyperlapse">Hyperlapse</a>, and kept their iPhone <abbr title="Photoshop Document">PSD</abbr> current with <a href="http://www.teehanlax.com/tools/iphone/">an iOS7 version</a>. They deserve the award.</p>
+                    <p><a href="http://teehanlax.com/">Teehan+Lax</a> <a href="http://www.teehanlax.com/blog/welcome-to-the-new-teehanlax-com/">launched a great new site</a>, <a href="http://www.teehanlax.com/story/medium/">helped make Medium a wonderful platform</a>, released <a href="http://labs.teehanlax.com/project/hyperlapse">Hyperlapse</a>, and kept their iPhone <abbr title="Photoshop Document">PSD</abbr> current with <a href="http://www.teehanlax.com/tools/iphone/">an iOS7 version</a>. They deserve the award.</p>
 
                 </article><!-- .award-type-entry -->
 
-            </section><!-- .team-awards -->
+            </section><!-- #team-awards -->
 
 
             <section id="individual-awards" class="award-type">
@@ -98,7 +98,7 @@
                         for <strong>Entrepreneur of the Year</strong>
                     </h1>
 
-                    <p>Simon and Natalie&rsquo;s journey from an idea of a product to seeing it succeed and selling it to Eventbrite is a testament to their hard work. It&rsquo;s the definition of entrepreneurship in action, if you ask me. Natalie&rsquo;s presentation of their story, &ldquo;<a href="https://speakerdeck.com/natbat/from-idea-to-exit">From Idea to Exit</a>,&rdquo; is one of the most honest and refreshing talks I&rsquo;ve seen in a long time.</p>
+                    <p><a href="http://lanyrd.com/">Simon and Natalie</a>&rsquo;s journey from an idea of a product to seeing it succeed and selling it to Eventbrite is a testament to their hard work. It&rsquo;s the definition of entrepreneurship in action, if you ask me. Natalie&rsquo;s presentation of their story, &ldquo;<a href="https://speakerdeck.com/natbat/from-idea-to-exit">From Idea to Exit</a>,&rdquo; is one of the most honest and refreshing talks I&rsquo;ve seen in a long time.</p>
 
                 </article><!-- .award-type-entry -->
 
@@ -125,7 +125,7 @@
                         for <strong>Young Developer of the Year</strong>
                     </h1>
 
-                    <p>I&rsquo;ve used Anthony&rsquo;s <a href="http://jpanelmenu.com/">jPanelMenu</a>, learned a ton from his &ldquo;<a href="http://alistapart.com/article/the-design-of-code-organizing-javascript">The Design of Code: Organizing JavaScript</a>&rdquo; article on <cite>A List Apart</cite>, and enjoyed reading about his adventures in <a href="http://acolangelo.com/blog/switch-programming">Switch Programming</a>. He gets my vote.</p>
+                    <p>I&rsquo;ve used <a href="http://acolangelo.com/">Anthony</a>&rsquo;s <a href="http://jpanelmenu.com/">jPanelMenu</a>, learned a ton from his &ldquo;<a href="http://alistapart.com/article/the-design-of-code-organizing-javascript">The Design of Code: Organizing JavaScript</a>&rdquo; article on <cite>A List Apart</cite>, and enjoyed reading about his adventures in <a href="http://acolangelo.com/blog/switch-programming">Switch Programming</a>. He gets my vote.</p>
 
                 </article><!-- .award-type-entry -->
 
@@ -139,15 +139,189 @@
                         for <strong>Young Designer of the Year</strong>
                     </h1>
 
-                    <p>Sawyer helped make <a href="http://creativemornings.com/">the new Creative Mornings site</a>, helped create <a href="http://www.siteleaf.com/">Siteleaf</a>, and curates <a href="http://www.organizedwonder.com/">Organized Wonder</a>.</p>
+                    <p><a href="http://sawyerhollenshead.com/">Sawyer</a> helped make <a href="http://creativemornings.com/">the new Creative Mornings site</a>, helped create <a href="http://www.siteleaf.com/">Siteleaf</a>, and curates <a href="http://www.organizedwonder.com/">Organized Wonder</a>. I once passed up on hiring Sawyer and have regretted it ever since.</p>
 
                 </article><!-- .award-type-entry -->
 
 
 
-            </section><!-- .team-awards -->
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <a class="award-type-entry-brand" href="https://thenetawards.com/vote/developer/jake-archibald/">
+                            <img class="award-type-entry-brand-image" src="jakearchibald.jpg" alt="" />
+                            Jake Archibald
+                        </a>
+                        for <strong>Developer of the Year</strong>
+                    </h1>
+
+                    <p><a href="http://jakearchibald.com/">Jake</a> has been a long time advocate of <a href="/articles/progressive-enhancement/">progressive enhancement</a>, which became a heated discussion again after I launched <a href="http://sighjavascript.tumblr.com/page/2">Sigh, JavaScript</a>. Jake <a href="http://jakearchibald.com/2013/progressive-enhancement-is-faster/">came to the rescue</a> with a <a href="https://www.google.com/#q=jake+archibald+progressive+enhancement+site:twitter.com">real-time defense</a>. Plus, <a href="http://lanyrd.com/profile/jaffathecake/">his talks</a> are some of the most hilarious, entertaining ways I&rsquo;ve heard technologies explained.</p>
+
+                </article><!-- .award-type-entry -->
 
 
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <span class="award-type-entry-brand award-type-entry-brand-abstain">
+                            &times;
+                        </span>
+                        for <strong>Designer of the Year</strong>
+                    </h1>
+
+                    <p>Because I&rsquo;m nominated in this category, I&rsquo;ll abstain from voting. If you&rsquo;ve gotten value out of <a href="/articles/">my articles</a>, <a href="/work/">my work</a>, or <a href="http://superfriend.ly/">SuperFriendly&rsquo;s work</a>, I&rsquo;d appreciate if you <a href="https://thenetawards.com/vote/designer/daniel-mall/">give me a vote</a>, but I won&rsquo;t vote in this category myself.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <a class="award-type-entry-brand" href="https://thenetawards.com/vote/contribution/brad-frost/">
+                            <img class="award-type-entry-brand-image" src="bradfrost.png" alt="" />
+                            Brad Frost
+                        </a>
+                        for <strong>Outstanding Contribution of the Year</strong>
+                    </h1>
+
+                    <p>I use <a href="http://bradfrostweb.com/">Brad</a>&rsquo;s work and resources almost daily. We worked on projects together that led to the creation of <a href="http://pattern-lab.info/">Pattern Lab</a>, a wonderful way to build websites. Also, <a href="http://bradfrost.github.io/this-is-responsive/">This Is Responsive</a> is priceless.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+            </section><!-- #individual-awards -->
+
+
+
+
+            <section id="community-awards" class="award-type">
+
+                <h1 class="award-type-headline">Community Awards</h1>
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <a class="award-type-entry-brand" href="https://thenetawards.com/vote/podcast/shoptalk/">
+                            <img class="award-type-entry-brand-image" src="shoptalk.jpg" alt="" />
+                            Shoptalk
+                        </a>
+                        for <strong>Podcast of the Year</strong>
+                    </h1>
+
+                    <p>I never miss any episode of <a href="http://shoptalkshow.com/">Shoptalk</a>. <a href="http://chriscoyier.net/">Chris</a> &amp; <a href="http://daverupert.com/">Dave</a> are hilarious, and listening to the show is a big part of how I stay updated on what the kids are doing in web development these days.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <a class="award-type-entry-brand" href="https://thenetawards.com/vote/talk/mike-monteiro-how-designers-des/">
+                            <img class="award-type-entry-brand-image" src="mikemonteiro.jpg" alt="" />
+                            &ldquo;How Designers Destroyed the World&rdquo;
+                        </a>
+                        for <strong>Conference Talk of the Year</strong>
+                    </h1>
+
+                    <p>This is an incredibly important topic, and <a href="https://twitter.com/monteiro">Mike</a> slays it.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <a class="award-type-entry-brand" href="https://thenetawards.com/vote/grassroots/creativemornings/">
+                            <img class="award-type-entry-brand-image" src="creativemornings.png" alt="" />
+                            Creative Mornings
+                        </a>
+                        for <strong>Grassroots Event of the Year</strong>
+                    </h1>
+
+                    <p>One of the things I miss most about living in Brooklyn is Friday mornings with an <a href="http://www.almondinebakery.com/">Almondine</a> croissant and Creative Mornings at <a href="http://www.galapagosartspace.com/">Galapagos</a>. What <a href="http://www.swiss-miss.com/">Tina</a> and crew has done with this event by making it worldwide is nothing short of amazing. (On a related note, keep your eyes peeled, Philly.)</p>
+
+                </article><!-- .award-type-entry -->
+
+
+            </section><!-- #community-awards -->
+
+
+
+            <section id="community-awards" class="award-type">
+
+                <h1 class="award-type-headline">Project Awards</h1>
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <span class="award-type-entry-brand award-type-entry-brand-abstain">
+                            &times;
+                        </span>
+                        for <strong>Best Online Portfolio</strong>
+                    </h1>
+
+                    <p>I&rsquo;m not very familiar with anyone&rsquo;s work in this category. I&rsquo;ll pass.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <a class="award-type-entry-brand" href="https://thenetawards.com/vote/side-project/can-i-use-beta/">
+                            <img class="award-type-entry-brand-image" src="caniuse.png" alt="" />
+                            Can I Use (beta)
+                        </a>
+                        for <strong>Side Project of the Year</strong>
+                    </h1>
+
+                    <p>I use <a href="http://beta.caniuse.com/">Can I Use</a> very frequently. It&rsquo;s the easiest way to reference browser support.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <span class="award-type-entry-brand award-type-entry-brand-abstain">
+                            &times;
+                        </span>
+                        for <strong>Demo of the Year</strong>
+                    </h1>
+
+                    <p>I don&rsquo;t think I saw any of these this past year. I&rsquo;ll pass.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <span class="award-type-entry-brand award-type-entry-brand-abstain">
+                            &times;
+                        </span>
+                        for <strong>Best Collaborative Project</strong>
+                    </h1>
+
+                    <p>Because I&rsquo;m part of a nomination in this category, I&rsquo;ll abstain from voting. If you&rsquo;ve gotten value out of the work we did and wrote up&mdash;<a href="http://bradfrostweb.com/blog/post/techcrunch/" title="&ldquo;TechCrunch,&rdquo; on Brad Frost&rsquo;s site">Brad</a>&rsquo;s, <a href="http://jonathanstark.com/blog/techcrunch/" title="&ldquo;TechCrunch,&rdquo; on Jonathan Stark&rsquo;s site">Jonathan</a>&rsquo;s, and <a href="/articles/techcrunch-responsive-redesign/" title="&ldquo;TechCrunch: A responsive redesign,&rdquo; on this site">mine</a>&mdash;for <a href="http://techcrunch.com/">TechCrunch</a>, I&rsquo;d appreciate if you <a href="https://thenetawards.com/vote/collaboration/the-techcrunch-redesign/">give us a vote</a>, but I won&rsquo;t vote in this category myself.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+
+                <article class="award-type-entry">
+                    <h1 class="award-type-entry-headline">
+                        <span class="award-type-entry-brand award-type-entry-brand-abstain">
+                            &times;
+                        </span>
+                        for <strong>Redesign of the Year</strong>
+                    </h1>
+
+                    <p>Because I&rsquo;m part of a nomination in this category, I&rsquo;ll abstain from voting. If you&rsquo;ve gotten value out of the work we did and wrote up&mdash;<a href="http://globalmoxie.com/blog/entertainment-weekly.shtml" title="&ldquo;Making of: Entertainment Weekly&rsquo;s Responsive Mobile Site,&rdquo; on Josh Clark&rsquo;s site">Josh</a>&rsquo;s, <a href="http://bradfrostweb.com/blog/post/entertainment-weekly/" title="&ldquo;Entertainment Weekly,&rdquo; on Brad Frost&rsquo;s site">Brad</a>&rsquo;s, <a href="http://jonathanstark.com/blog/entertainment-weekly" title="&ldquo;Entertainment Weekly,&rdquo; on Jonathan Stark&rsquo;s site">Jonathan</a>&rsquo;s, and <a href="/articles/responsive-mobile-entertainment-weekly/" title="&ldquo;The responsive mobile Entertainment Weekly,&rdquo; on this site">mine</a>&mdash;for <a href="http://m.ew.com/">Entertainment Weekly</a>, I&rsquo;d appreciate if you <a href="https://thenetawards.com/vote/redesign/entertainment-weekly-global-mox/">give us a vote</a>, but I won&rsquo;t vote in this category myself.</p>
+
+                </article><!-- .award-type-entry -->
+
+
+
+
+
+
+
+            </section><!-- #community-awards -->
 
             
 	    </section><!-- #net-awards-2014 -->	    
