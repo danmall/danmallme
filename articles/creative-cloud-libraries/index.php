87399264f68c1ed58271cbb6cedc33d1b528fc54
Dan Mall <dan@danielmall.com>
2014-10-05 20:34:52 -0400

    Setting up Creative Cloud Libraries

---
+++ articles/creative-cloud-libraries/index.php
@@ -0,0 +1,187 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head profile="http://gmpg.org/xfn/11">
+  <title>&ldquo;#sketchlive,&rdquo; an article by Dan Mall</title>
+  <meta name="description" content="A live 2-day tour through learning Sketch." />
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>
+
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/jsReferenceHead.php"); echo "\n"; ?>
+  <link href="http://fonts.googleapis.com/css?family=Muli:300,400,300italic,400italic" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="sketchlive.css" />
+
+  <!-- Facebook -->
+  <meta property="og:title" content="#sketchlive" />
+  <meta property="og:description"content="A live 2-day tour through learning Sketch."/>
+  <meta property="og:url" content="http://danielmall.com/articles/sketchlive/" />
+  <meta property="og:image" content="http://danielmall.com/articles/sketchlive/home-thumb.png" />
+
+</head>
+
+<body id="article" class="articles">
+
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+	<div id="article-area-wrap">
+
+	    <section id="sketchlive" class="hentry">
+
+            <header class="article-header">
+
+                <div class="inner">
+
+                    <h1 class="entry-title">#sketchlive</h1>
+
+                    <p class="intro">Two weeks ago, I opened <a href="http://bohemiancoding.com/sketch/">Sketch</a> for the first time. Lots of people were raving about it as a great new tool for design, so I wanted to see for myself. I <a href="https://twitter.com/danielmall/status/509717960999849984">tweeted my stream-of-consciousness thoughts</a> for 2 days. Here are the highlights.</p>
+
+                </div><!-- .inner -->
+
+            </header><!-- .article-header -->
+
+            <div class="inner">
+
+                <p class="disclaimer">Disclaimer: this post isn&rsquo;t affiliated with Sketch or Bohemian Coding at all, other than me using the trial version of Sketch 3 for this experiment. Also, my point of comparison is Photoshop, an app I&rsquo;ve used for about 15 years and is the second app I open every morning (after my email client). I&rsquo;ve been on customer advisory boards for new and current Adobe products, but this post and experiment have nothing to do with my affiliation with Adobe. I have never received any compensation from Adobe for endorsing any products, but they did send me a sweet Photoshop pillow once.</p>
+
+                <p>I recently started a new project, redesigning a site for a media company. I learn better when I have an actual project with which to learn the new skill (as opposed to only doing tutorials and theoretical examples), so I dove in head first. On the first day, I had some challenges getting oriented&hellip;</p>
+
+                <blockquote class="twitter-tweet" data-conversation="none" lang="en"><p><a href="https://twitter.com/hashtag/sketchlive?src=hash">#sketchlive</a>: Opening screen: what do I do? What&#39;s an intro document? Is that the same as a new, blank document? <a href="http://t.co/ekbY9UMj66">http://t.co/ekbY9UMj66</a></p>&mdash; Dan Mall (@danielmall) <a href="https://twitter.com/danielmall/status/509736790593667072">September 10, 2014</a></blockquote>
+
+                <p>&hellip; trying to overcome my muscle memory&hellip;</p>
+
+                <blockquote class="twitter-tweet" data-conversation="none" lang="en"><p><a href="https://twitter.com/hashtag/sketchlive?src=hash">#sketchlive</a> Dangit, I can&#39;t stop hitting V to switch to a nonexistent Move tool.</p>&mdash; Dan Mall (@danielmall) <a href="https://twitter.com/danielmall/status/509803501346902017">September 10, 2014</a></blockquote>
+
+                <p>&hellip;and remembering to eat because hangry. </p>
+
+                <blockquote class="twitter-tweet" data-conversation="none" lang="en"><p><a href="https://twitter.com/hashtag/sketchlive?src=hash">#sketchlive</a> Had soup and a sandwich. A super friendly PSA that lunch is as important as design process and tools.</p>&mdash; Dan Mall (@danielmall) <a href="https://twitter.com/danielmall/status/509767825045864448">September 10, 2014</a></blockquote>
+
+                <p>On day 2, I decided to switch from my current project to a slightly different one. My current project&rsquo;s style guide relies on flat colors and utilitarian typography, and, even in a short day, I&rsquo;m convinced that Sketch is great for designing sites like that. But, one of the reasons I continue to use Photoshop is that it&rsquo;s equally flexible for all types of pixel pushing. Sure, there are features&mdash;like the ones in tools like Sketch&mdash;that would make some tasks much easier to accomplish, but for me to fully switch, I&rsquo;ll need to be able to make a range of outputs with the tool.</p>
+
+                <blockquote class="twitter-tweet" data-conversation="none" lang="en"><p>Day 2 of <a href="https://twitter.com/hashtag/sketchlive?src=hash">#sketchlive</a> starts now. Goals for today: discover how good <a href="https://twitter.com/sketchapp">@sketchapp</a> is at allowing for a range of styles. I definitely see&hellip;</p>&mdash; Dan Mall (@danielmall) <a href="https://twitter.com/danielmall/status/510084996292698112">September 11, 2014</a></blockquote>
+
+                <p>I decided to replicate the <a href="http://www.meetyourmx.com/">Meet Your MX</a> site, expertly designed by <a href="https://twitter.com/brijanp">Brijan Powell</a>. I loved this site from the instant I saw it, because it contains a lot of richness in typographic design, graphic design, and interaction design that I think is missing from a lot of sites nowadays. A good design tool should allow me to design a site like this without much friction. A great design tool should make it easier for me to design a site like this.</p>
+
+                <p>I think I did a decent job with the replication (using a separate model):</p>
+
+                <blockquote class="twitter-tweet" data-conversation="none" lang="en"><p><a href="https://twitter.com/hashtag/sketchlive?src=hash">#sketchlive</a> Police dude is in. <a href="http://t.co/4luQ7t5CPG">http://t.co/4luQ7t5CPG</a>&#10;&#10;After importing him in, I&#39;m realizing I need to color correct to up the contrast to…</p>&mdash; Dan Mall (@danielmall) <a href="https://twitter.com/danielmall/status/510193937869721600">September 11, 2014</a></blockquote>
+                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+                <p>At the end of two days, I thought Sketch did a good job of getting out of my way in designing this interface. However, it didn&rsquo;t really do anything to <em>help</em> me design the interface, at least not any more than Photoshop does. To boot, I do a lot of photo manipulation when I&rsquo;m designing: dodging and burning, layer masks, level corrections, and more. Because Sketch doesn&rsquo;t have these tools&mdash;at least for now&mdash;I still need to rely on Photoshop for them, which means I&rsquo;m jumping back and forth between apps. </p>
+
+                <p>So far, Sketch certainly isn&rsquo;t a Photoshop replacement for me, and, if I&rsquo;m in Photoshop for part of the time, I&rsquo;m having a hard time justifying not being in it all of the time.</p>
+
+
+                <h2>the #sketchlive aftermath</h2>
+
+                <p>I&rsquo;ve continued to use Sketch for this media company redesign for the last two weeks. There are a few things that I&rsquo;m really liking:</p>
+
+                <ul>
+                  <li><strong>Export tools.</strong> By far the most impressive feature for me. Being able to select any layer or group and hit <kbd>Cmd+Shift+E</kbd> to generate an image&mdash;in lots of resolutions&mdash;and drag it out to anywhere is priceless. And lightning fast to boot? Photoshop&rsquo;s <kbd>Save for Web</kbd> has a lot to learn from Sketch&rsquo;s exporting.</li>
+                  <li><strong>Text styles.</strong> More Systematic thinking in a graphic design program is incredibly welcome. I use this a lot in Photoshop too.</li>
+                  <li><strong>File sizes.</strong> Seven full comps in one file with a file size of 6.7MB. My last equivalent PSD was 167MB. </li>
+                  <li><strong>Symbols.</strong> I love the idea of resuable elements because it relates to the way I think about developing these designs, but it&rsquo;s taking me a while adopt the practice. I just don&rsquo;t think about it yet. This week, I had to update the footer in all my comps. If I had used symbols, that probably would have been a breeze. But I didn&rsquo;t. So it wasn&rsquo;t.</li>
+                  <li><strong>Copy CSS Attributes</strong>, in theory. I haven&rsquo;t built a site that was designed in Sketch, and I won&rsquo;t be building this one myself. <a href="https://twitter.com/tpitre">TJ Pitre</a> will be building this one, so keep an eye on what he says about it.</li>
+                </ul>
+
+                <p>Is all that enough to be a Photoshop killer? No. At least not yet. But Sketch definitely has a pretty steady place in the arsenal.</p>
+
+            </div><!-- .inner -->
+
+	    </section><!-- #sketchlive -->
+
+
+
+	</div><!-- /#article-area-wrap -->
+
+	<?php
+
+        echo readNext('The Businessology Roadshow', '/articles/businessology-roadshow/');
+
+    ?>
+
+	<div id="comments">
+
+	    <?php /* ?><section id="article-meta">
+
+	        <header class="easy-clearing">
+	            <h1>{if comment_total == 0}No comments <a href="#comment-form">yet</a>&hellip;{if:else}{comment_total} Comments{/if}</h1>
+	            <p>Posted in {categories backspace="2"}<a href="{path='channel/index'}">{category_name}</a>, {/categories}</p>
+	        </header>
+
+	    </section><!-- /#article-meta --><?php */ ?>
+
+	    <div id="comments-wrap">
+
+            <div class="main">
+
+                <div id="disqus_thread"></div>
+                <script>
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'danielmall'; // required: replace example with your forum shortname
+
+                    // The following are highly recommended additional parameters. Remove the slashes in front to use.
+                    var disqus_identifier = 'sketchlive';
+                    var disqus_url = 'http://danielmall.com/articles/sketchlive/';
+                    var disqus_developer = 1;
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+
+            </div><!-- /.main -->
+
+
+    	    <div class="sub">
+
+    	        <section class="article-info">
+        	        <header>
+        	            <h2>Article Info</h2>
+        	        </header>
+        	        <time datetime="2014-09-25T08:35:00">Published September 25, 2014 at 8:35 <abbr title="Ante Meridian">AM</abbr></time>
+    	        </section><!-- /.article-info -->
+
+
+    	        <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/carbon.php"); echo "\n"; ?>
+
+
+    	    </div><!-- /.sub -->
+
+
+    	</div><!-- #comments-wrap -->
+
+
+	</div><!-- #comments -->
+
+
+	<div class="wrap">
+
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>
+
+
+    </div><!-- #wrap -->
+
+    <script>
+
+            var colorClasses = ['green', 'yellow', 'orange', 'red'];
+
+            $('#sketchlive a').mouseover(function(){
+                $(this).removeClass();
+                //assignClass();
+                $(this).addClass(assignClass());
+            });
+
+            function assignClass(){
+                var n = Math.floor((Math.random() * 4));
+                return colorClasses[n];
+                //console.log(n);
+            }
+
+        </script>
+
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>
