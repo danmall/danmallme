5bfdd52bf52b3b7c1010f250c38c9e9e2ab8aa55
Dan Mall <dan@danielmall.com>
2014-10-16 07:29:31 -0400

    Brooklyn Beta article HTML done

---
+++ articles/brooklyn-beta-2014/index.php
@@ -0,0 +1,282 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head profile="http://gmpg.org/xfn/11">
+  <title>&ldquo;Brooklyn Beta 2014,&rdquo; an article by Dan Mall</title>
+  <meta name="description" content="Celebrating my favorite industry event ever." />
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>
+
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/jsReferenceHead.php"); echo "\n"; ?>
+  <link rel="stylesheet" href="bb2014.css" />
+  <script src="//use.typekit.net/cab0tbh.js"></script>
+  <script>
+    try{
+        Typekit.load();
+        document.write('<link rel="stylesheet" href="bb2014.css" />');
+    }catch(e){}
+  </script>
+
+
+  <!-- Facebook -->
+  <meta property="og:title" content="Brooklyn Beta 2014" />
+  <meta property="og:description" content="Celebrating my favorite industry event ever."/>
+  <meta property="og:url" content="http://danielmall.com/articles/brooklyn-beta-2014/" />
+  <meta property="og:image" content="http://danielmall.com/articles/brooklyn-beta-2014/home-thumb.png" />
+
+</head>
+
+<body id="article" class="articles">
+
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+	<div id="article-area-wrap">
+
+	    <section id="bb2014" class="hentry">
+
+            <section class="chunk">
+
+                <h1><strong class="initial">B</strong>rooklyn Beta 2014</h1>
+
+                <p>A few years ago, I met <a href="https://twitter.com/fictivecameron">Cameron Koczon</a> and <a href="https://twitter.com/shiflett">Chris Shiflett</a> for the first time over lunch at Superfine in Brooklyn. I think our mutual friend <a href="http://twitter.com/jasonsantamaria">Jason Santa Maria</a> introduced us, but none of us could either confirm or deny that. Cameron and Chris told me about an idea they had to bring designers and developers together to talk about things we often don’t: products, collaborations, funding, creating. I was awestruck and excited. Months later&mdash;with no involvement from me&mdash;they launched a conference called Brooklyn Beta, the best industry event I&rsquo;ve ever attended. The recent fifth year of Brooklyn Beta is the final event, so I&rsquo;m taking a minute to reflect on what I enjoyed so much.</p>
+
+                <p>My favorite thing about Brooklyn Beta&mdash;which Cameron also celebrated in his closing talk&mdash;was the amazing group of people they brought together. Here are some of my favorite moments from this year:</p>
+
+                <ul class="moments-list">
+                    <li>Holding down the back row with <a href="https://twitter.com/tangentialism">David Yee</a></li>
+                    <li>Taking to <a href="http://twitter.com/jennlukas">Jenn Lukas</a> about the awful food in Italy</li>
+                    <li>Celebrating dad status with <a href="http://twitter.com/chadlervdw">Chandler Van De Water</a></li>                
+                    <li>Planning Git walkthroughs with <a href="https://twitter.com/robweychert">Rob Weychert</a></li>
+                    <li>Watching <a href="https://twitter.com/johnmaeda">John Maeda</a> talk about anything</li>
+                    <li>Having the badge name flip reinstated</li>
+                    <li>Lamenting gray hair with <a href="https://twitter.com/behoff">Brian Hoff</a></li>
+                    <li>Resuming my role of official &ldquo;speaker clicker provider&rdquo;</li>
+                    <li>Road tripping with <a href="https://twitter.com/plasticmind">Jesse Gardner</a></li>
+                    <li>Contemplating agency exits with <a href="https://twitter.com/davatron5000">Dave Rupert</a></li>
+                    <li>Finding out that <a href="https://twitter.com/hellogeri">Geri Coady</a> hates Web design</li>
+                    <li>Sriracha pomegranate wings with <a href="https://twitter.com/mleland">Michael Johnson</a></li>
+                    <li><a href="https://twitter.com/ableparris">Able Parris</a> and I trying to convince the <a href="https://twitter.com/benpeck">Ben Peck</a> from <a href="https://twitter.com/experticity">Experticity</a> to pivot into a magic startup</li>
+                    <li>Soulfully challenged by <a href="https://twitter.com/elleluna">Elle Luna</a></li>
+                    <li>Catching up with <a href="https://twitter.com/marcosuarez">Marco Suarez</a></li>
+                    <li>Chatting with <a href="https://twitter.com/jaminjantz">Jamin Jantz</a> about the butterfly effect</li>
+                    <li>Discussing pricing models with <a href="https://twitter.com/andybudd">Andy Budd</a></li>
+                    <li>Celebrating the luckiness of finding the right person with <a href="https://twitter.com/colly">Simon Collison</a></li>
+                    <li>Beating <a href="https://twitter.com/burciaga">Ismael Burciaga</a> at shuffleboard</li>
+                    <li>Deliberating the merits of mentorship with <a href="https://twitter.com/davedawson">Dave Dawson</a></li>
+                    <li>Talking camping with <a href="https://twitter.com/joshking">Josh King</a></li>
+                    <li>Thanking <a href="https://twitter.com/armstrong">Chris Armstrong</a> for making <a href="https://twitter.com/niiceapp">Niice</a> so nice</li>
+                    <li>Stumbling upon <a href="https://twitter.com/JimJones">Jim Jones</a>, <a href="https://twitter.com/samanthatoy">Samantha Warren</a>, and <a href="https://twitter.com/steph_hay">Steph Hay</a> in the hotel lobby</li>
+                    <li>Discussing small intimate conference-like events with <a href="https://twitter.com/iancoyle">Ian Coyle</a></li>
+                    <li>Making <abbr title="Frequently Asked Questions">FAQ</abbr> suggestions to <a href="https://twitter.com/ReubenIngber">Reuben Ingber</a></li>
+                    <li>Talking about falling out of trees with <a href="https://twitter.com/jonnygotham">Jonny Gotham</a></li>
+                    <li>Discussing light bulb warmth with <a href="https://twitter.com/ryanessmaker">Ryan Essmaker</a> and <a href="https://twitter.com/jasonsantamaria">Jason Santa Maria</a></li>
+                    <li>Taking about unlocking people&rsquo;s potential with <a href="https://twitter.com/elliotjaystocks">Elliot Jay Stocks</a></li>
+                    <li>Learning about <a href="https://twitter.com/CampTechCo">Camp Tech</a> from <a href="https://twitter.com/AverySwartz">Avery Swartz</a></li>
+                    <li>Getting advice on apprenticeships from <a href="https://twitter.com/blumenfeld">Ben Blumenfeld</a></li>
+                    <li>Chatting with <a href="https://twitter.com/chrisfahey">Chris Fahey</a> about mentorship booths</li>
+                    <li>Discussing monopolies with <a href="https://twitter.com/jstutzman">Jake Stutzman</a></li>
+                    <li>Celebrating online friendships with <a href="https://twitter.com/duaneking">Duane King</a></li>
+                    <li>Discussing teaching models with <a href="https://twitter.com/adactio">Jeremy Keith</a></li>
+                    <li>Talking scale with <a href="https://twitter.com/wmdmark">Mark</a> and <a href="https://twitter.com/Paul_Johnson">Paul Johnson</a></li>
+                    <li>Talking baby cuteness with <a href="https://twitter.com/tashmahal">Tash Lampard</a></li>
+                    <li>Taking freelance with <a href="https://twitter.com/ianpatrickhines">Ian Patrick Hines</a></li>
+                    <li>Weighing the pressures of being in charge with <a href="https://twitter.com/benjaminjackson">Ben Jackson</a></li>
+                    <li>Sidewalk talk prep with <a href="https://twitter.com/fictivecameron">Cameron Koczon</a></li>
+                    <li>Talking &ldquo;revenge moves&rdquo; with <a href="https://twitter.com/shiflett">Chris Shiflett</a></li>
+                    <li>Discussing the drawbacks of consulting with <a href="https://twitter.com/jbrewer">Josh Brewer</a></li>
+                    <li>Checking out <a href="https://twitter.com/hungercrunch">Hunger crunch</a> with <a href="https://twitter.com/f1fe">Rob Wright</a> and <a href="https://twitter.com/bryanrmartin">Bryan Martin</a></li>
+                    <li>Finally being able to give <a href="https://twitter.com/jayemsee">Josh Corliss</a> a big hug in person</li>
+                    <li>Taking street fighter with <a href="https://twitter.com/imjameshall">James Hall</a> and <a href="https://twitter.com/destroytoday">Jonnie Hallman</a></li>
+                    <li>Missing seeing <a href="https://twitter.com/blankenship">Joshua</a> and <a href="https://twitter.com/mrsblankenship">Mandy Blankenship</a> here</li>
+                    <li>Fantasizing with <a href="https://www.linkedin.com/pub/stephen-ng/28/b10/9a1">Steve Ng</a> and <a href="https://twitter.com/patrickbjohnson">Patrick Johnson</a> about attending Comic-Con</li>
+                    <li>Congratulating <a href="https://twitter.com/jameswidegren">James Widegren</a> and <a href="https://twitter.com/toreholmberg">Tore Holmberg</a> on their new baby</li>
+                    <li>Missing my annual chat with <a href="https://twitter.com/jontangerine">Jon Tan</a></li>
+                </ul><!-- .moments-list -->
+
+                <p>Did I miss a moment with you? <a href="http://twitter.com/home?status=@danielmall:%20Remember%20when%20we...">Remind me</a> and I&rsquo;ll add it!</p>
+
+            </section><!-- .chunk -->
+
+            <section class="chunk" id="books">
+
+                <p>Another thing that I was pleasantly surprised by was the amonut of book recommendations I received from a lot of people I talked to. Here are some books that I&rsquo;ve added to my reading list:</p>
+
+                <ul class="book-list">
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/0374521670/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=0374521670&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=NKNYFAGGVT7UB75C">
+                            <img src="books/sz.png" alt="" />
+                            S/Z: An essay
+                        </a>
+                        Recommended by <a href="http://twitter.com/mleland">MJ</a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/B004UZ7SQ0/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=B004UZ7SQ0&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=XKGD4J2GGNGOPNKB">
+                            <img src="books/on-the-road.png" alt="" />
+                            On the Road
+                        </a>
+                        Recommended by <a href="http://twitter.com/mfortress">Mike Fortress</a>
+                    </li>
+                    <li>
+                        <a href="http://www.dccomics.com//videos/dc-all-access-batgirls-new-villains-new-suit-and-new-love-life">
+                            <img src="books/batgirl.png" alt="" />
+                            Batgirl #35
+                        </a>
+                        Recommended by <a href="http://twitter.com/linndelicate">Linn Farley</a>
+                    </li>
+                    <li>
+                        <a href="http://www.boom-studios.com/lumberjanes-01-cover-a.html">
+                            <img src="books/lumberjanes.png" alt="" />
+                            Lumberjanes
+                        </a>
+                        Recommended by <a href="http://twitter.com/linndelicate">Linn Farley</a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/0310329035/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=0310329035&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=2A5SXIEV4DU2G4KX">
+                            <img src="books/the-next-story.png" alt="" />
+                            The Next Story
+                        </a>
+                        Recommended by <a href="http://twitter.com/plasticmind">Jesse Gardner</a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/0310329035/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=0310329035&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=2A5SXIEV4DU2G4KX">
+                            <img src="books/the-next-story.png" alt="" />
+                            The Next Story
+                        </a>
+                        Recommended by <a href="http://twitter.com/plasticmind">Jesse Gardner</a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/0393243370/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=0393243370&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=6ZKTHIQQ6HYQ46RI">
+                            <img src="books/how-to-speak-money.png" alt="" />
+                            How to Speak Money
+                        </a>
+                        Recommended by <a href="http://twitter.com/johnmaeda">John Maeda</a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/0595090796/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=0595090796&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=CSK3H4POMEQAMWBC">
+                            <img src="books/crisis-management.png" alt="" />
+                            Crisis Management
+                        </a>
+                        Recommended by <a href="http://twitter.com/johnmaeda">John Maeda</a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/0195373383/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=0195373383&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=DMCCJCJSZWCNGFKL">
+                            <img src="books/bottom-billion.png" alt="" />
+                            The Bottom Billion
+                        </a>
+                        Recommended by <a href="http://twitter.com/jaminjantz">Jamin Jantz</a>
+                    </li>
+                    <li>
+                        <a href="http://www.amazon.com/gp/product/0060005696/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=390957&amp;creativeASIN=0060005696&amp;linkCode=as2&amp;tag=danielmall-20&amp;linkId=XYOTSD4LKNRJQN5S">
+                            <img src="books/the-paradox-of-choice.png" alt="" />
+                            The Paradox of Choice
+                        </a>
+                        Recommended by <a href="http://twitter.com/jaminjantz">Jamin Jantz</a>
+                    </li>
+                </ul><!-- .book-list -->
+
+                <p>What books have you been recommended? What books do you recommend to me? <a href="http://twitter.com/home?status=@danielmall:%20You%20should%20read...">Tell me</a> and I&rsquo;ll add it to the list!</p>
+
+            </section><!-- .chunk -->
+
+            <p>A few years ago, I feel like I cracked the code on Brooklyn Beta. Sure, the speakers were all amazing, but my favorite thing to do was hang out in the back of the room or downstairs and have conversations with people that walked by. I&rsquo;ve made and strengthened friendships at Brooklyn Beta that I wouldn&rsquo;t have been able to since.</p>
+
+            <p>To Cameron, Chris, and the entire Brooklyn Beta team: <strong>thank you</strong> for creating something so special.</p>
+
+	    </section><!-- #bb2014-->
+
+
+
+	</div><!-- /#article-area-wrap -->
+
+	<?php
+
+        echo readNext('Creative Cloud Libraries', '/articles/creative-cloud-libraries/');
+
+    ?>
+
+	<div id="comments">
+
+	    <?php /* ?><section id="article-meta">
+
+	        <header class="easy-clearing">
+	            <h1>{if comment_total == 0}No comments <a href="#comment-form">yet</a>&hellip;{if:else}{comment_total} Comments{/if}</h1>
+	            <p>Posted in {categories backspace="2"}<a href="{path='channel/index'}">{category_name}</a>, {/categories}</p>
+	        </header>
+
+	    </section><!-- /#article-meta --><?php */ ?>
+
+	    <div id="comments-wrap">
+
+            <div class="main">
+
+                <div id="disqus_thread"></div>
+                <script>
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'danielmall'; // required: replace example with your forum shortname
+
+                    // The following are highly recommended additional parameters. Remove the slashes in front to use.
+                    var disqus_identifier = 'bb2014';
+                    var disqus_url = 'http://danielmall.com/articles/brooklyn-beta-2014/';
+                    var disqus_developer = 1;
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+
+            </div><!-- /.main -->
+
+
+    	    <div class="sub">
+
+    	        <section class="article-info">
+        	        <header>
+        	            <h2>Article Info</h2>
+        	        </header>
+        	        <time datetime="2014-10-16T06:45:00">Published October 16, 2014 at 6:45 <abbr title="Ante Meridian">AM</abbr></time>
+    	        </section><!-- /.article-info -->
+
+
+    	        <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/carbon.php"); echo "\n"; ?>
+
+
+    	    </div><!-- /.sub -->
+
+
+    	</div><!-- #comments-wrap -->
+
+
+	</div><!-- #comments -->
+
+
+	<div class="wrap">
+
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>
+
+
+    </div><!-- #wrap -->
+
+    <script>
+
+            var colorClasses = ['green', 'yellow', 'orange', 'red'];
+
+            $('#sketchlive a').mouseover(function(){
+                $(this).removeClass();
+                //assignClass();
+                $(this).addClass(assignClass());
+            });
+
+            function assignClass(){
+                var n = Math.floor((Math.random() * 4));
+                return colorClasses[n];
+                //console.log(n);
+            }
+
+        </script>
+
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>
