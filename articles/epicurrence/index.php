2e71b57582f7864eb3e1c99c8c27862e634e9792
DANIEL MALL <danielmall@dan.local.home>
2015-10-28 01:12:32 -0400

    Article written and done; now styling

---
+++ articles/epicurrence/index.php
@@ -0,0 +1,176 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head profile="http://gmpg.org/xfn/11">
+  <title>&ldquo;Epicurrence,&rdquo; an article by Dan Mall</title>
+  <meta name="description" content="The time has come for me to step away from Businessology." />
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>
+
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+  <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/jsReferenceHead.php"); echo "\n"; ?>
+  <link rel="stylesheet" href="epicurrence.css" />
+  <script src="https://use.typekit.net/rzf2idw.js"></script>
+  <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+  <!-- Facebook -->
+  <meta property="og:title" content="Epicurrence" />
+  <meta property="og:description"content="The time has come for me to step away from Businessology."/>
+  <meta property="og:url" content="http://danielmall.com/articles/finished-businessology/" />
+  <meta property="og:image" content="http://danielmall.com/articles/businessology-roadshow/home-thumb.png" />
+
+</head>
+
+<body id="article" class="articles">
+
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+	<div id="article-area-wrap">
+
+	    <section id="epicurrence" class="clearfix wrap hentry">
+
+        <h1 class="article-title">Epicurrence</h1>
+
+        <section class="chunk inner">
+
+          <p><a href="https://www.epicurrence.com/">Epicurrence</a> was the most beautiful, energizing, comfortable, and relaxing <em>uncomfortable</em> experience I&rsquo;ve had in a long time.</p>
+
+          <p>I&rsquo;ve been attending and speaking at design conferences for over a decade now, so I feel like I have the rhythm down: a full day of sessions of varying value, usually 9ish to 5ish, with a few hours before and after to catch some sights and meals with new friends you&rsquo;ve just made. But the beauty of Epicurrence is that every detail was designed. By a designer. And a damn good one at that.</p>
+
+          <p>I can&rsquo;t say enough good things about <a href="https://twitter.com/dannpetty">Dann Petty</a>, Epicurrence&rsquo;s producer, director, and creator. I&rsquo;m not sure if Dann envisioned every little amazing detail or if he just got lucky in the way it came together, but he totally got it right. I overheard him say that our industry works really hard together, so we deserve to play hard together too. The format of Epicurrence was flipped from a traditional conference: instead of talks during the day and extracurriculars outside of that, all the talks/panels happened from 8am &ndash; 10am or 6pm &ndash; 10pm. From 10am to 5pm, your time was your own to group up and enjoy being in Hawaii. </p>
+
+          <p>The calm and beauty of Oahu can&rsquo;t be understated. It&rsquo;s a long trip from Philly, which has always made me skeptical of the value of the trip. But Epicurrence was the perfect excuse to travel 5,000 miles to see for myself. The tranquility wasn&rsquo;t immediately apparent upon arriving at a fairly municipal Honolulu Airport, but every minute of the hour drive to the North Shore revealed more lush greenery and majestic mountains. Upon arriving at the amazing <a href="https://us.billabong.com/blog/post/take-a-tour-of-the-billabong-hawaii-house">Billabong House</a> (where the majority of the conference was held), I was completely ready for a week of stress-free relaxation.</p>
+
+          <p>And yet, as much as I was relaxed, I was just as uneasy the whole time. </p>
+
+          <p>Being at Epicurrence emphasized the myopia that exists all too often in our industry, and in the world. I became acutely aware of my minority status at this event:</p>
+
+          <ul>
+            <li>The premise of this edition of Epicurrence in Hawaii revolved around surfing. I don&rsquo;t know how to swim. Though I was honored and excited to be invited&mdash;especially after reading some <a href="http://philcoffman.com/epicurrence">amazing</a> <a href="http://mds.is/daydreaming-of-epicurrence/">recounts</a> and <a href="http://gallery.epicurrence.com/">images</a> of the first version&mdash;I was equally insecure and embarrassed of being alone checking email on the shore while everyone else caught some choice waves. As a kid, I always weaseled my way out of going to summer camp because I didn&rsquo;t want the other kids to make fun of me for not knowing how to swim, and this North Shore summer camp for adults dredged those feelings up nice and good.</li>
+            <li>Race is always an easy go-to minority designation for me. As a pretty unique mix&mdash;half-Pakistani, half-Filipino&mdash;I&rsquo;m under no illusion that I&rsquo;ll find other Pakipinos (that&rsquo;s right) wherever I am unless my brother&rsquo;s around. That said, of the seventy-some people in attendance, it was glaringly obvious that there were only a handful of us with slightly more pigment in our skin than others.</li>
+            <li>Most of the attendees came from the Bay Area. That&rsquo;s not accidental; a lot of the topics and conversations this event was designed for are well-suited to that demographic. But as a lifelong East-Coaster, certain conversations were straight-up culture shock. For example, <a href="https://twitter.com/jaredfitch">Jared Fitch</a> spent some time talking to us about all the cool things that <a href="http://www.vivintsolar.com/">Vivint Solar</a> is doing. When my wife and I responded to Jared&rsquo;s prompting that we often pay hundreds of dollars a month to heat our house in the Pennsylvania winter, there was an audible gasp in the crowd, revealing just how different some of our lives are.</li>
+            <li>A good majority of attendees worked as product designers at companies like Twitter, Facebook, OpenTable, Dropbox, and the like. I&rsquo;ve never worked at a product company.</li>
+            <li>Lots of attendees were fairly young in age and career path. While I&rsquo;m no veteran, I&rsquo;m not exactly a spring chicken either.</li>
+          </ul>
+
+          <p>The list goes on. In fact, a lot of the event content focused both directly and indirectly on minority groups in our work. Jessica Collier wrote specifically about <a href="https://medium.com/@verbagetruck/discomfort-zone-talking-diversity-at-epicurrence-ba77fda4a784#.anhvapq6x">the discomfort of a women&rsquo;s panel</a> that happened on the second night. After it didn&rsquo;t come up organically in a discussion about how separate disciplines at Dropbox figured out how to work well together, <a href="https://twitter.com/donovanxdb">Donovan Bass</a> volunteered his willingness to talk about his experiences as a minority in tech, and a panel that was just about to wrap up was immediately extended by 45 minutes. The desire for discussion around this topic was palpable. </p>
+
+        </section><!-- .chunk -->
+
+        <section class="chunk inner">
+
+          <p>When I&rsquo;m coaching designers or agency owners, one of the first exercises I have them do is to list out all the things that make them different than their competitors. It usually starts out with BS like &ldquo;We care more than they do&rdquo; but eventually gets to something substantive. Choosing to positioning yourself as a minority&mdash;as something different than the sea of same out there&mdash;is often an effective branding technique.</p>
+
+          <p>I&rsquo;ve always used being a minority for my benefit. Growing up in North Philly gives me an edge in designing grittier work than my more prevalent suburban counterparts. I&rsquo;m not sure if I&rsquo;ve ever been invited to speak at a conference because I&rsquo;m brown, but I&rsquo;m sure it doesn&rsquo;t hurt. Being a minority can be great when you&rsquo;re treated well because of it, but more often than not, it gets you treated more poorly. Females in male-centric environments tend to be overlooked. The poverty-stricken in affluent neighborhoods are feared. Millennials think boomers have outlived their usefulness. In uncomfortable situations, minorities tend to want to obfuscate their minority-ness; I know I do.</p>
+
+          <p>Going to Epicurrence forced me consider all of this. It was the first time I really thought about the advantages&mdash;and disadvantages&mdash;of being a minority. Being part of some minorities is a choice; being part of others isn&rsquo;t. I can&rsquo;t choose my race or my gender, but I <em>can</em> decide which experiences to partake in and which to abstain from.</p>
+
+        </section><!-- .chunk -->
+
+        <section class="chunk inner">
+
+          <p>So I surfed. </p>
+
+          <p>This non-swimmer strapped on a life jacket and went for it. My identification as part of the "non-surfers" minority group at a surf-centric conference and the realization that I could change that was exactly what I needed to stretch me. <a href="https://twitter.com/emilyjanemall">Em</a>, <a href="https://twitter.com/mantwan">Anthony</a>, <a href="https://twitter.com/natalievi">Natalie</a>, and I met a few friendly surf instructors at a mild beach and caught tiny waves for an hour and a half. I probably caught about ten waves and stood up on about four. (I could have stood on all of them, but I was too freaked out about falling into the drink and getting tousled around.) I didn&rsquo;t realize this until later, but those "friendly surf instructors" were actually professional surfers <a href="https://www.google.com/?gws_rd=ssl#q=myles+padaca">Myles Padaca</a>, <a href="https://www.google.com/?gws_rd=ssl#q=pancho+sullivan">Pancho Sullivan</a>,  <a href="http://www.surfermag.com/features/goliaths-of-the-pacific-2/#lRvFrF3Y63vjH4BG.97">Jason Murray</a>, and a local named Tawa. I learned to surf from pro surfers! And I almost missed it because I was too comfortable being uncomfortable.</p>
+
+          <p>With that newfound logic, this unadventurous homebody did a no-doors helicopter tour over Oahu.</p>
+
+          <p>This &ldquo;never show weakness&rdquo; fool humbly asked for career advice from people I really admire like <a href="https://twitter.com/dburka">Daniel Burka</a>, <a href="https://twitter.com/mikeindustries">Mike Davidson</a>, <a href="https://twitter.com/hemeon">Marc Hemeon</a>, <a href="https://twitter.com/gt">Geoff Teehan</a>, and more.</p>
+
+        </section><!-- .chunk -->
+
+        <section class="inner">
+
+          <p>Despite its low-key facade, Epicurrence was an incredibly deep and exploratory adventure for me, both internally and externally. Dann, I&rsquo;m not sure if you know what you&rsquo;re on to or if this was all part of your master plan, but, from the bottom of my heart, thank you.</p>
+
+          <h2>Read more about Epicurrence:</h2>
+
+          <ul>
+            <li><a href="https://medium.com/@verbagetruck/discomfort-zone-talking-diversity-at-epicurrence-ba77fda4a784#.bqaa9dc74">Make Me Uncomfortable: The Discomfort Zone at Epicurrence</a>, by Jessica Collier</li>
+            <?php /* ?><li><a href="https://in.invisionapp.com/boards/49ERL05V6Q8G/#/688932">Epicurrence No. 2</a>, by Aaron Stump</li><?php */ ?>
+            <li><a href="https://medium.com/@tobygrubb/stop-attending-conferences-8d4f6de09bd8#.wf4o67eu5">How do you get inspired? Do you attend a conference? Listen to a speech? Go surfing? We say: do it all.</a>, by Toby Grubb</li>
+          </ul>
+
+          <footer class="footnote">
+              <p>Also published on <a href="#">Medium</a>. If this was all too heavy and you&rsquo;d prefer a lighter, more photographic version, allow me to point you over to <a href="#">Exposure</a>.</p>
+          </footer><!-- .footnote -->
+
+        </section><!-- .inner -->
+
+	    </section><!-- #epicurrence -->
+
+	</div><!-- /#article-area-wrap -->
+
+	<?php
+
+        echo readNext('Time for Work', '/articles/time-for-work/');
+
+    ?>
+
+	<div id="comments">
+
+	    <?php /* ?><section id="article-meta">
+
+	        <header class="easy-clearing">
+	            <h1>{if comment_total == 0}No comments <a href="#comment-form">yet</a>&hellip;{if:else}{comment_total} Comments{/if}</h1>
+	            <p>Posted in {categories backspace="2"}<a href="{path='channel/index'}">{category_name}</a>, {/categories}</p>
+	        </header>
+
+	    </section><!-- /#article-meta --><?php */ ?>
+
+	    <div id="comments-wrap">
+
+            <div class="main">
+
+                <div id="disqus_thread"></div>
+                <script>
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'danielmall'; // required: replace example with your forum shortname
+
+                    // The following are highly recommended additional parameters. Remove the slashes in front to use.
+                    var disqus_identifier = 'epicurrence';
+                    var disqus_url = 'http://danielmall.com/articles/epicurrence/';
+                    var disqus_developer = 1;
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+
+            </div><!-- /.main -->
+
+
+    	    <div class="sub">
+
+    	        <section class="article-info">
+        	        <header>
+        	            <h2>Article Info</h2>
+        	        </header>
+        	        <time datetime="2015-07-03T14:00:00">Published July 3, 2015 at 2:00 <abbr title="Post Meridian">PM</abbr></time>
+    	        </section><!-- /.article-info -->
+
+
+    	        <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/carbon.php"); echo "\n"; ?>
+
+
+    	    </div><!-- /.sub -->
+
+
+    	</div><!-- #comments-wrap -->
+
+
+	</div><!-- #comments -->
+
+
+	<div class="wrap">
+
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>
+
+
+    </div><!-- #wrap -->
+
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>
