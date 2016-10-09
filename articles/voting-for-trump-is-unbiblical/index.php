875970ad849d054071df93e378348cde73d2dc22
DANIEL MALL <danielmall@dan.local>
2016-10-09 16:20:26 -0400

    Draft 1 of Trump article

---
+++ articles/voting-for-trump-is-unbiblical/index.php
@@ -0,0 +1,184 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head profile="http://gmpg.org/xfn/11">
+    <title>&ldquo;Voting for Trump is Unbiblical,&rdquo; an article by Dan Mall</title>
+    <meta name="description" content="Accountability" />
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>
+    
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/jsReferenceHead.php"); echo "\n"; ?>    
+    <link href='http://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="/articles/_vanilla/vanilla.css" />
+    <link rel="stylesheet" href="voting-for-trump-is-unbiblical.css" />
+
+    <?php /* ?>
+	<!-- Facebook -->
+    <meta property="og:title" content="Oil change &amp; Pizza" />
+    <meta property="og:description" content="Death to &ldquo;full service.&rdquo;"/>	
+    <meta property="og:url" content="http://danielmall.com/articles/oil-change-pizza/" />
+    <meta property="og:image" content="http://danielmall.com/articles/oil-change-pizza/home-thumb.png" />
+    <?php */ ?>
+
+    <!-- Trying @adactio's meta: https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@danielmall" />
+    <meta name="twitter:url" property="og:url" content="http://danielmall.com/articles/accountability/" />
+    <meta name="twitter:title" property="og:title" content="Voting for Trump is Unbiblical" />
+    <meta name="twitter:description" property="og:description" content="A 3-step formula for accountability between yourself and those you interact with." />
+    <meta name="twitter:image" property="og:image" content="http://danielmall.com/articles/accountability/home-thumb.jpg" />
+    
+</head>
+
+<body id="article" class="articles">	
+	
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+	
+	<div id="article-area-wrap">
+			
+	    <section class="clearfix wrap vanilla">
+
+            <div class="intro">			
+
+                <p>I&rsquo;ve usually been pretty silent on commenting about politics, as I don&rsquo;t know much about them. But I do know enough to dread waking up one day to find Trump as the president of my country. I&rsquo;m a registered Democrat who finds himself more and more siding with Republican ideas, but I can&rsquo;t in good conscience even consider Trump to <del>lead</del> <ins>serve</ins> our country as president.</p>
+
+                <p>There&rsquo;s a difference between behavior and character. Sure, the former hints of the latter, but I do believe behavior can change. Every presidential candidate in history has exhibited un-presidential behavior, and they won&rsquo;t be the last. However, everything I&rsquo;ve read and seen about Trump point to his actions reflecting his character, his true nature. His unapologetic aloofness towards every allegation against him is astounding, suggesting that he may even enjoy living the delusion that his lifestyle is harmless and unrelated to how he&rsquo;ll lead our nation.</p>
+
+                <p>I&rsquo;ve been haunted by a few thoughts from my friend Elliot Jay Stocks&rsquo; article, <a href="http://www.elliotjaystocks.com/blog/a-post-brexit-world/">A post-Brexit world</a>:</p>
+
+                <blockquote>
+                    <p>My personal mission, going forward, is to engage more with family members about politics. Rather than going for the easy option and not wanting to create arguments, I&rsquo;m going to question political viewpoints that don&rsquo;t seem to follow reason. I firmly believe that if I&rsquo;d done that before now, I would&rsquo;ve been able to sway at least a few relatives towards a Remain vote. And if friends had done the same with their families, well, it might&rsquo;ve made a difference. Perhaps that&rsquo;s where we failed.</p>
+                </blockquote>
+
+                <p>Time to speak up.</p>
+
+            </div><!-- .intro -->
+
+            <hr /><br /><br />
+			
+			<h1 class="article-header-title">Voting for Trump is Unbiblical</h1>
+
+            <?php /* ?>
+            <div class="margin-bottom">
+                <iframe src='https://spoken.co/t/2527241/embed' frameborder='0' width='100%' height='84' style='max-width:400px;'></iframe>
+            </div><!-- .margin-bottom -->
+            <?php */ ?>
+
+            <blockquote>
+
+                <p>&hellip;appoint elders<sup><a id="fn1-source" href="#fn1">1</a></sup> in every city as I commanded you&mdash;if a man is blameless, the husband of one wife, having faithful children not accused of dissipation or insubordination. For a bishop<sup><a id="fn2-source" href="#fn2">2</a></sup> must be blameless, as a steward of God, not self-willed, not quick-tempered, not given to wine, not violent, not greedy for money, but hospitable, a lover of what is good, sober-minded, just, holy, self-controlled, holding fast the faithful word as he has been taught, that he may be able, by sound doctrine, both to exhort and convict those who contradict (Titus 1:5&ndash;9, <abbr title="New King James Version">NKJV</abbr>).</p>
+                
+            </blockquote>
+
+            <p>This is the Bible&rsquo;s directive for any leader. Donald Trump fulfills very little (if any) of this criteria. Let&rsquo;s tally:</p>
+
+            <ul>
+                <li>Not self-willed. &ldquo;<a href="http://www.politico.com/blogs/2016-gop-primary-live-updates-and-results/2016/03/trump-foreign-policy-adviser-220853">Trump: I consult myself on foreign policy</a>.&rdquo;</li>
+                <li>Not quick-tempered. <a href="https://www.google.com/#q=donald+trump+temper">600,000 search results for &ldquo;Donald Trump temper&rdquo;</a></li>
+                <li>Not violent. &ldquo;<a href="http://gawker.com/the-time-donald-trumps-ex-wife-accused-him-of-brutally-1721129617">The Time Donald Trump&rsquo;s Ex-Wife Accused Him of Brutally Raping Her</a>.&rdquo;</li>
+                <li>Not greedy for money. &ldquo;<a href="http://ezkool.com/2016/02/donald-trump-i-like-money-im-very-greedy-im-a-greedy-person/">Donald Trump – &lsquo;I Like Money. I&rsquo;m Very Greedy. I&rsquo;m A Greedy Person&rsquo;</a>.&rdquo;</li>
+                <li>Hospitable. &ldquo;<a href="http://www.cnn.com/2015/12/07/politics/donald-trump-muslim-ban-immigration/">Donald Trump: Ban all Muslim travel to U.S.</a>.&rdquo;</li>
+            </ul>
+
+            <p>The list goes on.</p>
+
+            <p>How any Bible-believing Christian can consider voting for Trump is baffling to me.</p>
+
+            <p>And please don&rsquo;t conflate a denouncement of Trump for an endorsement of Hillary Clinton. I don&rsquo;t know much about her policies&mdash;something I&rsquo;ll correct between now and Election Day&mdash;but she at least scores higher on this list than he does.</p>
+
+
+			
+			
+            
+            <footer class="footnote">
+
+                <?php /* ?>
+                <p>Also <a href="https://medium.com/@danielmall/accountability-e64e18e4cfce#.p8ixxd221">syndicated to Medium</a>.</p>
+                <?php */ ?>
+
+                <ol>
+                    <li id="fn1">From the Greek <i lang="el">presbyteros</i>, meaning &ldquo;older man and older woman, who may or may not be official leaders of the church.&rdquo; <a class="footnote-arrow" href="#fn1-source">&#8617;</a></li>
+                    <li id="fn2">From the Greek <i lang="el">episkopos</i>, meaning &ldquo;overseer or supervisor.&rdquo; This word was often used to refer to official titles in civil life. <a class="footnote-arrow" href="#fn2-source">&#8617;</a></li>
+                  </ol>
+            </footer><!-- .footnote -->
+            
+			
+	    </section><!-- .vanilla -->	  	
+	    
+	</div><!-- /#article-area-wrap -->	
+
+	<?php
+
+        echo readNext('Accountability', '/articles/accountability/');
+
+    ?>
+	
+	<div id="comments">
+	
+	    <?php /* ?><section id="article-meta">
+
+	        <header class="easy-clearing">
+	            <h1>{if comment_total == 0}No comments <a href="#comment-form">yet</a>&hellip;{if:else}{comment_total} Comments{/if}</h1>
+	            <p>Posted in {categories backspace="2"}<a href="{path='channel/index'}">{category_name}</a>, {/categories}</p>
+	        </header>
+        
+	    </section><!-- /#article-meta --><?php */ ?>	    	    
+	    
+	    <div id="comments-wrap">                
+            
+            <div class="main">
+                
+                <div id="disqus_thread"></div>
+                <script>
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'danielmall'; // required: replace example with your forum shortname
+
+                    // The following are highly recommended additional parameters. Remove the slashes in front to use.
+                    var disqus_identifier = 'accountability';
+                    var disqus_url = 'http://danielmall.com/articles/accountability/';
+                    var disqus_developer = 1;
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+                
+            </div><!-- /.main -->
+	    
+	    
+    	    <div class="sub">
+	        
+    	        <section class="article-info">
+        	        <header>
+        	            <h2>Article Info</h2>
+        	        </header>
+        	        <time datetime="2016-09-19T20:25:00">Published September 19, 2016 at 8:25 <abbr title="Post Meridian">PM</abbr></time>
+    	        </section><!-- /.article-info -->
+    	        
+    	        
+    	        <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/carbon.php"); echo "\n"; ?>
+	        
+	        
+    	    </div><!-- /.sub -->
+    	    
+    	    
+    	</div><!-- #comments-wrap -->
+	    
+	        
+	</div><!-- #comments -->
+	
+	
+	<div class="wrap">
+    	
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>
+    	
+    	
+    </div><!-- #wrap -->
+    
+	
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>
