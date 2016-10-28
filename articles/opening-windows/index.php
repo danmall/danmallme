dba1a129c402a130b6a9a87b2d2e090899f825e1
Dan Mall <dan@superfriend.ly>
2016-10-28 00:12:11 -0400

    Windows post text in

---
+++ articles/opening-windows/index.php
@@ -0,0 +1,168 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head profile="http://gmpg.org/xfn/11">
+    <title>&ldquo;Opening Windows,&rdquo; an article by Dan Mall</title>
+    <meta name="description" content="Opening Windows" />
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>
+    
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/jsReferenceHead.php"); echo "\n"; ?>    
+    <link href='http://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="/articles/_vanilla/vanilla.css" />
+    <?php /* ?><link rel="stylesheet" href="voting-for-trump-is-unbiblical.css" /><?php */ ?>
+
+    <?php /* ?>
+	<!-- Facebook -->
+    <meta property="og:title" content="Oil change &amp; Pizza" />
+    <meta property="og:description" content="Death to &ldquo;full service.&rdquo;"/>	
+    <meta property="og:url" content="http://danielmall.com/articles/oil-change-pizza/" />
+    <meta property="og:image" content="http://danielmall.com/articles/oil-change-pizza/home-thumb.png" />
+    <?php */ ?>
+
+    <!-- Trying @adactio's meta: https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@danielmall" />
+    <meta name="twitter:url" property="og:url" content="http://danielmall.com/articles/opening-windows/" />
+    <meta name="twitter:title" property="og:title" content="Opening Windows" />
+    <meta name="twitter:description" property="og:description" content="Measuring Trump against the Bible&rsquo;s criteria for leaders" />
+    <meta name="twitter:image" property="og:image" content="http://danielmall.com/articles/opening-windows/home-thumb.jpg" />
+    
+</head>
+
+<body id="article" class="articles">	
+	
+	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+	
+	<div id="article-area-wrap">
+			
+	    <section class="clearfix wrap vanilla">
+			
+			<h1 class="article-header-title">Opening Windows</h1>
+
+            <p>The first computer I had in my house growing up was an <abbr title="International Business Machine">IBM</abbr> 386 something-or-other. I think it was an old one my dad&rsquo;s job had that he picked up when they upgraded. It had WordPerfect 3.1 on it, and I became a whiz at all the things you could do at an <a href="https://www.google.com/search?q=msdos+prompt&amp;rlz=1C6CHFA_enUS690US690&amp;biw=1440&amp;bih=776&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;sqi=2&amp;ved=0ahUKEwj_3OTdsfzPAhVF4IMKHeBHCMAQ_AUIBigB">MS-DOS prompt</a>. </p>
+
+            <p>Unlike many designers, I didn&rsquo;t grow up with a Mac. One of my cousins had a Mac, which I thought was weird and inferior to my <abbr title="Personal Computer">PC</abbr>. The first time I really used a Mac was when I went to <a href="http://drexel.edu/westphal/">design school</a> for college. I had my personal <abbr>PC</abbr> in my dorm room and the computers in the labs were all PCs because we were all doing <abbr title="Three-dimensional">3D</abbr> modeling and animation work, but the handful of computers in the photo lab were Macs.</p>
+
+            <p>My work computer at my first job where I was a design intern was a <abbr>PC</abbr>, but for every other job I&rsquo;ve had, a Mac was always sitting on my desk for my first day. I&rsquo;m now pretty entrenched in using Apple products to do my work, but in my history of using computers, Mac usage is actually in the minority.</p>
+
+            <p>A little while ago, my friend <a href="https://twitter.com/aarongustafson">Aaron Gustafson</a> asked me if, like <a href="/articles/day-one-with-android/">my very brief foray with an Android phone</a>, I&rsquo;d be interested and willing to switch to Windows for a while, specifically the new <a href="https://www.microsoft.com/en-us/surface/devices/surface-book/surface-book">Surface Book</a>. I was hesitant at first, mostly because I didn&rsquo;t want to take on anything that would negatively impact <a href="http://superfriend.ly/">my client work</a>, but I&rsquo;m pretty open about trying new hardware <a href="/articles/sketchlive/">and software</a> as I&rsquo;m not terribly tied to tools in particular. I was also closely following and intrigued by Dave Rupert&rsquo;s <a href="https://daverupert.com/2015/04/davegoeswindows/">#davegoeswindows adventures</a>, so I decided I&rsquo;d give it a shot myself.</p>
+
+            <p>I originally asked Aaron and team to replicate <a href="https://www.pressreader.com/australia/net-magazine-the-voice-of-web-design/20161006/281517930598008">my current setup</a> so that I&rsquo;d have the least painful time switching over. My main work machine is a pretty beefed-up iMac with an older external 23&Prime; Dell monitor, Wacom tablet for the right hand, and Apple Magic Trackpad for the left. Along with the Surface Book, they sent me an external monitor, a mouse, and a keyboard, but they asked that I forego the Wacom tablet to give the Surface Book Pen and clipboard/draw modes a run for their money. Fair enough.</p>
+
+            <p>As of this moment, everything is still in the box, but publishing this post is probably one of the last things I&rsquo;ll do on my Mac for a while, if all goes well. I&rsquo;ll be writing about the good, bad, and the ugly of this switch as often as I can.  Initially, there are a few things I&rsquo;m pretty excited about:</p>
+
+            <ul>
+                <li>I&rsquo;ve had the primary desktop machine + laptop as travel machine combo for a while, but I&rsquo;m excited for the excuse to go to one portable laptop that docks as the primary machine. Hoping that&rsquo;ll save me some headaches in the &ldquo;whoops, I forgot to sync that file to Dropbox&rdquo; department.</li>
+                <li>Because of that change, I&rsquo;m also making a few more changes to my workstation, like switching to an adjustable standing desk.</li>
+                <li>Switching to a completely different OS makes me realize how tied I probably am to <abbr title="Operationg system">OS</abbr>-specific software, so I&rsquo;m interested in finding and using more <abbr>OS</abbr>-agnostic software, namely web-based things. I&rsquo;ve been sitting on my <a href="https://www.figma.com/">Figma</a> login forever, so this seems like a good time to give it a whirl as it may help to make this transition more painless.</li>
+                <li>It&rsquo;s been a few years since I&rsquo;ve compared, but I seem to recall Adobe products that I use often (Photoshop, Illustrator, etc) running better on a <abbr>PC</abbr> than they do a Mac.</li>
+                <li>I have a few corporate clients that who all use <abbr>PC</abbr>s internally, so being on a similar environment as them will give me a bit more empathy and also generally help out on testing. </li>
+            </ul>
+
+            <p>Of course, there are also a few things that I&rsquo;m pretty hesitant about:</p>
+
+            <ul>
+                <li>Apple products are easy to use. From what I remember of Windows products and the little browser testing I do in firing up my cheap <abbr title="Hewlett Packard">HP</abbr> testing machine, it&rsquo;s less easy.</li>
+                <li>I&rsquo;m wondering if there are things I take for granted in the iPhone/iPad/MacBook/iMac combo that will suddenly break now that I&rsquo;m on a Windows machine.</li>
+                <li>Most of my worries are software-centric. There are a bunch of Mac-specific apps for which I don&rsquo;t know if there&rsquo;s a <abbr>PC</abbr> equivalent. What do I do with my <a href="https://www.sketchapp.com/">Sketch</a> files? <a href="http://mizage.com/shush/">Shush</a> is probably my most-used Mac app; is there something similar for Windows?</li>
+            </ul>
+
+            <p>We&rsquo;ll see how it all goes. If I can get through a few months on a Windows laptop, I think my ideal scenario would be to have an iMac and a Surface Book as my two primary machines. I quite like the idea of having a seamless enough setup that can allow me to bounce back and forth across different operating systems with minimal fuss. But, at this point, I have no idea how realistic that is.</p>
+
+            <p>If you&rsquo;re curious about how this is going, <a href="http://twitter.com/danielmall">tweet at me</a>, and I&rsquo;ll also try to post as many updates as I can here. I&rsquo;ll probably use a hashtag to collect all of this, but I can&rsquo;t come up with a clever one right now; suggestions are welcome.</p>
+
+            <p>Time for the unboxing. </p>
+
+
+
+
+			
+			
+            
+            <footer class="footnote">
+
+              <p>Also <a href="https://medium.com/@danielmall/voting-for-trump-is-unbiblical-b287966f7a9c#.6n11zgly8">syndicated to Medium</a>.</p>
+            
+            </footer><!-- .footnote -->
+            
+			
+	    </section><!-- .vanilla -->	  	
+	    
+	</div><!-- /#article-area-wrap -->	
+
+	<?php
+
+        echo readNext('Voting for Trump is Unbiblical', '/articles/voting-for-trump-is-unbiblical//');
+
+    ?>
+	
+	<div id="comments">
+	
+	    <?php /* ?><section id="article-meta">
+
+	        <header class="easy-clearing">
+	            <h1>{if comment_total == 0}No comments <a href="#comment-form">yet</a>&hellip;{if:else}{comment_total} Comments{/if}</h1>
+	            <p>Posted in {categories backspace="2"}<a href="{path='channel/index'}">{category_name}</a>, {/categories}</p>
+	        </header>
+        
+	    </section><!-- /#article-meta --><?php */ ?>	    	    
+	    
+	    <div id="comments-wrap">                
+            
+            <div class="main">
+                
+                <div id="disqus_thread"></div>
+                <script>
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'danielmall'; // required: replace example with your forum shortname
+
+                    // The following are highly recommended additional parameters. Remove the slashes in front to use.
+                    var disqus_identifier = 'opening-windows';
+                    var disqus_url = 'http://danielmall.com/articles/opening-windows/';
+                    var disqus_developer = 1;
+
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+                
+            </div><!-- /.main -->
+	    
+	    
+    	    <div class="sub">
+	        
+    	        <section class="article-info">
+        	        <header>
+        	            <h2>Article Info</h2>
+        	        </header>
+        	        <time datetime="2016-10-09T17:30:00">Published October 9, 2016 at 5:30 <abbr title="Post Meridian">PM</abbr></time>
+    	        </section><!-- /.article-info -->
+    	        
+    	        
+    	        <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/carbon.php"); echo "\n"; ?>
+	        
+	        
+    	    </div><!-- /.sub -->
+    	    
+    	    
+    	</div><!-- #comments-wrap -->
+	    
+	        
+	</div><!-- #comments -->
+	
+	
+	<div class="wrap">
+    	
+    	<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>
+    	
+    	
+    </div><!-- #wrap -->
+    
+	
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>
