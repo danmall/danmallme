2465cb850d29ea61a6a09eec02f0dfe21481aa7c
Dan Mall <dan@superfriend.ly>
2016-12-29 22:57:24 -0500

    upcoming events now added to speaking page

--- -/_inc/functions.php
+++ -/_inc/functions.php
@@ -42,6 +42,16 @@ function parseList($file, $num, $type, $displayStyle){
             }
         }
 
+        // coming up badge
+        if(!empty($articles[$i]['date'])){
+            $date_now = new DateTime();
+            $date2    = new DateTime($articles[$i]['date']);
+
+            if ($date_now < $date2) {
+                echo '<div class="dm-dp-textBlurb_badge">Coming Up</div>';
+            }
+        }
+
         // create article header
         echo '<header class="dm-dp-textBlurb_header">' . "\n\t\t\t";
 
