f5fd70557f583d1f269fada93020f058947e3717
DANIEL MALL <danielmall@dan.local>
2017-01-02 07:08:36 -0500

    Starting contact page

---
+++ contact/index.php
@@ -0,0 +1,67 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>Contact Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="http://<?php echo $_SERVER['SERVER_NAME']; ?>/contact/" />
+    <meta name="twitter:title" property="og:title" content="Contact Dan Mall" />
+    <meta name="twitter:description" property="og:description" content="Contact Dan Mall" />
+    <meta name="twitter:image" property="og:image" content="http://<?php echo $_SERVER['SERVER_NAME']; ?>/contact/thumb.png" />
+
+</head>
+
+
+<body class="dm-page--articles">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <header class="dm-c-pageHeader" role="banner">
+
+        <h1 class="dm-c-pageHeader_title">
+            Contact Dan Mall
+        </h1>
+
+        <p>The best ways to get in touch with me are to send me a direct message on Twitter or to email dan@danmall.me.</p>
+
+        <?php /* ?>
+        <nav role="navigation">
+            <ul class="dm-dp-inlineNav dm-u-font--sans dm-u-text--s">
+                <li class="dm-dp-inlineNav_item dm-dp-inlineNav_item--active">
+                    <mark>On this site</mark>
+                </li>
+                <li class="dm-dp-inlineNav_item">
+                    <a href="#elsewhere">Elsewhere</a>
+                </li>
+            </ul>
+        </nav><!-- .page-nav -->
+        <?php */ ?>
+
+    </header><!-- .page-header -->
+
+    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+
+    <!-- TODO: world map -->
+
+    <main role="main" class="dm-c-articlesList dm-u-padding--m">
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </main>
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
