d9f3bd15ddbf7da23c55a0e1fea8b57e34eb9ac1
Dan Mall <dan@superfriend.ly>
2017-12-23 05:19:32 -0500

    Finished first draft of scoping article

--- articles/how-to-scope-work/index.php
+++ articles/how-to-scope-work/index.php
@@ -46,7 +46,7 @@
 
         <div class="dm-dp-textPassage">
 
-            <p><span class="dm-dp-openingLines">After four days</span> of working together, she finally admitted, &ldquo;No one ever taught me how to scope.&rdquo; I was working with one of my client&rsquo;s senior developers to map out exactly how we were going to get all of the work done that we needed, but a few frustrating hours of going in circles finally revealed the culprit. It&rsquo;s a problem I&rsquo;ve seen from very junior to some of the most senior employees I&rsquo;ve worked with.</p>
+            <p><span class="dm-dp-openingLines">After four days</span> of working together, she finally admitted, &ldquo;No one ever taught me how to scope.&rdquo; I was working with one of my clients&rsquo; senior developers to map out exactly how we were going to get all of the work done that we needed, but a few frustrating hours of going in circles finally revealed the culprit. It&rsquo;s a problem I&rsquo;ve seen from very junior to some of the most senior employees I&rsquo;ve worked with.</p>
 
             <p>Simply put, I like to define <dfn>scope</dfn> as:</p>
             <ol>
@@ -58,7 +58,203 @@
 
             <p>Good scoping is often a case of simple arithmetic; by the time you were 8 or 9 years old, you&rsquo;ve had the skills to scope well. So why is it so difficult? You&rsquo;re mostly fighting the psychology of it all.</p>
 
-            <p>If I asked you if you could build a website (of any given complexity) in 6 months, I&rsquo;d bet your answer would be, &ldquo;Probably.&rdquo; That&rsquo;s the <a href="https://en.wikipedia.org/wiki/Valence_effect">valence effect</a> at play: your tendency to think that more good will happen to you than bad. Throw a little <a href="https://en.wikipedia.org/wiki/Parkinson%27s_law">Parkinson&rsquo;s law</a> in there (the adage that work expands to fill the time available for its completion) and that explains why you&rsquo;ve taken way longer on every project than you should have had&mdash;and felt good about it to boot. The reality is closer to <a href="https://en.wikipedia.org/wiki/Hofstadter%27s_law">Hofstadter&rsquo;s law</a>: it always takes longer than you expect (even when you take into account Hofstadter&rsquo;s law). Rather than relying on gut feelings, let&rsquo;s use math to figure out if you&rsquo;ll meet your deadlines.</p>
+            <p>If I asked you if you could build a website (of any given complexity) in 6 months, I&rsquo;d bet your answer would be, &ldquo;Probably.&rdquo; That&rsquo;s the <a href="https://en.wikipedia.org/wiki/Valence_effect">valence effect</a> at play: your tendency to think that more good will happen to you than bad. Throw a little <a href="https://en.wikipedia.org/wiki/Parkinson%27s_law">Parkinson&rsquo;s law</a> in there (the adage that work expands to fill the time available for its completion) and that explains why you&rsquo;ve taken way longer on every project than you should have had&mdash;and felt good about it to boot. The reality is closer to <a href="https://en.wikipedia.org/wiki/Hofstadter%27s_law">Hofstadter&rsquo;s law</a>: it always takes longer than you expect (even when you take into account Hofstadter&rsquo;s law). Rather than relying on gut feelings, we can use math to figure out if you&rsquo;ll meet your deadlines.</p>
+
+            <h2 id="budgeting">Budgeting time <a class="dm-dp-anchorLink" href="#budgeting-time">#</a></h2>
+
+            <p>If you&rsquo;re like most people (and like me), it&rsquo;s difficult to save money. Let&rsquo;s say you&rsquo;re trying to save $6,000 for a vacation next year. That&rsquo;s a daunting task at first glance, but it becomes easier when you break it down. Instead of looking at it as $6k, look at it as $500/month ($6,000 &divide; 12 months). Or maybe it&rsquo;s $125/week ($500 &divide; 4 weeks). Or $25/day ($125 &divide; 5 days). When you look at it that way, for every day you go to work, skip your Starbucks coffee, pack a lunch, throw that $25 in a jar, and watch that vacation happen in no time. By breaking it down into digestible chunks, it&rsquo;s much more manageable!</p>
+
+            <p>If it helps, think of scoping like a budget for your time.</p>
+
+            <h2 id="building-in-5-days">Building a website in 5 days <a class="dm-dp-anchorLink" href="#building-in-5-days">#</a></h2>
+
+            <p>Let&rsquo;s take the simple example from earlier: building a website in 5 days. If it&rsquo;s a 5-page website, our basic math skills (5 pages &divide; 5 days) tell us that we need to average building about 1 page per day. So far, our workload would look like this:</p>
+
+            <table class="dm-u-marginTop--l">
+                <thead>
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th scope="col" class="dm-u-font--bold">Workload</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row" class="dm-u-align--left dm-u-font--bold">Monday</th>
+                        <td class="dm-u-align--center">1 page</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="dm-u-align--left dm-u-font--bold">Tuesday</th>
+                        <td class="dm-u-align--center">1 page</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="dm-u-align--left dm-u-font--bold">Wednesday</th>
+                        <td class="dm-u-align--center">1 page</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="dm-u-align--left dm-u-font--bold">Thursday</th>
+                        <td class="dm-u-align--center">1 page</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="dm-u-align--left dm-u-font--bold">Friday</th>
+                        <td class="dm-u-align--center">1 page</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <p>But we can break that down even further, because building a page has three components to it: <abbr title="HyperText Marktup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, and JavaScript. So, let&rsquo;s divide a typical 9am&ndash;5pm workday into four parts (because lunch is incredibly important too, kids):</p>
+
+            <table class="dm-u-marginTop--l">
+                <tbody>
+                    <tr>
+                        <th rowspan="5" scope="row" class="dm-u-align--left dm-u-font--bold">Monday</th>
+                        <td colspan="2" class="dm-u-align--center">Page 1</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">HTML</td>
+                        <td class="dm-u-align--center">9:00am&ndash;11:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">CSS</td>
+                        <td class="dm-u-align--center">11:00am&ndash;1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Lunch</td>
+                        <td class="dm-u-align--center">1:00pm&ndash;2:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">JavaScript</td>
+                        <td class="dm-u-align--center">2:00pm&ndash;5:00am</td>
+                    </tr>
+                    <tr>
+                        <th rowspan="5" scope="row" class="dm-u-align--left dm-u-font--bold">Tuesday</th>
+                        <td colspan="2" class="dm-u-align--center">Page 2</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">HTML</td>
+                        <td class="dm-u-align--center">9:00am&ndash;11:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">CSS</td>
+                        <td class="dm-u-align--center">11:00am&ndash;1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Lunch</td>
+                        <td class="dm-u-align--center">1:00pm&ndash;2:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">JavaScript</td>
+                        <td class="dm-u-align--center">2:00pm&ndash;5:00am</td>
+                    </tr>
+                    <tr>
+                        <th rowspan="5" scope="row" class="dm-u-align--left dm-u-font--bold">Wednesday</th>
+                        <td colspan="2" class="dm-u-align--center">Page 3</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">HTML</td>
+                        <td class="dm-u-align--center">9:00am&ndash;11:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">CSS</td>
+                        <td class="dm-u-align--center">11:00am&ndash;1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Lunch</td>
+                        <td class="dm-u-align--center">1:00pm&ndash;2:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">JavaScript</td>
+                        <td class="dm-u-align--center">2:00pm&ndash;5:00am</td>
+                    </tr>
+                    <tr>
+                        <th rowspan="5" scope="row" class="dm-u-align--left dm-u-font--bold">Thursday</th>
+                        <td colspan="2" class="dm-u-align--center">Page 4</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">HTML</td>
+                        <td class="dm-u-align--center">9:00am&ndash;11:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">CSS</td>
+                        <td class="dm-u-align--center">11:00am&ndash;1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Lunch</td>
+                        <td class="dm-u-align--center">1:00pm&ndash;2:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">JavaScript</td>
+                        <td class="dm-u-align--center">2:00pm&ndash;5:00am</td>
+                    </tr>
+                    <tr>
+                        <th rowspan="5" scope="row" class="dm-u-align--left dm-u-font--bold">Friday</th>
+                        <td colspan="2" class="dm-u-align--center">Page 5</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">HTML</td>
+                        <td class="dm-u-align--center">9:00am&ndash;11:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">CSS</td>
+                        <td class="dm-u-align--center">11:00am&ndash;1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Lunch</td>
+                        <td class="dm-u-align--center">1:00pm&ndash;2:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">JavaScript</td>
+                        <td class="dm-u-align--center">2:00pm&ndash;5:00am</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <p>This is much better! The specificity here can help to assess if this is a realistic plan. But we can break it down even further.</p>
+
+            <p>Let&rsquo;s say the first page has 4 different sections. We can average the time to write the markup for each section, for example:</p>
+
+            <table class="dm-u-marginTop--l">
+                <tbody>
+                    <tr>
+                        <th rowspan="9" scope="row" class="dm-u-align--left dm-u-font--bold">Monday</th>
+                        <td colspan="3" class="dm-u-align--center">Page 1</td>
+                    </tr>
+                    <tr>
+                        <td rowspan="4" class="dm-u-align--center">HTML</td>
+                        <td class="dm-u-align--center">Section 1</td>
+                        <td class="dm-u-align--center">9:00am&ndash;9:30am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Section 2</td>
+                        <td class="dm-u-align--center">9:30am&ndash;10:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Section 3</td>
+                        <td class="dm-u-align--center">10:00am&ndash;10:30am</td>
+                    </tr>
+                    <tr>
+                        <td class="dm-u-align--center">Section 4</td>
+                        <td class="dm-u-align--center">10:30am&ndash;11:00am</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="dm-u-align--center">CSS</td>
+                        <td class="dm-u-align--center">11:00am&ndash;1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="dm-u-align--center">Lunch</td>
+                        <td class="dm-u-align--center">1:00pm&ndash;2:00pm</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="dm-u-align--center">JavaScript</td>
+                        <td class="dm-u-align--center">2:00pm&ndash;5:00am</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <p>You can continue to break down each task further if it&rsquo;s useful to do so, but, even at this level, you can realistically assess the work. Can you write a section of <abbr>HTML</abbr> in 30 minutes? If yes, great! You&rsquo;re on schedule and will make your timeline. If not, what can you adjust or reorder to make it work?</p>
+
+            <p>Herein lies the real beauty of this technique: <strong>you&rsquo;ll know when you&rsquo;re in danger of missing a deadline days, weeks, or even months in advance.</strong> For example, if you haven&rsquo;t completed Section 3 of Page 1&rsquo;s <abbr>HTML</abbr> by Monday at 10:30am, you&rsquo;re going to miss your deadline. Sure, it&rsquo;s possible that you may make up the time later on, but if you&rsquo;ve estimated realistically and remember Hofstadter&rsquo;s law, now&mdash;4 days in advance&mdash;is a good time to ask your client or manager for an extra day, just in case.</p>
+
+            <p>Scoping is about appropriately setting expectations for yourself, your team, and your clients and managers. It&rsquo;s a rare skill that separates the amateurs from the professionals. Which one are you?</p>
 
         </div>
 
@@ -67,8 +263,8 @@
             <h1 class="dm-c-upNext_kicker">Up Next</h1>
 
             <h2 class="dm-c-upNext_title">
-                <a href="/articles/design-systems-pilots-scorecards/">
-                    Design Systems: Pilots &amp; <span class="dm-c-upNext_title_lastWord">Scorecards</span>
+                <a href="/articles/stealing-your-way-to-original-designs/">
+                    Stealing Your Way to Original <span class="dm-c-upNext_title_lastWord">Designs</span>
                 </a>
             </h2>
 
