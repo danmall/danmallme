b75cbc113d56d1c8999d97e3fd04e5cf0ce669fc
Kevin Deal <kevindeal@Kevins-MacBook-Pro.local>
2018-03-13 14:54:04 -0400

    updates

--- -/_inc/footer.php
+++ -/_inc/footer.php
@@ -4,6 +4,7 @@
 		<p>&copy; 2005 &ndash; <?php echo date('Y'); ?> Dan Mall, a Philly jawn.Thou shalt not <a href="/articles/stealing-your-way-to-original-designs/">steal</a>.</p>
 		<p class="">Powered by Hand Coding&trade;. Check out this site&rsquo;s code on <a href="https://github.com/danmall/danielmallcom">Github</a>.</p>
 		<p class="">Graciously hosted by <a href="http://www.mediatemple.net/go/order/?refdom=danielmall.com">Media Temple</a></p>
+		
 		<?php /* ?><p class=""><a href="http://feeds.feedburner.com/danielmall-articles">RSS</a></p><?php */ ?>
 	</div>
 
@@ -14,4 +15,9 @@
 		<script src="http://platform.twitter.com/widgets.js"></script>
 	</div>
 	
+	<div class="dm-c-siteFooter_github">
+
+		<p>Last updated: Mar 13, <?php echo date('Y'); ?>
+
+	</div>
 </footer>
