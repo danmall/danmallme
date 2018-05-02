693eb881aa433cc7e93aed96496498f252c74d80
Dan Mall <dan@superfriend.ly>
2018-05-02 06:08:56 -0700

    Onlyness statement article done and ready for publication

---
+++ articles/onlyness/index.php
@@ -0,0 +1,169 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;The Only-ness Statement,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="The Only-ness Statement" />
+    <meta name="twitter:description" property="og:description" content="A simple brand exercises that speaks volumes." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/thumb.png" />
+
+    <style type="text/css">
+
+        .dm-c-madLib {
+            min-width: 230px;
+            padding: 0.5em;
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <header class="dm-c-pageHeader" role="banner">
+
+        <h1 class="dm-c-pageHeader_title">
+            The Only-ness Statement
+        </h1>
+
+        <h2 class="dm-c-pageHeader_date">May 2, 2018 at 5:55 <abbr title="Ante Meridien">AM</abbr></h2>
+
+    </header><!-- .page-header -->
+
+    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+
+    <main role="main" class="dm-c-articleWell">
+
+        <div class="dm-dp-textPassage">
+
+            <p><span class="dm-dp-openingLines">Creating an Only-ness Statement</span> is one of my favorite exercises to do with new clients. I first came across it in my friend <a href="https://twitter.com/jpostendorf">J&auml;n Paul Ostendorf</a>&rsquo;s <a href="https://issuu.com/forge5/docs/forge_brandworkbook-40pg">Brand Formation Workbook</a>, who found it in <a href="http://www.martyneumeier.com/">Marty Neumeier</a>&rsquo;s book <a href="https://amzn.to/2HGJIej">Zag</a>.</p>
+
+            <p>It&rsquo;s a simple premise: every brand on the planet should be able to say what it&rsquo;s the only one of. Marty suggests this simple format:</p>
+
+            <p class="dm-u-textIndent--0 dm-u-marginTop--l dm-u-paddingBottom--l">
+                What:<br />
+                How:<br />
+                Who:<br />
+                Where:<br />
+                Why:<br />
+                When:<br />
+            </p>
+
+            <p>Here&rsquo;s an example. Harley-Davidson is:</p>
+
+            <table class="dm-u-marginTop--l">
+                <tr>
+                    <td class="dm-u-align--left">What</td>
+                    <td class="dm-u-align--left">The only motorcycle manufacturer</td>
+                </tr>
+                <tr>
+                    <td class="dm-u-align--left">How</td>
+                    <td class="dm-u-align--left">that makes big, loud motorcycles</td>
+                </tr>
+                <tr>
+                    <td class="dm-u-align--left">Who</td>
+                    <td class="dm-u-align--left">for macho guys (and macho wannabes)</td>
+                </tr>
+                <tr>
+                    <td class="dm-u-align--left">Where</td>
+                    <td class="dm-u-align--left">mostly in the United States</td>
+                </tr>
+                <tr>
+                    <td class="dm-u-align--left">Why</td>
+                    <td class="dm-u-align--left">who want to join a gang of cowboys </td>
+                </tr>
+                <tr>
+                    <td class="dm-u-align--left">When</td>
+                    <td class="dm-u-align--left">in an era of decreasing personal freedom.</td>
+                </tr>
+            </table>
+
+            <p>A great test of the only-ness statement is to put a competitor&rsquo;s name in place and see if it still fits. Slotting in Triumph Motorcycles or Suzuki or Yamaha makes this statement awkward at best and inaccurate at worst; therefore, it&rsquo;s a worthy statement for Harley-Davidson.</p>
+
+            <p>Jan suggests a slightly different framing&mdash;a sort-of mad libs style&mdash;which I find a little easier to use:</p>
+
+            <p class="dm-u-marginTop--l dm-u-textIndent--0 dm-u-paddingBottom--l dm-u-text--m dm-u-fontStyle--italic">&ldquo;This is the only <input class="dm-c-madLib" type="text" placeholder="Type of digital experience" /> that <input class="dm-c-madLib" type="text" placeholder="Unique service or product you provide" /> for <input class="dm-c-madLib" type="text" placeholder="Target audience" /> in <input class="dm-c-madLib" type="text" placeholder="Geographic region or market segment" /> who want to <input class="dm-c-madLib" type="text" placeholder="Customer&rsquo;s emotional motivation" /> during a time of <input class="dm-c-madLib" type="text" placeholder="Increasing or decreasing cultural trend" />.&rdquo;</p>
+
+            <p>I&rsquo;ll often use this exercise in workshops with large groups. The format is similar to a <a href="http://goodkickoffmeetings.com/2010/04/design-studioprototyping-exercise/">design studio</a> setup. I&rsquo;ll start by giving each person 2 worksheets to fill out over 8 minutes. Once the 8 minutes are up, I&rsquo;ll pair people together and ask them to create one worksheet between the two of them. That could mean using whichever one version is best, or maybe the pair creates a brand new one with the best of each of their ideas. Groups of two get combined into groups of four people, then eight&hellip; you get the idea. The ultimate output is two or three versions that conceivably reflect the best or most rallying ideas in the room. For example, <a href="https://bigmedium.com/projects/about-verywell-thebalance.html">when we worked with Dotdash</a> to create new home and lifestyle site <a href="https://www.thespruce.com/">The Spruce</a>, here were the three final only-ness statements that we used to drive our <a href="http://v3.danielmall.com/articles/on-creative-direction/">creative direction</a> for the whole project:</p>
+
+            <blockquote><p class="dm-u-text--m">This is the only <strong>home and food website</strong> that <strong>simplifies and empowers DIYers and aspiring superheroes</strong> who want to <strong>conquer their to-do list and feel proud</strong> during a time of <strong>perceived and unachievable perfection</strong>.</p></blockquote>
+
+            <blockquote><p class="dm-u-text--m">This is the only <strong>immersive home life site</strong> that <strong>gives you what you came for and more</strong> for <strong>people like you</strong> who want to <strong>feel good about what you come home</strong> to during a time of <strong>increasing choice and decreasing bandwidth</strong>.</p></blockquote>
+
+            <blockquote><p class="dm-u-text--m">This is the only <strong>lifestyle brand</strong> that <strong>gives practical, real-world advice and ideas</strong> for <strong>the roll-up-your-sleeves crowd and those looking for inspiration</strong> who want to <strong>reinvigorate their home life with small victories</strong> during a time when <strong>food and home trends feel unattainable.</strong></p></blockquote>
+
+            <p>You can simultaneously see different themes emerging in each, but there are also more than a few common threads among them too.</p>
+
+            <p>Next time you start work with a new client&mdash;or heck, maybe you use your own business or employer as a test&mdash;feel free to give this a shot to see if it gives you a new perspective. Good luck!</p>
+
+        </div>
+
+        <section id="up-next" class="dm-c-upNext">
+
+            <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+            <h2 class="dm-c-upNext_title">
+                <a href="/articles/how-to-scope-work/">
+                    How to Scope <span class="dm-c-upNext_title_lastWord">Work</span>
+                </a>
+            </h2>
+
+        </section><!-- .dm-c-upNext -->
+
+    </main>
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
