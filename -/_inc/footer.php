e7727cc9b67bb0867251fd754984e5e77c212200
Dan Mall <dan@superfriend.ly>
2018-09-14 18:33:14 -0400

    Simplifying RSS structure

--- -/_inc/footer.php
+++ -/_inc/footer.php
@@ -3,7 +3,7 @@
 	<div class="dm-c-siteFooter_copyright">
 		<p>&copy; 2005 &ndash; <?php echo date('Y'); ?> Dan Mall, a Philly jawn.Thou shalt not <a href="/articles/stealing-your-way-to-original-designs/">steal</a>.</p>
 		<p>Powered by Hand Coding&trade;. Check out this site&rsquo;s code on <a href="https://github.com/danmall/danmallme" rel="me">Github</a>.</p>
-		<p>RSS: <a href="/rss/full/">Full posts</a>. <a href="/rss/excerpts/">Excerpts</a>.</p>
+		<p>RSS: <a href="/rss/full.xml">Full posts</a>. <a href="/rss/">Excerpts</a>.</p>
 		<p>Graciously hosted by <a href="http://www.mediatemple.net/go/order/?refdom=danielmall.com">Media Temple</a></p>
 		<?php /* ?><p class=""><a href="http://feeds.feedburner.com/danielmall-articles">RSS</a></p><?php */ ?>
 	</div>
