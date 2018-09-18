212d9cfaecf227752d41f53ef94d56d9048a57fa
Dan Mall <dan@superfriend.ly>
2018-09-18 09:37:10 -0400

    Shop Talk Show article ready to publish

---
+++ articles/redesign-shop-talk-show/index.php
@@ -0,0 +1,196 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Work With Me to Redesign the Shop Talk Show Website,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Work With Me to Redesign the Shop Talk Show Website" />
+    <meta name="twitter:description" property="og:description" content="An opportunity for someone who might not have it otherwise." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <!-- Webmentions -->
+    <link rel="pingback" href="https://webmention.io/webmention?forward=<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+
+    <style type="text/css">
+
+        body {
+            /*--articlePrimaryColor: blue;
+            --articlePrimaryColorHover: #e5e6fd;*/
+        }
+
+    </style>
+
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <figure class="dm-c-imageObject">
+        <div class="dm-c-imageObject_imageWrap" style="background: #fff;">
+            <script>
+            document.write('<img class="dm-c-imageObject_image dm-js-lazy" width="1200" height="400" data-src="shoptalkshow--mosaic2.png" alt="A cryptic Shop Talk Show website" />');
+            </script>
+            <noscript>
+                <img class="dm-c-imageObject_image" width="1200" height="400" src="shoptalkshow--mosaic2.png" alt="A cryptic Shop Talk Show website" />
+            </noscript>
+        </div><!-- .dm-c-imageObject_imageWrap -->            
+    </figure><!-- .dm-c-imageObject -->
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-c-pageHeader--withImage dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Work With Me to Redesign the Shop Talk Show&nbsp;Website
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">September 18, 2018 at 09:21 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->        
+    
+        <main role="main" class="dm-c-articleWell">        
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <p>
+                    <span class="dm-dp-openingLines"><b class="dm-dp-dropCap">O</b>n <a href="https://shoptalkshow.com/episodes/329-dan-mall-building-portfolio/">Episode 329</a></span> of <a href="https://shoptalkshow.com/">Shop Talk Show</a>, I joined <a href="https://chriscoyier.net/">Chris Coyier</a> and <a href="https://daverupert.com/">Dave Rupert</a> to talk about the role <a href="/articles/portfolios/">portfolios</a> have in getting a ful-time job, and how you can increase your odds of getting a job. We talked about many things, one of which was the all-time popular objection: &ldquo;But I don&rsquo;t have anything good to put in my portfolio!&rdquo; So, we&rsquo;re putting our money where our mouth is and looking for an up-and-coming new designer to work with me to redesign the Shop Talk Show website!
+                </p>
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <p>Here&rsquo;s the deal: I&rsquo;ll <a href="http://v3.danielmall.com/articles/on-creative-direction/">creative direct</a> it, you design it. That means I&rsquo;ll be providing oversight, ideas, inspiration, and guidance while you do the actual pixel-pushing in Sketch/Photoshop/Figma/whatever to make the screens we&rsquo;ll all need. Chris and Dave act as clients, but will also be coding what we come up with.</p>
+
+                <p>We want this to be an opportunity for a designer who might have have something like it otherwise. If you have lots of portfolio pieces, this probably isn&rsquo;t the gig for you. If you work full-time as a designer somewhere, this probably isn&rsquo;t the gig for you.</p>
+
+                <p>Some criteria for our ideal designer:</p>
+
+                <ul>                    
+                    <li>You&rsquo;re in an underrepresented group in this field.</li>
+                    <li>You&rsquo;re looking to change careers from whatever you&rsquo;re currently doing into becoming a full-time designer.</li>
+                    <li>You need an opportunity like this project to break into the field, because you&rsquo;ve had a difficult time getting someone to hire you to do design work for them.</li>
+                    <li>You have some tolerance for hard work and transparency. Chris, Dave, and I all have an affinity for <a href="https://chriscoyier.net/2012/09/23/working-in-public/">working in public</a>, so that&rsquo;ll likely be some part of this. Be prepared for some amount of live-streaming conversations, posting work before it&rsquo;s done, and being able to publicly track the progress of this work.</li>
+                    <li>You&rsquo;re a Shop Talk Show listener (at have least listened to an episode or two).</li>
+                </ul>
+
+                <p>Still here? Great! We&rsquo;re taking applications for this opportunity until about October 1, at which time we&rsquo;ll sort through the applications to see if there&rsquo;s a good fit. Without further ado&hellip;</p>
+
+                <section class="dm-dp-asteriskSplit">
+
+                    <p class="dm-u-align--center"><strong><a href="http://shoptalkshow.superfriend.ly/">Apply here!</a></strong></p>
+
+                </section><!-- .dm-dp-asteriskSplit -->
+
+                <section class="dm-dp-asteriskSplit">
+
+                    <p>Chris, Dave, and I would love any comments or feedback you have about how we can make this better for everyone involved (including everyone sitting on the sidelines, watching all this unfold). If you have any questions, <a href="https://twitter.com/danmall">tweet at me</a>, either publicly or privately.</p>
+
+                    <p>Let&rsquo;s make something great!</p>
+
+                </section><!-- .dm-dp-asteriskSplit -->
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/portfolios/">
+                        A Portfolio Hiring Managers Can&rsquo;t <span class="dm-c-upNext_title_lastWord">Deny</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+
+            <!-- ====== WEBMENTIONS ======= -->
+            <!-- https://gist.github.com/aaronpk/6207445 -->
+
+            <span id="webmention-count" style="display: none;"></span>
+
+            <?php 
+                /*
+                echo '<div style="display: none;">';
+                $json = file_get_contents('https://webmention.io/api/links.jf2?target=http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
+                $obj = json_decode($json);
+                //echo $obj;
+                echo '</div>';
+                */
+            ?>
+            
+
+            <script type="text/javascript">
+                function loadWebmentions(data){
+                    var webmentionCount = document.querySelector('#webmention-count');
+                    if(webmentionCount){
+                        webmentionCount.innerHTML = data.count + " mentions";
+                    }
+                    //document.getElementById("webmention-count").innerHTML = data.count + " mentions";
+                }
+            </script>
+            <script type="text/javascript" src="http://webmention.io/api/count?jsonp=loadWebmentions&amp;target=<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>"></script>
+
+
+
+            
+            <!-- ====== DISQUS ======= -->
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
