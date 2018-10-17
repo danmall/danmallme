9eb5c0309cd07b4beadd79be1a785a8220d20565
Dan Mall <dan@superfriend.ly>
2018-10-17 14:32:37 -0400

    Fixing metadata for Speaking page

--- speaking-podcasts/index.php
+++ speaking-podcasts/index.php
@@ -8,7 +8,7 @@
     <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@danmall" />
-    <meta name="twitter:url" property="og:url" content="http://<?php echo $_SERVER['SERVER_NAME']; ?>/articles/" />
+    <meta name="twitter:url" property="og:url" content="http://<?php echo $_SERVER['SERVER_NAME']; ?>/speaking-podcasts/" />
     <meta name="twitter:title" property="og:title" content="Dan Mall" />
     <meta name="twitter:description" property="og:description" content="Speaking Schedule, Speaking History, and Podcasts" />
     <meta name="twitter:image" property="og:image" content="http://<?php echo $_SERVER['SERVER_NAME']; ?>/i/dan-headshot.jpg" />
