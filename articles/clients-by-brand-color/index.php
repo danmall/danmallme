cefa1a98e342c28e5034f14c5ebbc357770f73df
Dan Mall <dan@superfriend.ly>
2018-12-18 09:24:04 -0500

    Clients by Brand Color post done

---
+++ articles/clients-by-brand-color/index.php
@@ -0,0 +1,155 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Clients by Brand Color,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Clients by Brand Color" />
+    <meta name="twitter:description" property="og:description" content="Celebrating clients over the years." />
+    <meta name="description" content="Celebrating clients over the years." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/thumb.png" />
+    <style type="text/css">
+        td {
+            text-align: center;
+        }
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Clients by Brand Color
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">December 18, 2018 at 9:25 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <p><span class="dm-dp-openingLines">As I&rsquo;m getting ready</span> to close up shop for the year and recharge for 2019, I&rsquo;m in a reflective mood. Every year, I&rsquo;m incredibly privileged to work with clients that pay me to do something I enjoy, and this year was no exception.</p>
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <p>After <a href="http://superfriend.ly/">SuperFriendly</a>&rsquo;s first year of business in 2012, I published <a href="http://v3.danielmall.com/articles/superfriendly-annual-report-2012/">an annual report</a> that looked at workload, clients, and tasks at a high level. Visualizing data in aggregate like this has always helped me be intentional what I&rsquo;m currently doing and what I might want to change moving forward.</p>
+
+                <p>2019 will mark year seven for SuperFriendly. As a <a href="https://www.adventist.org/en/">Seventh-day Adventist</a>, I&rsquo;m a big believer in the value of cycles of seven, which often bring completeness and wholeness. It seems like an appropriate time for some more reflection.</p>
+
+                <p>Here&rsquo;s a rough breakdown of the distribution of work per year. Clients are represented by their main brand color (at the time of working with them) and roughly sequenced by where the work occurred during the year.</p>
+                
+                <h2 class="dm-u-text--m dm-u-marginBottom--xs">2018</h2>    
+                <img class="dm-u-marginBottom--xl" src="2018.svg" alt="SuperFriendly client work for 2018" /> 
+
+                <h2 class="dm-u-text--m dm-u-marginBottom--xs">2017</h2>    
+                <img class="dm-u-marginBottom--xl" src="2017.svg" alt="SuperFriendly client work for 2017" /> 
+
+                <h2 class="dm-u-text--m dm-u-marginBottom--xs">2016</h2>    
+                <img class="dm-u-marginBottom--xl" src="2016.svg" alt="SuperFriendly client work for 2016" /> 
+
+                <h2 class="dm-u-text--m dm-u-marginBottom--xs">2015</h2>    
+                <img class="dm-u-marginBottom--xl" src="2015.svg" alt="SuperFriendly client work for 2015" /> 
+
+                <h2 class="dm-u-text--m dm-u-marginBottom--xs">2014</h2>    
+                <img class="dm-u-marginBottom--xl" src="2014.svg" alt="SuperFriendly client work for 2014" /> 
+
+                <h2 class="dm-u-text--m dm-u-marginBottom--xs">2013</h2>    
+                <img class="dm-u-marginBottom--xl" src="2013.svg" alt="SuperFriendly client work for 2013" /> 
+
+                <h2 class="dm-u-text--m dm-u-marginBottom--xs">2012</h2>    
+                <img class="dm-u-marginBottom--xl" src="2012.svg" alt="SuperFriendly client work for 2012" />
+
+                <p>A few things I noticed when doing this exercise:</p>
+
+                <ul>
+                    <li>I&rsquo;ve long followed the mantra that no single client should be responsible for 50% of revenue, but I also apply that in terms of attention. I&rsquo;m pleased to say that that&rsquo;s held through since day 1. The closest we&rsquo;ve gotten (in terms of attention, not revenue) are a 38% client in 2012, a 32% clients in 2013, and a 37% client in 2015.</li>
+                    <li>The number of clients per year is slowly dwindling, which I&rsquo;m happy about. We&rsquo;re able to do better work for a smaller number of clients with revenue and profit still increasing. We had 17 clients in 2012, while 2018 wrapped up with 11 clients.</li>
+                    <li>Attention seems to be normalizing over the years. In 2012, we had a lot of little projects and one gigantic project. This past year, the distribution seems much more even.</li>
+                    <li>Though you wouldn&rsquo;t be able to tell by the scale of these graphs, we&rsquo;re spending less time overall on work. The number of hours we&rsquo;ve put in this past year is about 8% lower than we did in 2012, which makes sense if you compare that with the fact that we worked with 6 less clients. But that also means we&rsquo;re spending a little more time with each client.</li>
+                    <li>Revenue is up 51% from 2012, while profit is down 32%. Hmm, that&rsquo;s definitely something to fix for 2019.</li>
+                </ul>
+
+                <p>What did you learn about your business over the last year? What more can I tell you about mine?</p>
+
+                <p>Happy holidays, superfriends, and see you all in 2019!</p>
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/passing-the-superbooked-baton/">
+                        Passing the SuperBooked <span class="dm-c-upNext_title_lastWord">Baton</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
