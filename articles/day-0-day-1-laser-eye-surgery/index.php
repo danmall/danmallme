9005afdda00112f95f3133db391cf89097eb1082
Dan Mall <dan@superfriend.ly>
2019-02-22 05:56:02 -0500

    Starting Laser Eye Surgery article

---
+++ articles/day-0-day-1-laser-eye-surgery/index.php
@@ -0,0 +1,153 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Break-Even Points for Value Pricing,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Break-Even Points for Value Pricing" />
+    <meta name="twitter:description" property="og:description" content="Understanding client expectations for results will help you create great pricing options." />
+    <meta name="description" content="Understanding client expectations for results will help you create great pricing options." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/breakEven.png" />
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <figure class="dm-c-imageObject">
+        <div class="dm-c-imageObject_imageWrap" style="background: #fff;">
+            <script>
+            document.write('<img class="dm-c-imageObject_image dm-js-lazy" width="1200" height="450" data-src="breakEven.svg" alt="Trading knowledge work for money" />');
+            </script>
+            <noscript>
+                <img class="dm-c-imageObject_image" width="1200" height="450" src="breakEven.svg" alt="Trading knowledge work for money" />
+            </noscript>
+        </div><!-- .dm-c-imageObject_imageWrap -->            
+    </figure><!-- .dm-c-imageObject -->
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Break-Even Points for Value Pricing
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">February 19, 2018 at 9:12 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <p><span class="dm-dp-openingLines">When <a href="https://abookapart.com/products/pricing-design">value pricing</a></span>&mdash;pricing work based on the value it brings your customer&mdash;here&rsquo;s a question I typically ask:</p>
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <p><strong>&ldquo;At what point does this project pay for itself?&rdquo;</strong></p>
+
+                <p>Great answers I&rsquo;ve heard:</p>
+
+                <ul>
+                    <li>When 100 more students enroll.</li>
+                    <li>When we get an average of 5 more leads a month.</li>
+                    <li>When we add 1,000 more people to or mailing list.</li>
+                    <li>When people prefer us over our top competitor.</li>
+                </ul>
+
+                <p>Answers I&rsquo;ve heard that make me think they&rsquo;re not ready for a project (at least not with <a href="http://superfriend.ly/">SuperFriendly</a>):</p>
+
+                <ul>
+                    <li>I don&rsquo;t know.</li>
+                    <li>Why is that any of your business?</li>
+                    <li>There&rsquo;s no way we can quantify that.</li>
+                </ul>
+
+                <p>Smart clients know what change they&rsquo;re looking for, even if they don&rsquo;t know how to achieve it. (That&rsquo;s why they&rsquo;re hiring help.) Sometimes I&rsquo;ll try to help them <em>articulate</em> the value, but I don&rsquo;t try to <em>define</em> it for them. That&rsquo;s like going to a mechanic without a specific repair in mind and asking them to find a part of your car to fix.</p> 
+
+                <p>In my experience, clients that don&rsquo;t initially know what they want turn into clients that stretch out rounds of review and aren&rsquo;t happy with the final result. Even when we&rsquo;ve delivered something that might work for them, they can&rsquo;t tell, because they don&rsquo;t know what the right thing is.</p>
+
+                <p>But clients that know what they want from the start? They know <em>way</em> early on if we&rsquo;re on track or off course, because they know the destination.</p> 
+
+                <p>A client that knows what they want knows where the project pays for itself, where it breaks even. And that&rsquo;s the first price ceiling. If they have a $5,000 budget and expect 1000 more signups, they&rsquo;ll think they overpaid if you try to charge them $10,000 and they get 1,000 more signups (whether or not that&rsquo;s accurate).</p>
+
+                <p>But what if you&rsquo;re confident than you could get them 2,000 signups? Then they may accept a $10,000 price because you&rsquo;re delivering twice the results they&rsquo;re expecting. Here&rsquo;s the kicker: that&rsquo;s still a break-even price, even though it&rsquo;s double their stated budget.</p>
+
+                <p><strong>You can confidently price over any stated budget if you can demonstrate that you will deliver more value than their break-even metric.</strong> The more you understand the break-even point, the more liberty you have to assess and communicate the multiples of value you can deliver.</p>
+
+                <p>When you value price, every price you give a client should be at least a break-even price. Better yet, every price you give a client should be a &ldquo;they got a deal&rdquo; price.</p>
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/designer-developer-collaboration-workshop/">
+                        Designer + Developer Collaboration <span class="dm-c-upNext_title_lastWord">Workshop</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
