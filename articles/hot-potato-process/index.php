72895b4d83a71cc7d484cb531c8b0e3d791b54d4
Dan Mall <dan@superfriend.ly>
2019-10-13 08:08:11 -0400

    Hot potato process article ready

---
+++ articles/hot-potato-process/index.php
@@ -0,0 +1,150 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;The Hot Potato Process,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="The Hot Potato Process" />
+    <meta name="twitter:description" property="og:description" content="A better way for designers and developers to truly work together.." />
+    <meta name="description" content="A better way for designers and developers to truly work together." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/og.png" />
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <figure class="dm-c-imageObject dm-c-laserEyeHero">
+        <div class="dm-c-imageObject_imageWrap" style="background: #a22622;">
+            <script>
+            document.write('<img class="dm-c-imageObject_image dm-js-lazy" width="1200" height="630" data-src="hot-potato.svg" alt="The hot potato process" />');
+            </script>
+            <noscript>
+                <img class="dm-c-imageObject_image" width="1200" height="630" src="hot-potato.png" alt="The hot potato process" />
+            </noscript>
+        </div><!-- .dm-c-imageObject_imageWrap -->            
+    </figure><!-- .dm-c-imageObject -->
+    
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                The Hot Potato Process
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">October 13, 2019 at 8:07 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <p><span class="dm-dp-openingLines">The big misconception</span> I&rsquo;ve seen designers and developers often fall victim to is believing that handoff goes one way. Designers hand off comps to developers and think their work is done. That puts a lot of pressure on the designer to get everything perfect in one pass.</p>
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <p>Instead, great collaboration follows what <a href="https://bradfrost.com/">Brad Frost</a> and I call &ldquo;The Hot Potato Process,&rdquo; where ideas are passed quickly back and forth from designer to developer and back to designer then back to developer for the entirety of a product creation cycle. This is exactly what we&rsquo;re demonstrating in <a href="/articles/designer-developer-workflow/">our designer + developer collaboration recording</a> and <a href="/articles/designer-developer-collaboration-workshop/">what we teach in our collaboration workshops</a>.</p>
+
+                <img src="waterfall-vs-hotPotato.svg" alt="Waterfall vs. Hot Potato" />
+
+                <h2 id="sit-together">Sit together <a class="dm-dp-anchorLink" href="#sit-together">#</a></h2>
+
+                <p>It seems overly simplistic, but the best way for designers and developers to start trying The Hot Potato Process is to sit together. We&rsquo;ve seen many a designer + developer pair who have worked together for years become enlightened as to how the other works within the first few minutes of sitting together.</p>
+
+                <h2 id="distributed-teams">Distributed teams <a class="dm-dp-anchorLink" href="#distributed-teams">#</a></h2>
+
+                <p>The first piece of pushback we often get about The Hot Potato Process is that it seems ideal only for co-located teams. But, as <a href="https://www.nytimes.com/2017/02/15/us/remote-workers-work-from-home.html">increasingly more people are working remotely</a>, good processes should adapt to all the different ways that people work; the Hot Potato Process is no exception to that.</p>
+
+                <p>If you can&rsquo;t sit together in person, use video chat and other real-time synchronous tools to simulate working together in a co-located way. My teams will often <a href="https://twitter.com/danmall/status/1100082226346250240">leave a Zoom chat open for a few hours</a> as a proxy for being in the same office together, even if we&rsquo;re not talking to each other the whole time.</p>
+
+                <aside id="note1" class="dm-c-sidenotes dm-l-articleGrid--sidenote">
+                    <p>For more great tips on remote working, see Mandy Brown&rsquo;s excellent article, &ldquo;<a href="http://stet.editorially.com/articles/making-remote-teams-work/">Making remote teams work</a>.&rdquo;</p>
+                </aside>
+
+                <h2 id="recordings-bridge-time-zones">Use recordings to bridge time zone gaps <a class="dm-dp-anchorLink" href="#recordings-bridge-time-zones">#</a></h2>
+
+                <p>If you can&rsquo;t work at the same time due to time zones not lining up, you can record yourself working and send it to your collaborator. Your collaborator can then work alongside your recording while making their own recording. Then continue to pass recordings back and forth. Tools like <a href="https://www.voxer.com/">Voxer</a>, <a href="https://www.marcopolo.me/">Marco Polo</a>, and other walkie-talkie- and intercom-like apps help to make asynchronous collaboration feel a bit closer to synchronous collaboration.</p>
+
+                <h2 id="working-together">Working together means working together <a class="dm-dp-anchorLink" href="#working-together">#</a></h2>
+
+                <p>If you can&rsquo;t sit together in person or trade recordings or approximate these kinds of approaches, you might have to come to terms with the fact that you&rsquo;re not truly working together.</p> 
+
+                
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/commarts-interactive-2020/">
+                        Communication Arts Interactive: <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
