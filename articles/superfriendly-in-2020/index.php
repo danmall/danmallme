e910a34eca94b15300b4b3d63d35723ac9f2a4c5
Dan Mall <dan@superfriend.ly>
2019-12-28 13:49:49 -0500

    Hours and profit text + graphs done

---
+++ articles/superfriendly-in-2020/index.php
@@ -0,0 +1,306 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;SuperFriendly in 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="SuperFriendly in 2020" />
+    <meta name="twitter:description" property="og:description" content="Looking forward to a new kind of year for SuperFriendly by looking back at the past few years." />
+    <meta name="description" content="Looking forward to a new kind of year for SuperFriendly by looking back at the past few years. p" />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/og.png" />
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+    
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                SuperFriendly in 2020
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">December 23, 2019 at 8:07 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <p><span class="dm-dp-openingLines">I believe</span> that 2020 will be a big year for <a href="https://superfriendlydesign.systems/">SuperFriendly</a>.</p>
+                
+                <p>Midway through 2019, we officially and publicly repositioned the company to focus solely on <a href="https://superfriendlydesign.systems/glossary/#design-system">design systems</a>. An overwhelming majority of the work we were doing over the last few years was design systems work anyway, so we decided to be upfront about this focus in hopes that <a href="http://v3.danielmall.com/articles/oil-change-pizza/">the narrower positioning</a> would bring some advantages.</p>
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <p>And boy, it did. The end of 2019 showed bore plenty of fruits of that labor. We&rsquo;ve seen a 1,000% increase in the amout of leads in our pipeline compared to what we had at this time last year.</p>
+
+                <p>In order to figure out how to set up well for a great 2020, I found it useful to look back at how SuperFriendly has done thus far. (I shared a more detailed version of this with the team and thought it could be helpful to share some of this publicly in case it helps any other agency owner out there.)</p>
+
+                <h2 id="time-spent">Time spent <a class="dm-dp-anchorLink" href="#time-spent">#</a></h2>
+
+                <p>At the end of 2012, I shared some data about <a href="http://v3.danielmall.com/articles/superfriendly-annual-report-2012/">how I spent my time</a> as an owner of a new agency that was still primarily responsible for the majority of production work. Nowadays, I&rsquo;m much less involved. Here&rsquo;s a comparison of how 2019 compared to 2012. <sup class="dm-c-sidenotes--link"><a href="#note1">1</a></sup></p>
+
+                <object class="dm-u-marginTop--xl dm-u-marginBottom--xl" role="img" aria-label="2012 vs. 2019" aria-describedby="2012v2019" type="image/svg+xml" data="2012v2019.svg">
+                    <table id="2012v2019">
+                        <caption>2012 vs. 2019</caption>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th id="hours" scope="col">Total Hours Worked</th>
+                            <th id="average" scope="col">Average Hours per Week</th>
+                        </tr>
+                        <tr>
+                            <th scope="row">2012</th>
+                            <td>2,430</td>
+                            <td>47</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2019</th>
+                            <td>1,984</td>
+                            <td>38</td>
+                        </tr>
+                    </table>
+                </object>
+
+                <object class="dm-u-marginTop--xl dm-u-marginBottom--xl" role="img" aria-label="Hours per Task" aria-describedby="hours-per-task-2012 hours-per-task-2019" type="image/svg+xml" data="hours-per-task.svg">
+
+                    <table id="hours-per-task-2012">                    
+                        <caption>Hours Spent per Task in 2012</caption>
+                        <tr>
+                            <th>Task</th>
+                            <th>Hours</th>
+                            <th>Percentage</th>
+                        </tr>
+                        <tr>
+                            <td>Design</td>
+                            <td>819</td>
+                            <td>34%</td>
+                        </tr>
+                        <tr>
+                            <td>Admin</td>
+                            <td>700</td>
+                            <td>29%</td>
+                        </tr>
+                        <tr>
+                            <td>Front-End Development</td>
+                            <td>352</td>
+                            <td>14%</td>
+                        </tr>
+                        <tr>
+                            <td>Business Development</td>
+                            <td>276</td>
+                            <td>11%</td>
+                        </tr>
+                        <tr>
+                            <td>Speaking/Conferences</td>
+                            <td>96</td>
+                            <td>4%</td>
+                        </tr>
+                        <tr>
+                            <td>Vacation</td>
+                            <td>64</td>
+                            <td>3%</td>
+                        </tr>
+                    </table>
+
+                    <table id="hours-per-task-2019">
+                        <caption>Hours Spent per Task in 2019</caption>
+                        <tr>
+                            <th>Task</th>
+                            <th>Hours</th>
+                            <th>Percentage</th>
+                        </tr>
+                        <tr>
+                            <td>Creative Direction</td>
+                            <td>587</td>
+                            <td>30%</td>
+                        </tr>
+                        <tr>
+                            <td>Business Development</td>
+                            <td>283</td>
+                            <td>14%</td>
+                        </tr>
+                        <tr>
+                            <td>Design</td>
+                            <td>237</td>
+                            <td>12%</td>
+                        </tr>
+                        <tr>
+                            <td>Speaking/Conferences</td>
+                            <td>222</td>
+                            <td>11%</td>
+                        </tr>
+                        <tr>
+                            <td>Vacation</td>
+                            <td>168</td>
+                            <td>8%</td>
+                        </tr>
+                        <tr>
+                            <td>Admin</td>
+                            <td>110</td>
+                            <td>6%</td>
+                        </tr>
+                        <tr>
+                            <td>Front-End Development</td>
+                            <td>70</td>
+                            <td>3%</td>
+                        </tr>
+                    </table>
+
+                </object>                
+
+                <aside id="note1" class="dm-c-sidenotes dm-l-articleGrid--sidenote">
+                    <p>The percentages don&rsquo;t add up to 100%, mostly because I only wanted to show the most significant changes between 2012 and 2019.</p>
+                </aside>
+
+                <p>A few takeaways here for me:</p>
+
+                <ul>
+                    <li>Though I&rsquo;m still involved in project work a bit, my project time tends to be spent significantly more on direction than actual design and coding work. That said, we&rsquo;ve made some pretty major changes that will now further remove me from both direction and production work in 2020, so I&rsquo;ll be really curious to see how this graph changes over the next year.</li>
+                    <li>I&rsquo;ve been a fan of Cameron Moll&rsquo;s <a href="https://medium.com/come-in-we-re-hiring/the-minimum-vacation-policy-15f6c3b922f">Minimum Vacation Policy</a> since he wrote about it, and I&rsquo;ve been trying to put it into effect more and more each year. My general premise was to pick 2 days at the beginning of each month that I was going to take off, not really to do anything specific but really just to not work and give myself a break. That didn&rsquo;t really go as planned,as I instead took longer breaks in big chunks, but I&rsquo;m still pleased to see that I averaged 21 days of vacation in 2019 which computes to about 2 days per month.</li>
+                </ul>
+
+                <h2 id="profit">Profit <a class="dm-dp-anchorLink" href="#profit">#</a></h2>
+
+                <p>Our profit has really fluctuated over the years and is generally trending downwards. A major focus for 2020 is to at least level this out, if not trend upwards. A profit of 58% would do the trick, but it&rsquo;s a touch too ambitious to be realistic. We&rsquo;re shooting for 50%.</p>
+
+                <object class="dm-u-marginTop--xl dm-u-marginBottom--xl" role="img" aria-label="SuperFriendly Profit" aria-describedby="profit" type="image/svg+xml" data="profit.svg">
+                    <table id="profit">
+                        <caption>SuperFriendly Profit</caption>
+                        <tr>
+                            <th scope="col">Year</th>
+                            <th scope="col">Profit %</th>
+                        </tr>
+                        <tr>
+                            <th scope="row">2012</th>
+                            <td>29%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2013</th>
+                            <td>22%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2014</th>
+                            <td>36%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2015</th>
+                            <td>9%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2016</th>
+                            <td>36%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2017</th>
+                            <td>24%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2018</th>
+                            <td>-18%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2019</th>
+                            <td>28%</td>
+                        </tr>
+                    </table>
+                </object>
+
+                <h2 id="distributed-teams">Distributed teams <a class="dm-dp-anchorLink" href="#distributed-teams">#</a></h2>
+
+                <p>The first piece of pushback we often get about The Hot Potato Process is that it seems ideal only for co-located teams. But, as <a href="https://www.nytimes.com/2017/02/15/us/remote-workers-work-from-home.html">increasingly more people are working remotely</a>, good processes should adapt to all the different ways that people work; the Hot Potato Process is no exception to that.</p>
+
+                <p>If you can&rsquo;t sit together in person, use video chat and other real-time synchronous tools to simulate working together in a co-located way. My teams will often <a href="https://twitter.com/danmall/status/1100082226346250240">leave a Zoom chat open for a few hours</a> as a proxy for being in the same office together, even if we&rsquo;re not talking to each other the whole time.</p>
+
+                <aside id="note1" class="dm-c-sidenotes dm-l-articleGrid--sidenote">
+                    <p>For more great tips on remote working, see Mandy Brown&rsquo;s excellent article, &ldquo;<a href="http://stet.editorially.com/articles/making-remote-teams-work/">Making remote teams work</a>.&rdquo;</p>
+                </aside>
+
+                <h2 id="recordings-bridge-time-zones">Use recordings to bridge time zone gaps <a class="dm-dp-anchorLink" href="#recordings-bridge-time-zones">#</a></h2>
+
+                <p>If you can&rsquo;t work at the same time due to time zones not lining up, you can record yourself working and send it to your collaborator. Your collaborator can then work alongside your recording while making their own recording. Then continue to pass recordings back and forth. Tools like <a href="https://www.voxer.com/">Voxer</a>, <a href="https://www.marcopolo.me/">Marco Polo</a>, and other walkie-talkie- and intercom-like apps help to make asynchronous collaboration feel a bit closer to synchronous collaboration.</p>
+
+                <h2 id="working-together">Working together means working together <a class="dm-dp-anchorLink" href="#working-together">#</a></h2>
+
+                <p>If you can&rsquo;t sit together in person or trade recordings or approximate these kinds of approaches, you might have to come to terms with the fact that you&rsquo;re not truly working together.</p> 
+
+                
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/commarts-interactive-2020/">
+                        Communication Arts Interactive: <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
