34b2c1f583e2a95d85e58705ca9e1c0397807417
Dan Mall <dan@superfriend.ly>
2020-02-29 15:45:14 -0500

    Creating mixin for nowrap; 02/29/2020 weeknotes article done

---
+++ articles/weeknotes-2020-02-23/index.php
@@ -0,0 +1,142 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: February 23&ndash;March 1, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: February 23&ndash;March 1, 2020" />
+    <meta name="twitter:description" property="og:description" content="Stepping up quality for video calls." />
+    <meta name="description" content="Stepping up quality for video calls." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/thumb.png" />
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: February 23&ndash;<span class="dm-u-wrap--none">February 29, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">February 29, 2020 at 3:43 <abbr title="Post Meridien">PM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Sketched out some new ideas for a soon-to-debut <a href="https://superfriendlydesign.systems/">SuperFriendly</a> service offering that a few clients are beta testing</li>
+                    <li>Held 1-on-1 coaching sessions with SuperFriendly Directors <a href="https://isabelsousa.com/">Isa Sousa</a> and <a href="https://www.antonsten.com/">Anton Sten</a></li>
+                    <li>Conducted onsite workshop with new client with <a href="https://bigmedium.com/">Josh Clark</a> and <a href="https://bradfrost.com/">Brad Frost</a></li>
+                    <li>Debrief meeting with client</li>
+                    <li>Confirmed workshop dates with a new prospective client</li>
+                    <li>Chatted with <a href="https://feather.ca/">Derek Featherstone</a> about a new project</li>
+                    <li>Followed up over email with a prospective client to try and line up some upcoming work</li>
+                    <li>Discussed with team whether we should do projects with two prospective clients</li>
+                    <li>Chatted with <a href="https://twitter.com/favoriteshape">Anthony Armendariz</a> and <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a> about a potential collaboration between <a href="https://www.funsize.co/">Funsize</a> and SuperFriendly</li>
+                    <li>Braintrust meeting with a SuperFriendly team about template design</li>
+                    <li>Lurked silently in a meeting between all SuperFriendly Producers</li>
+                    <li>Talked to three prospective clients</li>
+                    <li>Caught up with <a href="https://www.afyia.design/index.html">Afyia Smith</a> about the new <a href="https://shoptalkshow.com/">Shop Talk Show</a> site</li>                    
+                    <li>Sat in on a working session between <a href="https://www.sushiandrobots.com/">Jina Anne</a> and <a href="https://ianfrostweather.com/">Ian Frost</a> about building a prototype for a current client</li>
+                    <li>Chatted with <a href="https://twitter.com/jessiofhall/">Jessi Hall</a>, <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a>, and Joe Rinaldi about how to improve the way we do value pricing</li>
+                    <li>Recorded a podcast episode for a soon-to-debut podcast</li>
+                    <li>Interviewed a strategist</li>
+                    <li>Weekly business development chat with Joe Rinaldi</li>
+                </ol>
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>Ate mediocre sushi with <a href="http://emilymall.me/">Em</a></li>
+                    <li>Went to 6-month dental checkup</li>
+                    <li>Played basketball in a 5:30<abbr title="Ante Meridien">am</abbr> game</li>  
+                    <li>Participated in the <a href="https://www.eventbrite.com/e/philly-collective-sunrise-meet-up-tickets-94074823329">Philly Collective Sunrise Meet Up</a> at the Philadelphia Museum of Art</li>
+                    <li>Took <a href="https://twitter.com/siddaleemall">Sidda</a> to the aquarium</li>
+                    <li>Went to IKEA with the fam</li>
+                </ol>
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/better-webcam-video/">
+                        Better Webcam <span class="dm-c-upNext_title_lastWord">Video</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
