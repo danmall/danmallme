38bec141d53f130b7a6104d33cb6488a4fe77607
Dan Mall <dan@superfriend.ly>
2020-03-07 22:15:18 -0500

    Weeknotes March 1 2020

---
+++ articles/weeknotes-2020-03-01/index.php
@@ -0,0 +1,159 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: March 1&ndash;March 7, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: March 1&ndash;March 7, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">March 1</span>&ndash;<span class="dm-u-wrap--none">March 7, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">March 7, 2020 at 10:15 <abbr title="Post Meridien">PM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Braintrust meeting for client project</li>
+                    <li>Talked to <a href="https://bradfrost.com/">Brad Frost</a> about new potential project</li>
+                    <li>Interviewed new content strategist with <a href="https://twitter.com/jessiofhall/">Jessi Hall</a></li>
+                    <li>Client presentation for recommendations</li>
+                    <li>Business development check-ins with <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a></li>
+                    <li>Talked project strategy with <a href="http://www.geofcrowl.com/">Geof Crowl</a></li>
+                    <li>Started new blog post draft for <a href="https://superfriendlydesign.systems/">SuperFriendly</a> site</li>
+                    <li>Team retrospective on year-long design system project</li>
+                    <li>Pitched new project to existing client</li>
+                    <li>Project check-in with <a href="https://www.sushiandrobots.com/">Jina Anne</a></li>
+                    <li>Chat with <a href="https://www.afyia.design/">Afyia Smith</a> about <a href="https://shoptalkshow.com/">Shop Talk Show</a> site</li>
+                    <li>Scheming with <a href="https://blankenship.xyz/">Joshua Blankenship</a> &amp; <a href="https://www.mattecook.com/">Matt Cook</a></li>
+                    <li>Prepped with <a href="https://bigmedium.com/">Big Medium</a> team about upcoming project deliverables</li>
+                    <li>Contract negotiations with a new hire</li>
+                    <li>Created 2 drafts of an infrastructure diagram for a client</li>
+                    <li>Lunch with <a href="http://dealtdev.com/">Kevin Deal</a></li>
+                    <li>Studio meeting about how we better could onboard SuperFriends with Jessi Hall, <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a>, and Joe Rinaldi</li>
+                </ol>
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>Hosted a friend of <a href="https://twitter.com/siddaleemall">Sidda</a>&rsquo;s for a playdate</li>
+                    <li>Played in the weekly Monday night basketball game</li>
+                    <li>Accidentally went back to <a href="https://serpicoonsouth.com/">Serpico</a> with <a href="http://emilymall.me/">Em</a></li>
+                    <li>Tried out a new babysitter</li>
+                    <li>Drove Em to the airport</li>
+                    <li>Solo parented for a few days while Em was out of town</li>
+                    <li>Posted a 50th picture to <a href="http://photos.danmall.me/">my photography site</a></li>
+                    <li>Talked to my therapist about openness and playfulness</li>                    
+                    <li>Dropped off dry cleaning</li>
+                    <li>Picked up prescriptions</li>                    
+                    <li>Ate burgers with the kids</li>
+                    <li>Bought supplies for some crafts</li>
+                    <li>Went grocery shopping for fruits and veggies</li> 
+                    <li>Tried out a new system for sorting mail in the house</li> 
+                </ol>
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-02-23/">
+                    Weeknotes: February 23&ndash;February 29, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
