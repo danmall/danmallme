d44fd954fbeb37c3deacd74597d76275bc2450eb
Dan Mall <dan@superfriend.ly>
2020-03-21 21:22:44 -0400

    Weeknotes March 15 2020 ready to publish

---
+++ articles/weeknotes-2020-03-15/index.php
@@ -0,0 +1,147 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: March 15&ndash;March 21, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: March 15&ndash;March 21, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">March 15</span>&ndash;<span class="dm-u-wrap--none">March 22, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">March 21, 2020 at 9:20 <abbr title="Post Meridien">PM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Published <a href="https://superfriendlydesign.systems/articles/making-design-systems-public/">Making Design Systems Public</a></li>
+                    <li>Talked to <a href="https://www.finamoredesign.com/">Troy Finamore</a> about the class I&rsquo;ll be teaching soon</li>
+                    <li>Talked to <a href="https://twitter.com/megasheppy">Megan Shuffle</a> about an upcoming project</li>
+                    <li>Drexel online class briefing</li>
+                    <li>Three meetings over five hours with a client to wrap up a project by making final recommendations and help them write user stories against those recommendations</li>
+                    <li>Work on new SuperFriendly site section for our new design systems podcast</li>
+                    <li>Talked to <a href="https://www.afyia.design/">Afyia Smith</a> about <a href="https://shoptalkshow.com/">Shop Talk Show</a> site</li>
+                    <li>Worked on a new section of the <a href="https://superfriendlydesign.systems/">SuperFriendly site</a></li>
+                    <li>Sorted through all the <a href="https://twitter.com/superfriendlyco/status/1240705305886912515">COVID-19 illustrator applicants</a></li>
+                    <li>Interviewed a new prospective tech director</li>
+                    <li>Talked to <a href="https://twitter.com/tpitre">TJ Pitre</a> about a new project</li>
+                    <li>Business development check-in meeting with <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a></li>
+                </ol>
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>Watched <cite>Jumanji: The Next Level</cite>, <cite>Split</cite>, and <cite>Onward</cite> (<a href="https://twitter.com/danmall/status/1236736228940275717">again</a>)</li>
+                    <li>Slow-cooked <a href="https://www.allrecipes.com/recipe/18032/slow-cooker-lemon-garlic-chicken-ii/">lemon garlic chicken</a> and <a href="https://thestayathomechef.com/slow-cooker-beef-short-ribs/">beef short ribs</a></li>
+                    <li><a href="https://twitter.com/danmall/status/1241130099585560576">Reverse-seared steaks for the first time</a></li>
+                    <li>Talked to my therapist</li>
+                    <li>Took the family for a hike at the Delaware Water Gap</li>
+                    <li>Publicly announced <a href="https://twitter.com/superfriendlyco/status/1240705305886912515">an idea</a> to help illustrators affected by COVID-19</li>
+                    <li>Slept an average of 6 hours and 38 minutes per night. Average bedtime: 10:25pm. Average wake up time: 6:16am.</li>
+                </ol>
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-03-08/">
+                    Weeknotes: March 8&ndash;March 14, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
