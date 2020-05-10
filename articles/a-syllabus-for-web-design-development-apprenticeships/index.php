d96a8234bc56bb2c49caf04b5983e583f8a7bca7
Dan Mall <dan@superfriend.ly>
2020-05-10 07:56:45 -0400

    Apprenticeship article started

---
+++ articles/a-syllabus-for-web-design-development-apprenticeships/index.php
@@ -0,0 +1,152 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;A Syllabus for Web Design &amp; Web Development Apprenticeships,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="A Syllabus for Web Design &amp; Web Development Apprenticeships" />
+    <meta name="twitter:description" property="og:description" content="Stepping up quality for video calls." />
+    <meta name="description" content="Stepping up quality for video calls." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/thumb.png" />
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                A Syllabus for Web Design &amp; Web Development Apprenticeships
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">May 10, 2020 at 12:55 <abbr title="Post Meridien">PM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+            
+                <p><span class="dm-dp-openingLines">From 2012&ndash;2019</span>, I ran a 9-month apprenticeship for people new to design and development that wanted to make their way into the tech industry. I&rsquo;ve previously written about <a href="http://v3.danielmall.com/articles/apprenticeships-superfriendly-academy/"><em>why</em> apprenticeships are valuable</a>; here are the details about <em>how</em> I ran them.</p>
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <p>Within that 7-year span, I had almost 20 apprentices. Some finished the apprenticeship; some didn&rsquo;t. Some got full-time jobs in the discipline they pursued during their apprenticeship. Some took the skills they learned in their apprenticeship and applied them to a tangential industry. Some went freelance or started their own companies. Some went back to doing what they did before the apprenticeship.</p>
+
+                <p>As I write this information for the first time, I&rsquo;m realizing in hindsight how valuable documenting this earlier would have been for me and even moreso for my apprentices. I&rsquo;m partially writing all of this down as a resource for myself in case I ever bring the apprenticeship back again.</p>
+                
+                <p lang="fr">C&lsquo;est la vie.</p> 
+
+                <h2 id="objective">Objective <a class="dm-dp-anchorLink" href="#objective">#</a></h2>
+                
+                <p>To unlock a new way of life for the apprentice through gaining knowledge of web design and/or web development and the accompanying industry.</p>
+
+
+                <h2 id="prerequisites">Prerequisites <a class="dm-dp-anchorLink" href="#prerequisites">#</a></h2>
+
+                <p>Computer proficiency. As little prior knowledge about design and development is preferred, as most of it would likely have to be untaught. If you can already make a design or development portfolio, you&rsquo;re over-qualified for this apprenticeship.</p>                
+
+
+                <h2 id="required-materials">Required Materials <a class="dm-dp-anchorLink" href="#required-materials">#</a></h2>
+
+                <ul>
+                    <li>Laptop</li>
+                </ul>
+
+
+                <h2 id="attendance-policy">Attendance Policy <a class="dm-dp-anchorLink" href="#attendance-policy">#</a></h2>
+
+                <p>At the beginning of the apprenticeship, in-person attendance at SuperFriendly <abbr title="Headquarters">HQ</abbr> 4&ndash;5 days a week from 10<abbr title="Ante meridien">am</abbr>&ndash;4<abbr title="Post meridien">pm</abbr> is required. During the second and third units of the apprenticeship, in-person attendance can decrease and independent remote work can increase.</p>
+
+                <p>(Because so much of the learning was designed to be passive, I could never figure out how to make a remote apprenticeship work as well as an in-person one, so to date it has never been an option that I offered.)</p>
+
+
+                <h2 id="calendar">Calendar <a class="dm-dp-anchorLink" href="#calendar">#</a></h2>
+
+
+                <h1>Questions to answer</h1>     
+
+                <ul>
+                    <li>When you put an apprentice on a project, what’s the balance of time between them watching you work and asking questions, vs. them working on their own and asking you questions on an as-needed basis?</li>
+                    <li>Do you happen to have any apprentice exit interview questions/topics you used for your apprentices? Quincy's last day is Monday and while I have some things I'd like to talk about, I wanted to see if you had topics you like to get into.</li>
+                </ul>
+
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/superfriendly-in-2020/">
+                        SuperFriendly in <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
