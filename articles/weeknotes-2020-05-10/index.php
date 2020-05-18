b03c4f0e98c610ebe209581d3839a0b5a229fbea
Dan Mall <dan@superfriend.ly>
2020-05-18 10:28:27 -0400

    Weeknotes ready to publish

---
+++ articles/weeknotes-2020-05-10/index.php
@@ -0,0 +1,164 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: May 10&ndash;May 16, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: May 10&ndash;May 16, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">May 10</span>&ndash;<span class="dm-u-wrap--none">May 16, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">May 18, 2020 at 10:24 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Posted about <a href="https://twitter.com/danmall/status/1260587643798261764">SuperFriendly Q1 OKRs</a></li>
+                    <li>Prepped for this month&rsquo;s core team meeting</li>
+                    <li>Workshopped with <a href="https://twitter.com/gregsarault">Greg Sarault</a ></li>
+                    <li>1-on-1s with <a href="https://twitter.com/jessiofhall/">Jessi Hall</a>, <a href="https://twitter.com/tpitre">TJ Pitre</a>, and <a href="https://www.sherlocklegal.com/">Matt Sherlock</a></li>
+                    <li>Talked to <a href="https://www.creative-curls.com/">Jenn Piatkowski</a> about branding and illustration project                </li>
+                    <li>Gave career advice to a client</li>
+                    <li>Biz dev chats with <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a></li>
+                    <li>Schemed with <a href="https://twitter.com/noel">No&euml;l Jackson</a></li>
+                    <li>Wrote <a href="https://superfriendlydesign.systems/">SuperFriendly</a> Q2 OKRs</li>
+                    <li>Talked to <a href="https://twitter.com/abbyfretz">Abby Fretz</a>, <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a>, and Joe Rinaldi about 2 new business opportunities</li>
+                    <li>Pruned SuperFriendly backlog</li>
+                    <li>Reviewed SuperFriendly backlog with Crystal Vitelli and <a href="https://southleft.com/">Southleft</a></li>
+                    <li>Internal sync about upcoming presentation with Crystal Vitelli, <a href="https://blankenship.xyz/">Joshua Blankenship</a>, <a href="https://www.mattecook.com/">Matt Cook</a>, and Joe Rinaldi</li>
+                    <li>Schemed with <a href="https://twitter.com/emmmily_e">Emily Edwards</a></li>
+                    <li>Talked to <a href="https://twitter.com/smashingmag">Vitaly Friedman</a> about Smashing Conference workshop ideas</li>
+                    <li>Wrote about <a href="/articles/ceo-covid19-calendar/">how my schedule has changed recently</a></li>
+                    <li>Talked to a student about her final project</li>
+                    <li>Talk to <a href="https://www.sushiandrobots.com/">Jina Anne</a> and her conference production team about <a href="https://www.clarityconf.com/">Clarity Conference</a></li>
+                    <li>Weekly studio chat about Executive Producers and Executive Directors with Crystal Vitelli and Joe Rinaldi</li>
+                    <li>Worked on SuperFriendly site updates</li>
+                    <li>Taught Design Systems class with special guest <a href="https://twitter.com/nathanacurtis">Nathan Curtis</a></li>
+                    <li>Talked to <a href="https://twitter.com/jonejackson_">Jon Jackson</a> about new project</li>
+                </ol>
+
+                
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>Watched episodes 7 and 8 of <cite><a href="https://www.espn.com/nba/story/_/id/28973557/the-last-dance-updates-untold-story-michael-jordan-chicago-bulls">The Last Dance</a></cite></li>
+                    <li>Finished <cite><a href="https://www.netflix.com/title/80228244">Waco</a></cite></li>
+                    <li>Hiked the <a href="https://myphillypark.org/explore/parks/belmont-plateau-trails/">Belmont Plateau Trails</a></li>
+                    <li>Started (and abandoned) <cite><a href="https://www.starz.com/us/en/movies/45080">Dragon Ball Super: Broly Online</a></cite></li>
+                    <li>Watched <cite><a href="https://www.paramountmovies.com/movies/sonic-the-hedgehog">Sonic the Hedgehog</a></cite></li>
+                    <li>The kids finished the final season of <cite>Full House</cite>&hellip; and proceeded to start it all over again</li>
+                    <li>I realized I haven&rsquo;t listened to music in a long time. I fired up Spotify and played undiscriminatingly through new releases. Specifically enjoyed <a href="https://open.spotify.com/album/2R3MFgSzJ42sfMKk5LqtC5">ZEROZEROZERO</a> from <a href="http://www.mogwai.co.uk/">Mogwai</a></li>
+                    <li>Finished <cite><a href="https://amzn.to/2AGdddt">Delivering Happiness</a></cite></li>
+                    <li>Started reading <cite><a href="https://amzn.to/3e2Poel">Stiff</a></cite> and <cite><a href="https://amzn.to/2X7xEYf">The Challenger Sale</a></cite></li>
+                    <li>Painted the kids&rsquo; rooms</li>
+                    <li>Watched Jeff Bartlett&rsquo;s new film, <cite><a href="https://portfolio.jeffbartlettmedia.com/Choosing-to-Live">Choosing to Live</a></cite></li>
+                </ol>
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/ceo-covid19-calendar/">
+                    A CEO&rsquo;s COVID-19 <span class="dm-c-upNext_title_lastWord">Calendar</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
