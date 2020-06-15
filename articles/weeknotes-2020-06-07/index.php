66475b564038e57d2597d2e7274e045c45f505ff
Dan Mall <dan@superfriend.ly>
2020-06-15 03:30:08 -0400

    Weeknotes June 7 draft done

---
+++ articles/weeknotes-2020-06-07/index.php
@@ -0,0 +1,173 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: June 7&ndash;June 13, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: June 7&ndash;June 13, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">June 7</span>&ndash;<span class="dm-u-wrap--none">June 13, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">June 15, 2020 at 3:29 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Talked to <a href="https://twitter.com/_KenSparks_">Ken Sparks</a>, <a href="https://dontaebenn.myportfolio.com/">Dontae Benn</a>, <a href="https://www.octaviusanewman.com/">Octavius A. Newman</a>, <a href="https://twitter.com/K_Deal2">Kevin Deal</a>, <a href="https://twitter.com/lola_deal">Lauren Deal</a>, and <a href="https://jonathanjackson.myportfolio.com/">Jon Jackson</a> about a few of their ideas for making more space for Black people at <a href="https://superfriendlydesign.systems/">SuperFriendly</a></li>
+                    <li>Sent out feelers for new team members for a new phase of work with a current client</li>
+                    <li>Finished SuperFriendly product map work</li>
+                    <li>SuperFriendly Systems Team monthly meeting</li>
+                    <li>1-on-1s with <a href="https://twitter.com/tpitre">TJ Pitre</a>, <a href="https://twitter.com/jessiofhall/">Jessi Hall</a>, <a href="https://www.antonsten.com/">Anton Sten</a>, <a href="http://www.geofcrowl.com/">Geof Crowl</a>, <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a>, and <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a></li>
+                    <li>Interviewed Lauren Deal</li>
+                    <li>Caught up (finally!) with <a href="https://twitter.com/lungsoftheocean/">Ben Meszaros</a> from <a href="https://underbelly.is/">Underbelly</a></li>
+                    <li>Talked to <a href="http://www.nickhahn.com/">Nick Hahn</a> with Joe Rinaldi</li>
+                    <li>Biz dev check-in with Joe Rinaldi</li>
+                    <li>Talked to <a href="https://www.lariarogers.com/">LaRia Rogers</a> about teaching design in higher ed</li>
+                    <li>Recommended team members for two new prospective accounts</li>
+                    <li>First Snack n&rsquo; Learn for SuperFriends about Challenger Selling</li>
+                    <li>Talked to <a href="https://twitter.com/pws">Harold Emsheimer</a> from <a href="http://www.overcommitted.com/">Overcommitted</a> with <a href="https://blankenship.xyz/">Joshua Blankenship</a> and <a href="https://www.mattecook.com/">Matt Cook</a> about a potential collaboration</li>
+                    <li>Account planning with Jessi Hall, Anton Sten, TJ Pitre, and Joe Rinaldi</li>
+                    <li>Product planning with Crystal Vitelli, TJ Pitre, and Geof Crowl</li>
+                    <li>Caught up with Joshua Blankenship</li>
+                    <li>Caught up with <a href="https://jkosoy.com/">Jamie Kosoy</a></li>
+                    <li>Extended Drexel class (even though the term is over)</li>
+                    <li>Workshopping with <a href="https://www.afyia.design/">Afyia Smith</a></li>
+                    <li>Caught up with <a href="https://www.sarahazpeitia.com/">Sarah Azpeitia</a></li>
+                    <li>Caught up with <a href="https://twitter.com/courtney271">Courtney Bradford</a></li>
+                </ol>
+
+                
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>Set up my new <a href="https://www.synology.com/en-us/products/DS218+">Synology DiskStation DS218+</a> drive for better backups. Many thanks to <a href="https://chrislea.com/">Chris Lea</a> for sharing his knowledge and experience about network drives with me.</li>
+                    <li>Did <a href="https://www.delawarerivertubing.com/">Delaware River Tubing</a> with <a href="http://emilymall.me/">Emily Mall</a> and Sharon &amp; Jeff Clark</li>
+                    <li>Slept an average of 6 hours and 54 minutes per night. Average bedtime: 10:41pm. Average wake up time: 6:29am.</li>
+                </ol>
+
+                <h3 id="watched">Watched <a class="dm-dp-anchorLink" href="#watched">#</a></h3>
+
+                <ol>
+                    <li><cite>Interstellar</cite> (again)</li>
+                    <li><cite><a href="https://www.netflix.com/title/81214015">Dave Chappelle: The Kennedy Center Mark Twain Prize for American Humor</a></cite></li>
+                </ol>
+
+                <h3 id="read">Notable reads <a class="dm-dp-anchorLink" href="#read">#</a></h3>
+
+                <ol>
+                    <li><a href="https://usefathom.com/">Fathom Analytics</a></li>
+                    <li><a href="https://www.designerfund.com/blog/building-a-more-inclusive-design-venture-ecosystem/">Building a More Inclusive Design + Venture Ecosystem | Designer Fund</a></li>
+                    <li><a href="https://www.atlassian.com/team-playbook/plays/balanced-teams">Balanced Teams Diversity Assessment Tool | Atlassian Team Playbook</a></li>
+                    <li><a href="https://www.pakeys.org/reopen-guidance-for-child-care-providers-during-covid-19/">Reopen Guidance for Child Care Providers During COVID-19</a></li>
+                    <li><a href="https://peopleofcolorintech.com/articles/the-black-founder-list/">The Black Founder List</a></li>
+                    <li><a href="https://shift.newco.co/2016/11/07/use-slacks-advanced-search-to-find-lost-files-urls-or-the-terrible-jokes-none-of-your-coworkers/">Use Slack&rsquo;s Advanced Search to Find Lost Files, URLs, or the Terrible Jokes None of Your Coworkers&hellip;</a></li>
+                </ol>
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-05-31/">
+                    Weeknotes: May 31&ndash;June 6, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
