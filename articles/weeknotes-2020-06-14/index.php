2a06295329c473017dbca812764182770aed3640
Dan Mall <dan@superfriend.ly>
2020-06-23 03:12:15 -0400

    Weeknotes ready to publish

---
+++ articles/weeknotes-2020-06-14/index.php
@@ -0,0 +1,180 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: June 14&ndash;June 20, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: June 14&ndash;June 20, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">June 14</span>&ndash;<span class="dm-u-wrap--none">June 20, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">June 23, 2020 at 3:11 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Planning meeting for new partnership with <a href="https://www.antonsten.com/">Anton Sten</a> and <a href="https://twitter.com/tpitre">TJ Pitre</a></li>
+                    <li>Talked to and <a href="https://www.linkedin.com/in/nikki-loffredo-5a8b1945/">Nikki Loffredo</a>, <a href="https://twitter.com/fullygodwin">Jonathan Godwin</a>, and <a href="https://www.glimmering.com/">Leslie Camacho</a> about modifying SuperFriendly financial allocations</li>
+                    <li>Discuss SuperFriendly art direction with <a href="https://dontaebenn.myportfolio.com/">Dontae Benn</a>, <a href="http://www.geofcrowl.com/">Geof Crowl</a>, and TJ Pitre</li>
+                    <li>Account planning with <a href="https://twitter.com/lola_deal">Lauren Deal</a> and <a href="https://www.mattecook.com/">Matt Cook</a></li>
+                    <li>Caught up with <a href="https://ayakaito.com/">Ayaka Ito</a></li>
+                    <li>Account braintrust with <a href="https://twitter.com/jessiofhall/">Jessi Hall</a>, Anton Sten, and <a href="http://www.thatwasclutch.com/">Joe Rinaldi</a></li>
+                    <li>Value statement workshopping with Lauren Deal and Matt Cook</li>
+                    <li>Scheming with <a href="https://twitter.com/RMBanfield">Richard Banfield</a> from <a href="https://www.invisionapp.com/">InVision</a> about with Joe Rinaldi</li>
+                    <li>Interviewed <a href="https://www.jnaypenn.com/">J&rsquo;Nay Penn</a></li>
+                    <li>1-on-1 with Jessi Hall</li>
+                    <li>Interviewed <a href="https://uxfol.io/iamwillemschrieks">Willem Schrieks</a></li>
+                    <li>Biz dev check-in with Joe Rinaldi</li>
+                    <li>Interviewed <a href="http://gintelgee.com/">Gintel Gee</a></li>
+                    <li>Interviewed <a href="https://dribbble.com/jenellemiller">Jenelle Miller</a></li>
+                    <li>Talked to Dontae Benn about joining SuperFriendly Systems team</li>
+                    <li>Talked to <a href="https://twitter.com/_KenSparks_">Ken Sparks</a> about joining the SuperFriendly product team</li>
+                    <li>Posted narrative examples for new pitch</li>
+                    <li>1-on-1 with Anton Sten</li>
+                    <li>1-on-1 with TJ Pitre</li>
+                    <li>Schemed with <a href="https://twitter.com/srwhitney19?lang=en">Sean Whitney</a> from <a href="https://www.figma.com/">Figma</a> with Joe Rinaldi</li>
+                    <li>Workshopped with <a href="https://www.afyia.design/">Afyia Smith</a></li>
+                    <li>Drafted policy for what SuperFriendly can and will do for the Black community</li>
+                    <li>Joined team sync with <a href="https://www.sarahazpeitia.com/">Sarah Azpeitia</a>, <a href="https://www.sarasoueidan.com/">Sara Soueidan</a>, Jessi Hall, Anton Sten, Joe Rinaldi, and TJ Pitre</li>
+                    <li>Talked Director expectations with <a href="https://isabelsousa.com/">Isa Sousa</a></li>
+                    <li>Talked with <a href="https://www.clarityconf.com/">Clarity Conference</a> organizers</li>
+                    <li>Reviewed logos from <a href="https://www.creative-curls.com/">Jenn Piatkowski</a> with Dontae Benn</li>
+                    <li>Caught up with Jessi Hall about producers</li>
+                    <li>Extended Drexel design systems class</li>
+                    <li>Interviewed <a href="https://www.esiwahomi.com/">Esi Ozemebhoya</a></li>
+                    <li>Caught up with <a href="https://www.linkedin.com/in/gina-villavicencio-4b62673/">Gina Villavicencio</a></li>
+                    <li>Catch up with <a href="https://twitter.com/jaminjantz">Jamin Jantz</a></li>
+                    <li>Schemed new product idea with <a href="https://twitter.com/MikeCarbone">Mike Carbone</a> and Leslie Camacho</li>
+                    <li>1-on-1 with Geof Crowl</li>
+                    <li>1-on-1 with Joe Rinald</li>
+                    <li>1-on-1 with <a href="https://www.sherlocklegal.com/">Matt Sherlock</a></li>
+                </ol>
+
+                
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>Shot photos at the <a href="https://navyyard.org/">Philadelphia Navy Yard</a>, the <a href="https://en.wikipedia.org/wiki/South_Philadelphia_Sports_Complex">South Philadelphia Sports Complex</a>, <a href="https://www.oldcitydistrict.org/">Old City</a>, and <a href="https://www.centercityphila.org/">Center City</a></li>
+                    <li>Ordered takeout from <a href="https://theloverestaurant.com/">The Love</a> with <a href="http://emilymall.me/">Em</a> and ate it in <a href="https://www.visitphilly.com/things-to-do/attractions/rittenhouse-square-park/">Rittenhouse Square Park</a></li>
+                    <li>Slept an average of 6 hours and 38 minutes per night. Average bedtime: 10:03pm. Average wake up time: 5:10am.</li>
+                </ol>
+
+                <h3 id="read">Notable reads <a class="dm-dp-anchorLink" href="#read">#</a></h3>
+
+                <ol>
+                    <li><a href="https://juneteenthconf.com/">JuneteenthConf</a></li>
+                    <li><a href="https://www.strategyzer.com/">Strategyzer</a></li>
+                    <li><a href="https://thecreativeindependent.com/guides/how-to-begin-designing-for-diversity/">How to begin designing for diversity</a></li>
+                    <li><a href="https://www.wired.com/story/five-years-tech-diversity-reports-little-progress/">Five Years of Tech Diversity Reports&mdash;and Little Progress</a></li>
+                    <li><a href="https://www.helloruby.com/">Hello Ruby</a></li>
+                    <li><a href="https://www.zachleat.com/web/comprehensive-webfonts/">A Comprehensive Guide to Font Loading Strategies</a></li>
+                </ol>
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-06-07/">
+                    Weeknotes: June 7&ndash;June 13, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
