f0eb25fe2999d7a8604a534d68ad6039fde71b68
Dan Mall <dan@superfriend.ly>
2020-07-20 05:37:21 -0400

    First draft of support systems article done; structure

---
+++ articles/support-systems-for-learning/index.php
@@ -0,0 +1,159 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Support Systems for Learning,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Support Systems for Learning" />
+    <meta name="twitter:description" property="og:description" content="An intentional way to teach web design and web development from scratch." />
+    <meta name="description" content="An intentional way to teach web design and web development from scratch." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>/thumb.jpg" />
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+            Support Systems for Learning
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">July 12, 2020 at 7:10 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+            
+                <p><span class="dm-dp-openingLines">As we </span> at <a href="https://superfriendlydesign.systems/">SuperFriendly</a> work with new SuperFriends as well as more junior SuperFriends, I&rsquo;m recognizing the need for different ways to support how people learn and grow. It&rsquo;s become more apparent to me that we need different ways to support people as they learn.</p>
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <p>Here&rsquo;s a list of some separate support systems to invest in:</p>
+
+                <ol>
+                    <li>Knowledge Transfer</li>
+                    <li>Principles</li>
+                    <li>Stories &amp; Examples</li>
+                    <li>Coaching</li>
+                    <li>Accountability partners</li>
+                    <li>Practice</li>
+                </ol>
+
+                <h2 id="knowledge-transfer">Knowledge Transfer <a class="dm-dp-anchorLink" href="#knowledge-transfer">#</a></h2>
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque a asperiores dolore optio repudiandae. Iure, quisquam iusto quidem voluptas eveniet rem, architecto laudantium incidunt nulla ipsa corporis tenetur similique aut?</p>
+
+
+                <h2 id="principles">Principles <a class="dm-dp-anchorLink" href="#principles">#</a></h2>
+
+                <p>People need purpose. Drive. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque a asperiores dolore optio repudiandae. Iure, quisquam iusto quidem voluptas eveniet rem, architecto laudantium incidunt nulla ipsa corporis tenetur similique aut?</p>
+
+
+                <h2 id="stories-examples">Stories &amp; Examples <a class="dm-dp-anchorLink" href="#stories-examples">#</a></h2>
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque a asperiores dolore optio repudiandae. Iure, quisquam iusto quidem voluptas eveniet rem, architecto laudantium incidunt nulla ipsa corporis tenetur similique aut?</p>
+
+
+
+                <h2 id="coaching">Coaching <a class="dm-dp-anchorLink" href="#coaching">#</a></h2>
+
+                <p>Coaching for Performance. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque a asperiores dolore optio repudiandae. Iure, quisquam iusto quidem voluptas eveniet rem, architecto laudantium incidunt nulla ipsa corporis tenetur similique aut?</p>
+
+
+
+                <h2 id="accountability-partners">Accountability Partners <a class="dm-dp-anchorLink" href="#accountability-partners">#</a></h2>
+
+                <p>NaNoWriMo. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque a asperiores dolore optio repudiandae. Iure, quisquam iusto quidem voluptas eveniet rem, architecto laudantium incidunt nulla ipsa corporis tenetur similique aut?</p>
+
+                
+                
+                <h2 id="practice-experience">Practice &amp; Experience <a class="dm-dp-anchorLink" href="#practice-experience">#</a></h2>
+
+                <p>Kobe. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque a asperiores dolore optio repudiandae. Iure, quisquam iusto quidem voluptas eveniet rem, architecto laudantium incidunt nulla ipsa corporis tenetur similique aut?</p>
+
+
+
+                
+
+                <p>Since I&rsquo;ve decided to grow SuperFriendly, I&rsquo;ve had to be more explicit about how we can support all of our SuperFriends. We definitely don&rsquo;t have all of this in place, but making a list like this gives us some standards to meet.</p>
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-05-17/">
+                        Weeknotes: May 17&ndash;May 23, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
