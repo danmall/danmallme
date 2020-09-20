dddf22739adc41fd20a5ff04ddc1b7b9ee6755ac
Dan Mall <dan@superfriend.ly>
2020-09-20 16:58:15 -0400

    Weeknotes 2020 09 20 ready to publish

---
+++ articles/weeknotes-2020-09-13/index.php
@@ -0,0 +1,216 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: September 13&ndash;September 19, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: September 13&ndash;September 19, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">September 13</span>&ndash;<span class="dm-u-wrap--none">September 19, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">September 20, 2020 at 4:52 <abbr title="Post Meridien">PM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Worked 31 hours</li>
+                    <li>Spoke at <a href="https://2020.uxpl.us/">UX+</a></li>
+                    <li>Sent out <a href="https://mailchi.mp/superfriend.ly/design-system-resources">SuperFriendly newsletter about design system resources</a></li>
+                    <li>Plucky chat part 2 with <a href="https://twitter.com/jenniferdary">Jen Dary</a> and <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a></li>
+                    <li>Mapped out the structure for a new account</li>
+                    <li>Caught up with <a href="https://www.josiahplatt.com/">Josiah Platt</a> and <a href="https://jacobmorse.com/">Jacob Morse</a> from <a href="https://vega.studio/">Vega</a></li>
+                    <li>Chatted with <a href="https://twitter.com/bettay">Kristin Skinner</a> and <a href="https://twitter.com/louisrosenfeld">Lou Rosenfeld</a> about <a href="https://rosenfeldmedia.com/designopssummit2020/">DesignOps Summit</a></li>
+                    <li>Caught up with <a href="https://kensparks.dev/">Ken Sparks</a></li>
+                    <li>Sales conversation with new prospective client leadership with <a href="https://bradfrost.com/">Brad Frost</a> and <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a></li>
+                    <li>Biz dev check-in with Crystal Vitelli and Joe Rinaldi</li>
+                    <li>Chatted with <a href="https://www.linkedin.com/in/rebeccabeacham/">Becca Mitchell</a> from <a href="https://www.point72.com/">Point72</a> about account status</li>
+                    <li>Caught up with <a href="https://twitter.com/tpitre">TJ Pitre</a></li>
+                    <li>Caught up with <a href="https://airbagindustries.com/">Greg Storey</a></li>
+                    <li>Caught up with <a href="http://nicole-hampton.com/">Nicole Hampton</a></li>
+                    <li>Caught up to <a href="https://timshelburne.com/">Tim Shelburne</a></li>
+                    <li><a href="https://www.usearcade.com/">Arcade</a> planning with <a href="https://www.juliafernandez.design/">Julia Fernandez</a>, <a href="https://carbonology.in/">Mike Carbone</a>, and <a href="https://lesliecamacho.com/">Leslie Camacho</a> </li>
+                    <li>Reviewed new account proposal</li>
+                    <li>Created Arcade walkthroughs</li>
+                    <li>Joined <a href="https://www.familylife.com/">FamilyLife</a> Discovery Roadmap readout with <a href="https://twitter.com/lola_deal">Lauren Deal</a> and <a href="https://www.mattecook.com/">Matt Cook</a></li>
+                    <li>Watched a few talks from <a href="https://config.figma.com/">Figma Config</a></li>
+                    <li>Brainstormed new account options with Nicole Hampton, Crystal Vitell, TJ Pitre, and Joe Rinaldi</li>
+                    <li>Caught up with <a href="https://feather.ca/">Derek Featherstone</a></li>
+                    <li>Collaborated with Joe Rinaldi on proposal writing</li>
+                    <li>Caught up with <a href="https://twitter.com/jessiofhall">Jessi Hall</a></li>
+                    <li>Working session with Crystal Vitelli</li>
+                </ol>
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>
+                        My trusty iPhone X started to act up, so I decided is it was a good time to upgrade to the <a href="https://www.apple.com/iphone-11-pro/">11 Pro</a>. I like starting new phones fresh as opposed to restoring from backup so that I only have the apps that I need. Here&rsquo;s the short list of apps I manually added back to my phone:
+                        <ol>
+                            <li><a href="https://apps.apple.com/us/app/teuxdeux/id384291782">TeuxDeux</a></li>
+                            <li><a href="https://apps.apple.com/us/app/bank-of-america-mobile-banking/id284847138">Bank of America</a></li>
+                            <li><a href="https://apps.apple.com/us/app/instagram/id389801252">Instagram</a></li>
+                            <li><a href="https://apps.apple.com/us/app/google-maps-transit-food/id585027354">Google Maps</a></li>
+                            <li><a href="https://apps.apple.com/us/app/sleep-cycle-sleep-tracker/id320606217">Sleep Cycle</a></li>
+                            <li><a href="https://apps.apple.com/us/app/foursquare-swarm-check-in-app/id870161082">Foursquare Swarm</a></li>
+                            <li><a href="https://apps.apple.com/us/app/fresh-air-hyperlocal-weather-noaa-radar-map/id933439687">Fresh Air</a></li>
+                            <li><a href="https://apps.apple.com/us/app/twitter/id333903271">Twitter</a></li>
+                            <li><a href="https://apps.apple.com/us/app/google-calendar-get-organized/id909319292">Google Calendar</a></li>
+                            <li><a href="https://apps.apple.com/us/app/slack/id803453959?mt=12">Slack</a></li>
+                            <li><a href="https://apps.apple.com/us/app/facebook/id284882215">Facebook</a></li>
+                            <li><a href="https://apps.apple.com/us/app/notion-notes-projects-docs/id1232780281">Notion</a></li>
+                            <li><a href="https://apps.apple.com/us/app/spotify-music-and-podcasts/id324684580">Spotify</a></li>
+                            <li><a href="https://apps.apple.com/us/app/gmail-email-by-google/id422689480">Gmail</a></li>
+                            <li><a href="https://apps.apple.com/us/app/google-chrome/id535886823">Chrome</a></li>
+                            <li><a href="https://apps.apple.com/us/app/pinterest/id429047995">Pinterest</a></li>
+                            <li><a href="https://apps.apple.com/us/app/youtube-watch-listen-stream/id544007664">YouTube</a></li>
+                            <li><a href="https://apps.apple.com/us/app/doordash-food-delivery/id719972451">DoorDash</a></li>
+                            <li><a href="https://apps.apple.com/us/app/audible-audio-books-stories/id379693831">Audible</a></li>
+                            <li><a href="https://apps.apple.com/us/app/classdojo/id552602056">ClassDojo</a></li>
+                            <li><a href="https://apps.apple.com/us/app/google-drive/id507874739">Google Drive</a></li>
+                            <li><a href="https://apps.apple.com/us/app/google-sheets/id842849113">Google Sheets</a></li>
+                            <li><a href="https://apps.apple.com/us/app/credit-karma/id519817714">Credit Karma</a></li>
+                            <li><a href="https://apps.apple.com/us/app/uber-eats-food-delivery/id1058959277">Uber Eats</a></li>
+                            <li><a href="https://apps.apple.com/us/app/1password-password-manager/id568903335">1Password</a></li>
+                            <li><a href="https://apps.apple.com/us/app/google-docs-sync-edit-share/id842842640">Google Docs</a></li>
+                            <li><a href="https://apps.apple.com/us/app/venmo/id351727428">Venmo</a></li>
+                            <li><a href="https://apps.apple.com/us/app/dropbox-backup-sync-share/id327630330">Dropbox</a></li>
+                            <li><a href="https://apps.apple.com/us/app/adobe-lightroom-photo-editor/id878783582">Lightroom</a></li>
+                            <li><a href="https://apps.apple.com/us/app/quibi-all-new-original-shows/id1449048178">Quibi</a></li>
+                            <li><a href="https://apps.apple.com/us/app/batman-the-enemy-within/id1256141441">Batman: The Enemy Within</a></li>
+                            <li><a href="https://apps.apple.com/us/app/tovala/id1166594440">Tovala</a></li>
+                            <li><a href="https://apps.apple.com/us/app/cookunity-your-private-chef/id1103061273">CookUnity</a></li>
+                            <li><a href="https://apps.apple.com/us/app/google-photos/id962194608">Google Photos</a></li>
+                            <li><a href="https://apps.apple.com/us/app/goodreads-book-reviews/id355833469">Goodreads</a></li>
+                        </ol>
+                    </li>
+                    <li>Back in May, I moved <a href="/articles/ceo-covid19-calendar/">my daily schedule</a> around so I was working 1<abbr title="Post meridien">pm</abbr> &ndash; 6<abbr>pm</abbr>. Because of my kids&rsquo; virtual school, I&rsquo;m moving it again for me to be working 9<abbr title="Ante meridien">am</abbr> &ndash; 2<abbr>pm</abbr>.</li>
+                    <li>Slept an average of 6 hours and 10 minutes per night. Average bedtime: 11:15pm. Average wake up time: 6:25am.</li>
+                </ol>
+
+
+                <h3 id="watched">Watched <a class="dm-dp-anchorLink" href="#watched">#</a></h3>
+
+                <ol>
+                    <li><cite><a href="https://www.imdb.com/title/tt2543164/">Arrival</a></cite> (again)</li>
+                    <li>Episodes 1 and 2 of <a href="https://tv.apple.com/show/umc.cmc.vtoh0mn0xn7t3c643xqonfzy?at=1000lDR&amp;itscg=MC_20000&amp;itsct=atvp_brand_omd&amp;mttnagencyid=1625&amp;mttncc=US&amp;mttngadurl=https%3A%2F%2Ftv.apple.com%2Fshow%2Fumc.cmc.vtoh0mn0xn7t3c643xqonfzy%3F&amp;mttngmo=&amp;mttnmyad=461870170304&amp;mttnmysite=uqa99n2zg0&amp;mttnpid=305109&amp;mttnsiteid=143238&amp;mttnsub1=e&amp;mttnsub2=c&amp;mttnsub3=srdMG7cVq_461870170304_uqa99n2zg0_c&amp;mttnsubad=OUS2019863-1&amp;mttnsubadgpname=kwd-937070194980&amp;mttnsubadgpref=106182847425&amp;mttnsubadref=&amp;mttnsubcmp=10558867171&amp;mttnsubkw=ted%20lasso&amp;mttnsubpid=g&amp;mttnsubplmnt=">Ted Lasso</a></li>
+                    <li>NBA playoffs</li>
+                </ol>
+
+
+                <h3 id="read">Notable reads <a class="dm-dp-anchorLink" href="#read">#</a></h3>
+
+                <ol>
+                    <li>Finished <cite><a href="https://a16z.com/book/secrets-of-sand-hill-road/">Secrets of Sand Hill Road</a></cite> by Scott Kupor.</li>
+                    <li>Started <cite><a href="https://mitpress.mit.edu/books/mismatch">Mismatch</a></cite> by Kat Holmes.</li>
+                    <li>Here&rsquo;s a list of <a href="https://www.thebalancesmb.com/all-the-taxes-your-business-must-pay-399045">all the taxes your business must pay</a> (listed on a site I helped to create!).</li>
+                    <li>&ldquo;To encourage a culture of flexible persistence and iterative improvement&hellip; <a href="https://www.linkedin.com/pulse/arent-any-typos-essay-we-launched-too-late-reid-hoffman/">a little embarrassment</a> in those first few weeks after lunch is a small price to pay.&rdquo;</li>
+                    <li>The U.S. Digital Response has announced a <a href="https://www.usdigitalresponse.org/ui-pm-in-residence/">Product Manager-in-Residence</a> position for their unemployment insurance team.</li>
+                </ol>
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-08-30/">
+                    Weeknotes: August 30&ndash;September 5, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
