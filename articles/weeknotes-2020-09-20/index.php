01f6b5ae08cac65c57927688d16342691a8d3c96
Dan Mall <dan@superfriend.ly>
2020-09-28 13:42:31 -0400

    Weeknotes Sept 20 2020 ready to publish

---
+++ articles/weeknotes-2020-09-20/index.php
@@ -0,0 +1,175 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: September 20&ndash;September 26, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: September 20&ndash;September 26, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">September 20</span>&ndash;<span class="dm-u-wrap--none">September 26, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">September 28, 2020 at 1:36 <abbr title="Post Meridien">PM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+
+                <ol>
+                    <li>Worked 32 hours</li>
+                    <li>I pulled my first all-nighter in years. (I did a &ldquo;stay up until 3:30am to finish a presentation&rdquo; a few weeks ago, but this one was working a full day and rolling right into the next day without sleeping.) I never want to do that again.</li>
+                    <li>Did some design for <a href="https://www.usearcade.com/">Arcade</a></li>
+                    <li>Prepped for a conversation with a new prospective client</li>
+                    <li>Talked to <a href="https://shl.vc/">Sahil Lavingia</a> about Arcade</li>
+                    <li>Workshopped with SuperFriendly team Immortal Tacos on their work for the <a href="https://faithandliberty.org/">Faith and Liberty Discovery Center</a></li>
+                    <li>Caught up with with <a href="https://twitter.com/uszynski32">Ed Usyznski</a> from <a href="https://www.familylife.com/">FamilyLife</a></li>
+                    <li>Talked to <a href="https://www.angelinacalin.com/">Lina Calin</a> and <a href="http://nicole-hampton.com/">Nicole Hampton</a> about new <a href="https://superfriendlydesign.systems/">SuperFriendly</a> projects</li>
+                    <li>Working session with <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a></li>
+                    <li>Monthly check-in with <a href="https://www.linkedin.com/in/nikki-loffredo-5a8b1945/">Nikki Loffredo</a></li>
+                    <li>Biz dev check-ins with Crystal Vitelli and <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a>    </li>
+                    <li>Talked to new prospective client about how we&rsquo;d like to work with them with <a href="https://bradfrost.com/">Brad Frost</a> and Joe Rinaldi</li>
+                    <li>Talked to <a href="https://www.linkedin.com/in/eleanor-morgan/">Eleanor Morgan</a> from <a href="https://www.invisionapp.com/">InVision</a></li>
+                    <li>Talked with <a href="https://twitter.com/Michelephant">Michelle Morrison</a>, <a href="https://twitter.com/laurenloprete">Lauren LoPrete</a>, <a href="https://www.byjarrett.com">Jarrett Ford</a>, and <a href="http://wesohaire.com">Wes O&rsquo;Haire</a> from <a href="https://dropbox.design">Dropbox Design</a> and <a href="https://twitter.com/lola_deal">Lauren Deal</a> about mentorship</li>
+                    <li>Brainstorming with Nicole Hampton, Crystal Vitelli, <a href="https://timshelburne.com">Tim Shelburne</a>, <a href="https://airbagindustries.com">Greg Storey</a>, and Joe Rinaldi about new client account strategy</li>
+                    <li>Talked to <a href="https://www.sarasoueidan.com">Sara Soueidan</a> about new work</li>
+                    <li>1-on-1 with <a href="https://isabelsousa.com">Isa Sousa</a></li>
+                    <li>Chatted with to <a href="https://twitter.com/andrew_rohman">Andrew Rohman</a> from <a href="https://www.knapsack.cloud/">Knapsack</a></li>
+                    <li>Arcade weekly planning with <a href="https://www.juliafernandez.design/">Julia Fernandez</a>, <a href="https://twitter.com/MikeCarbone">Mike Carbone</a>, and <a href="https://lesliecamacho.com/">Leslie Camacho</a></li>
+                    <li>Talked to <a href="https://www.linkedin.com/in/chloe-oddleifson-11817185/">Chloe Oddleifson</a> from Dribbble<a href="https://dribbble.com/"></a></li>
+                    <li>Talk to <a href="https://twitter.com/demianborba">Demian Borba</a> from <a href="https://www.adobe.com/products/xd.html">Adobe XD</a> team with Mike Carbone, and Leslie Camacho</li>
+                </ol>
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>I took my kids on a hike to <a href="https://www.dcnr.pa.gov/StateParks/FindAPark/RickettsGlenStatePark/Pages/default.aspx">Ricketts Glen State Park</a> to hunt for <a href="http://photos.danmall.me/#ricketts-glen-waterfall">some waterfalls</a> while <a href="http://emilymall.me/">Em</a> had some friends over.</li>
+                    <li>I lost my wallet and didn&rsquo;t even realize it. <a href="https://twitter.com/danmall/status/1307772606137987074">A kind man named Jason returned it fully intact.</a></li>
+                    <li><a href="https://twitter.com/danmall/status/1308141279231045635">Max turned 1</a>!</li>
+                    <li>We met up with <a href="https://twitter.com/octaviusanewman">Octavius</a> and his family for pizza and salads on the beach. </li>
+                    <li>Slept an average of 4 hours and 57 minutes per night. Average bedtime: 10:31pm. Average wake up time: 5:34am.</li>
+                </ol>
+
+
+                <h3 id="watched">Watched <a class="dm-dp-anchorLink" href="#watched">#</a></h3>
+
+                <ol>
+                    <li>NBA Conference finals: Celtics vs. Heat and Nuggets vs. Lakers</li>
+                    <li><a href="https://quibi.com/shows/most-dangerous-game-566/">The Most Dangerous Game</a ></li>
+                    <li><a href="https://tv.apple.com/show/umc.cmc.vtoh0mn0xn7t3c643xqonfzy?at=1000lDR&amp;itscg=MC_20000&amp;itsct=atvp_brand_omd&amp;mttnagencyid=1625&amp;mttncc=US&amp;mttngadurl=https%3A%2F%2Ftv.apple.com%2Fshow%2Fumc.cmc.vtoh0mn0xn7t3c643xqonfzy%3F&amp;mttngmo=&amp;mttnmyad=461870170304&amp;mttnmysite=uqa99n2zg0&amp;mttnpid=305109&amp;mttnsiteid=143238&amp;mttnsub1=e&amp;mttnsub2=c&amp;mttnsub3=srdMG7cVq_461870170304_uqa99n2zg0_c&amp;mttnsubad=OUS2019863-1&amp;mttnsubadgpname=kwd-937070194980&amp;mttnsubadgpref=106182847425&amp;mttnsubadref=&amp;mttnsubcmp=10558867171&amp;mttnsubkw=ted%20lasso&amp;mttnsubpid=g&amp;mttnsubplmnt=">Ted Lasso</a></li>
+                    <li><a href="https://quibi.com/shows/blackballed-622">Blackballed</a></li>
+                </ol>
+
+
+                <h3 id="read">Notable reads <a class="dm-dp-anchorLink" href="#read">#</a></h3>
+
+                <ol>
+                    <li><a href="https://medium.com/positiveslope/crafting-the-first-mile-of-product-7ed25e8f1027#.751hpkf8k">The absolute best hook in the first mile of a user experience is DOING things proactively for the user.</a></li>
+                    <li>Check your voter registration at <a href="https://2020upto.us/">Up To Us</a>.</li>
+                    <li><a href="https://unblast.com/">Unblast</a> has free design resources to download.</li>
+                    <li>&ldquo;What I&rsquo;ve experienced over and over again is a mindset where teams across domains, brands, or businesses tend to think their products are unique. The truth is they&rsquo;re far more alike than different,&rdquo; <a href="https://twitter.com/hayhughes">Hayley Hughes</a> wisely observes in <a href="https://www.designsystemsforfigma.com/blog/bringing-context-to-design-systems">Bringing Context To Design Systems</a>.</li>
+                </ol>
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-09-13/">
+                    Weeknotes: September 13&ndash;September 19, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
