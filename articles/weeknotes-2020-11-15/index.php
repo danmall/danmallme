5b71f8a32ab426f937db55d2c60f11a3a4bc2d57
Dan Mall <dan@superfriend.ly>
2020-11-22 06:49:13 -0500

    Weeknotes 2020-11-15 done

---
+++ articles/weeknotes-2020-11-15/index.php
@@ -0,0 +1,190 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: November 15&ndash;November 21, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: November 15&ndash;November 21, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">November 15</span>&ndash;<span class="dm-u-wrap--none">November 21, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">November 22, 2020 at 6:50 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+                
+                <ol>
+                    <li>Worked 38 hours</li>
+                    <li>Work on project content creation</li>
+                    <li>Sketched out SuperFriend fees for 3 new prospective projects</li>
+                    <li>Talked design systems with <a href="https://mcleodm3.com/">Mike McLeod</a></li>
+                    <li>Working session with <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a></li>
+                    <li>Caught up with <a href="https://scottcook.me/">Scott Cook</a></li>
+                    <li>Talked business models with <a href="http://sydleigh.com/">Syd Leigh</a></li>
+                    <li>Biz dev check-in with Crystal Vitelli and <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a></li>
+                    <li>Finished editing the first episode of an upcoming design systems course</li>
+                    <li>Joined Immortal Tacos team for a launch prep call</li>
+                    <li>Shared thoughts with team 🤖⚡️ for growing their account</li>
+                    <li>Shot some B-roll</li>
+                    <li>Schemed with <a href="https://twitter.com/octaviusanewman">Octavius A. Newman</a></li>
+                    <li>Talked agency positioning with <a href="https://twitter.com/iamlaurendeal/">Lauren Deal</a></li>
+                    <li>1-on-1 with <a href="https://isabelsousa.com/">Isa Sousa</a></li>
+                    <li>Account strategy call with <a href="https://joshua-jantz.com/">Joshua Jantz</a>, <a href="https://www.mattecook.com/">Matt Cook</a>, <a href="https://blankenship.xyz/">Joshua Blankenship</a>, Crystal Vitelli, and Joe Rinaldi</li>
+                    <li>New client sales call with <a href="http://nicole-hampton.com/">Nicole Hampton</a>, <a href="https://bradfrost.com/">Brad Frost</a>, and Joe Rinaldi</li>
+                    <li>Reviewed content samples with a client</li>
+                    <li>Interviewed by <a href="https://www.ritaolds.com/">Rita Olds</a></li>
+                    <li>Pre-project call with Lauren Deal, <a href="https://www.babygrand.co/">Baby Grand</a>, <a href="https://celsowhite.com/">Celso White</a>, <a href="https://dontaebenn.myportfolio.com/">Dontae Benn</a>, <a href="https://twitter.com/jessiofhall/">Jessi Hall</a>, and Joe Rinaldi</li>
+                    <li>Caught up with <a href="https://twitter.com/abbyfretz">Abby Fretz</a></li>
+                </ol>
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li>Construction is going on in our house now in order to list to sell.</li>
+                    <li>Talked to my therapist about constantly feeling misunderstood.</li>
+                    <li>Took <a href="http://emilymall.me/">Em</a> out for dinner at <a href="https://www.vernickfish.com/">Vernick Fish</a> for one last hoorah before Philly restaurants shut down again due to COVID restrictions.</li>
+                    <li>Twitter released Fleets this week. I thought I didn&rsquo;t like the Story format, but I&rsquo;m realizing it&rsquo; because they didn&rsquo;t fit my usage of other social media. I use Facebook and Instagram for friends and family only, and I&rsquo;m much more guarded about the content I share there. But, I use Twitter more casually as well as professionally and have a lot more to share via that platform. I could see myself using Fleets a lot.</li>
+                    <li>Our new pool inspection came back horrible. Lots of expensive work to do 😞</li>
+                    <li>The Sixers picked up Seth Curry, Dwight Howard, Danny Green, Terrance Ferguson, Tyrese Maxey, Isiah Joe, and Paul Reed and got rid of Al Horford and Josh Richardson. I&rsquo;M SO EXCITED FOR THIS SEASON.</li>                    
+                    <li>Slept an average of 6 hours and 22 minutes per night. Average time in bed: 7 hours and 21 minutes. Average bedtime: 9:46pm. Average wake up time: 4:42am.</li>
+                </ol>
+
+
+                <h3 id="watched">Watched <a class="dm-dp-anchorLink" href="#watched">#</a></h3>
+
+                <ol>
+                    <li><cite><a href="https://www.hbomax.com/feature/urn:hbo:feature:GXdkpqAvyDaXCPQEAADdn">The Dark Knight</a></cite> (again)</li>
+                    <li><cite><a href="https://www.hbomax.com/coming-soon/fresh-prince-reunion">The Fresh Prince of Bel-Air Reunion</a></cite></li>
+                    <li>Started <cite><a href="https://www.amazon.com/Inception-Leonardo-DiCaprio/dp/B0047WJ11G">Inception</a></cite> (again)</li>
+                    <li>Started <cite><a href="https://www.netflix.com/title/80107414">The Killing Joke</a></cite></li>
+                    <li><cite><a href="https://www.netflix.com/title/70044883">Nacho Libre</a></cite></li>
+                    <li>Started part 2 of the <a href="https://30x500.com/academy/">30x500 Academy course</a></li>
+                </ol>
+
+
+                <h3 id="read">Notable reads <a class="dm-dp-anchorLink" href="#read">#</a></h3>
+
+                <ol>
+                    <li><cite>Fantastic Four</cite> &ldquo;<a href="https://www.marvel.com/comics/issue/25182/fantastic_four_1998_570">Solve Everything</a>&rdquo; story arc by Jonathan Hickman and Dale Eaglesham</li>
+                    <li>Started <cite><a href="https://amzn.to/3fldzpY">Mister Miracle</a></cite> by Tom King and Mitch Gerads</li>
+                    <li>Invision release a great <cite><a href="https://www.invisionapp.com/lp/design-system-community-guide">Guide to Building a Design System Community</a></cite>.</li>
+                    <li>
+                        I learned a lot about shooting B-roll this week. Some videos that were invaluable:
+                        <ul>
+                            <li><a href="https://www.youtube.com/watch?v=xkOMaMuk6jY">The Difference Between Beginner and Pro B-Roll</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=lVIraY22xzw">How I turn a boring shot into &ldquo;EPIC B ROLL&ldquo; | Behind the Edit</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=3Zv1KDn_Lk8">Start Shooting Quick B-Roll</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=qJBdSlXvhUA">The Secret to Shooting Good B-Roll of Yourself</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=OQio1niW_do">Teaching a Subscriber How to Shoot B-Roll</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=Xra1qIQHscI">The B-Roll Challenge</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=jgYg0LfFUI4">How I Edit B-Roll &amp; Sound Design</a></li>
+                        </ul>
+                    </li>
+                </ol>
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-11-08/">
+                    Weeknotes: November 8&ndash;November 14, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
