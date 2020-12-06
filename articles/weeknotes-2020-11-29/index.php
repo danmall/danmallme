0c2a2d9e784e338c20eab0b349c5d2627e54bb3d
Dan Mall <dan@superfriend.ly>
2020-12-06 07:32:03 -0500

    Weeknotes ready to publish

---
+++ articles/weeknotes-2020-11-29/index.php
@@ -0,0 +1,188 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>&ldquo;Weeknotes: November 29&ndash;December 5, 2020,&rdquo; an article by Dan Mall</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?> 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>
+
+    <!-- Thanks Jeremy! https://adactio.com/journal/9881  -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@danmall" />
+    <meta name="twitter:url" property="og:url" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" property="og:title" content="Weeknotes: November 29&ndash;December 5, 2020" />
+    <meta name="twitter:description" property="og:description" content="Highlights from the week." />
+    <meta name="description" content="Highlights from the week." />
+    <meta name="twitter:image" property="og:image" content="<?php echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>thumb.png" />
+
+    <style type="text/css">
+
+        @media screen and (max-width: 800px){
+            .dm-c-pageHeader_title .dm-u-wrap--none {
+                white-space: normal;
+            }
+        }
+
+    </style>
+    
+</head>
+
+
+<body class="dm-page--articleDetail">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <script>
+        document.write('<div class="dm-js-pageShield"></div>');
+    </script>
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/header.php"); echo "\n"; ?>
+
+    <div class="dm-l-articleGrid">
+
+        <header class="dm-c-pageHeader dm-l-articleGrid--main" role="banner">
+
+            <h1 class="dm-c-pageHeader_title">
+                Weeknotes: <span class="dm-u-wrap--none">November 29</span>&ndash;<span class="dm-u-wrap--none">December 5, 2020</span>  
+            </h1>
+
+            <h2 class="dm-c-pageHeader_date">December 6, 2020 at 7:30 <abbr title="Ante Meridien">AM</abbr></h2>
+
+        </header><!-- .page-header -->
+
+        <main role="main" class="dm-c-articleWell">
+
+            <div class="dm-dp-textPassage dm-l-articleGrid--main">
+
+                <div class="dm-c-carbonWrap">
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&amp;serve=CVYD42T&amp;placement=danielmallcom" id="_carbonads_js"></script>
+                </div><!-- .dm-c-carbonWrap -->
+
+                <h2 id="themes">Themes <a class="dm-dp-anchorLink" href="#themes">#</a></h2>
+
+                <ul>
+                    <li>When I focus on a smaller number of things, I work more. When I have a lot of little things to do, I work less. I wonder if lots of little things fatigue me, as opposed to a smaller number of big things.</li>
+                    <li>I tend to work more when I&rsquo;m enjoying myself or am excited about something. This week, I was excited about launching the course, and I started on a side project I&rsquo;ve been mulling over in my head for months.</li>
+                    <li>I tend to see holiday weeks as weeks I can focus on my own things. I wonder if that means I see &ldquo;normal&rdquo; weeks as ones where I&rsquo;m obligated to do something instead of pursuing. I&rsquo;d like to figure out how to carve out more time each week to work on the stuff I want to.</li>
+                </ul>
+                
+                <h2 id="work">Work <a class="dm-dp-anchorLink" href="#work">#</a></h2>
+                
+                <ol>
+                    <li>Worked 36 hours</li>
+                    <li>Sent out <a href="https://mailchi.mp/superfriend.ly/make-design-systems-people-want-to-use">an email newsletter</a> about the &ldquo;<a href="https://superfriendlydesign.systems/classes/make-design-systems-people-want-to-use/">Make Design Systems People Want to Use</a>&rdquo; course</li>
+                    <li>Debugged some Mailchimp email newsletter signup weirdness</li>
+                    <li>Talked email lists with <a href="https://twitter.com/crystalvitelli">Crystal Vitelli</a>, <a href="https://www.linkedin.com/in/nikki-loffredo-5a8b1945/">Nikki Loffredo</a>, and <a href="https://twitter.com/tpitre">TJ Pitre</a></li>
+                    <li>Attended my first meeting as a board member of <a href="https://give.vocatioschool.org/">Vocatio Career Prep High School</a></li>
+                    <li>Started designing and coding a new internal <a href="https://superfriendlydesign.systems/">SuperFriendly</a> project (used <a href="https://www.11ty.dev/">Eleventy</a> for the first time)</li>
+                    <li>Recorded an upcoming podcast episode</li>
+                    <li>Workshopped with Crystal Vitelli</li>
+                    <li>Sales call with prospective client with <a href="https://twitter.com/iamlaurendeal/">Lauren Deal</a>, <a href="https://celsowhite.com/">Celso White</a>, <a href="https://www.babygrand.co/">Baby Grand</a>, and <a href="https://twitter.com/joerinaldi">Joe Rinaldi</a></li>
+                    <li>Biz dev check-ins with Crystal Vitelli and Joe Rinaldi</li>
+                    <li>Caught up with <a href="https://www.hilaryfenton.com/">Hilary Fenton</a></li>
+                    <li>Wrote <a href="/articles/weeknotes-2020-11-22/">weeknotes</a></li>
+                    <li>Workshopping prospective client team structure with Crystal Vitelli and Joe Rinaldi</li>
+                    <li>Talked prospective client tech discovery process with <a href="https://www.vagabondcollective.com/">Nicole Hampton</a>, <a href="https://bradfrost.com/">Brad Frost</a>, <a href="https://timshelburne.com/">Tim Shelburne</a>, and Joe Rinaldi</li>
+                    <li>Attended the <a href="https://www.notion.so/Practical-Innovation-Summit-Tokens-e3d2d33c8d0246949261abdb72b75247">Practical Innovation Summit: Tokens</a> with the <a href="https://www.usearcade.com/">Arcade team</a></li>
+                    <li>Wrote new client statement of work</li>
+                    <li>Workshopped client content with Lauren Deal, Crystal Vitelli, and <a href="https://www.mattecook.com/">Matt Cook</a></li>
+                    <li>1-on-1 with <a href="https://isabelsousa.com/">Isa Sousa</a></li>
+                    <li>1-on-1 with <a href="https://www.juliafernandez.design/">Julia Fernandez</a></li>
+                    <li>New project scoping with <a href="https://feather.ca/">Derek Featherstone</a></li>
+                    <li>Talked to client tech team about what handoff should look like with Lauren Deal and Crystal Vitelli</li>
+                    <li>Schemed with <a href="https://twitter.com/K_Deal2">Kevin Deal</a></li>
+                    <li>Schemed with <a href="https://www.afyiasmith.com/">Afyia Smith</a></li>
+                    <li>Schemed with <a href="https://appropriateinc.com/">Margot Bloomstein</a></li>
+                    <li>Caught up with <a href="https://lesliecamacho.com/">Leslie Camacho</a></li>
+                </ol>
+
+                <h2 id="personal">Personal <a class="dm-dp-anchorLink" href="#personal">#</a></h2>
+
+                <ol>
+                    <li><a href="https://twitter.com/charlottemall">Charlie</a> turned 7!</li>
+                    <li>My parents celebrated their 38th anniversary. Happy anniversary, Mom &amp; Dad!</li>
+                    <li>Buying a new house is equal parts exciting and boring. I spent a lot of time this week on the phone and on email get homeowners insurance quotes and filling out/signing documents.</li>
+                    <li>Slept an average of 6 hours and 18 minutes per night. Average time in bed: 7 hours and 15 minutes. Average bedtime: 9:58pm. Average wake up time: 5:10am.</li>
+                </ol>
+
+
+                <h3 id="watched">Watched <a class="dm-dp-anchorLink" href="#watched">#</a></h3>
+
+                <ol>
+                    <li><cite><a href="https://www.disneyplus.com/movies/d2-the-mighty-ducks/t92qLjllJsbr">D2: The Mighty Ducks</a></cite> (again)</li>
+                    <li><cite><a href="https://www.disneyplus.com/movies/d3-the-mighty-ducks/60MKBZyPVXSC">D3: The Mighty Ducks</a></cite> (again)</li>
+                    <li><cite><a href="https://www.amazon.com/Mission-Impossible-Fallout-Tom-Cruise/dp/B07FSQDJKG">Mission: Impossible - Fallout</a></cite> (again)</li>
+                    <li><cite><a href="https://www.disneyplus.com/series/the-mandalorian/3jLIGMDYINqD">The Mandalorian</a></cite>, season 2, episodes 2&ndash;6</li>
+                </ol>
+
+
+                <h3 id="read">Notable reads <a class="dm-dp-anchorLink" href="#read">#</a></h3>
+
+                <ol>
+                    <li><cite>Captain Britain (1976) <a href="https://www.marvel.com/comics/issue/62513/captain_britain_1976_1">#1</a>&ndash;<a href="https://www.marvel.com/comics/issue/62514/captain_britain_1976_2">2</a></cite></li>
+                    <li><cite>Marvel Team-Up <a href="https://www.marvel.com/comics/issue/19687/marvel_team-up_1972_65">#65</a>&ndash;<a href="https://www.marvel.com/comics/issue/19688/marvel_team-up_1972_66">#66</a></cite></li>
+                    <li><cite>Excalibur (1988) <a href="https://www.marvel.com/comics/issue/8562/excalibur_1988_1">#1</a></cite></li>
+                    <li><cite>Marauders (2019) <a href="https://www.marvel.com/comics/issue/78405/marauders_2019_11">#11</a></cite></li>
+                    <li><cite>Excalibur (2019) <a href="https://www.marvel.com/comics/issue/78392/excalibur_2019_11">#11</a></cite></li>
+                    <li><a href="https://codeopinion.com/the-complexity-of-caching/">Avoid caching if you can</a>.</li>
+                </ol>
+
+
+            </div>
+
+            <section id="up-next" class="dm-c-upNext">
+
+                <h1 class="dm-c-upNext_kicker">Up Next</h1>
+
+                <h2 class="dm-c-upNext_title">
+                    <a href="/articles/weeknotes-2020-11-22/">
+                    Weeknotes: November 22&ndash;November 28, <span class="dm-c-upNext_title_lastWord">2020</span>
+                    </a>
+                </h2>
+
+            </section><!-- .dm-c-upNext -->
+
+        </main>
+
+    </div><!-- .dm-l-articleGrid -->
+
+
+
+    <section id="comments" class="dm-c-comments">      
+            
+        <div class="main">
+            
+            <div id="disqus_thread"></div>
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                 */
+                
+                var disqus_config = function () {
+                    this.page.url = 'http://danmall.me/articles/<?php echo getArticleSlug(); ?>';
+                    this.page.identifier = '<?php echo getArticleSlug(); ?>';
+                    //this.page.title = 'a unique title for each page where Disqus is present';
+                };
+                
+                (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+                    var d = document, s = d.createElement('script');
+                    
+                    s.src = '//danielmall.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+            
+        </div><!-- /.main -->
+        
+            
+    </section><!-- #comments -->
+
+    
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/footer.php"); echo "\n"; ?>    
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
